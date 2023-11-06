f5b3168318387cd7917bb9cef5a5141a8c6d766c
lucas-phillips28 <lucas.phillips@hmcts.net>
2023-11-06 14:25:55 +0000

    Testing endpoint to get url for streaming of mediakind

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/RecordingController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/RecordingController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import uk.gov.hmcts.reform.preapi.controllers.response.StreamingLinkResponse;
 import uk.gov.hmcts.reform.preapi.entities.Recording;
 import uk.gov.hmcts.reform.preapi.entities.User;
+import uk.gov.hmcts.reform.preapi.mediakind.MediaKindService;
 import uk.gov.hmcts.reform.preapi.sharedrecordings.service.SharedRecordingService;
 import uk.gov.hmcts.reform.preapi.user.service.UserService;
 
@@ -26,6 +28,9 @@ public class RecordingController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private MediaKindService mediaKindService;
 
     private static final UUID USER_ID = UUID.fromString("26b4398e-72d0-43d6-a0c5-3f16b9629cb6");
 
@@ -60,5 +65,14 @@ public class RecordingController {
         }
 
         return new ResponseEntity<>(sharedRecording.get(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{recording_id}/play")
+    public ResponseEntity<StreamingLinkResponse> getLink(@PathVariable(name = "recording_id") String recordingId) {
+        String streamingLocatorName = mediaKindService.getStreamingLocatorName(recordingId);
+        return new ResponseEntity<>(
+            mediaKindService.getStreamingPathsForStreamingLocator(streamingLocatorName),
+            HttpStatus.OK
+        );
     }
 }
