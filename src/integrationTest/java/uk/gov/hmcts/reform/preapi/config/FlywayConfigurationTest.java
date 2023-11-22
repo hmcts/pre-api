96e1b04e22f0ce8c529ae452d37c72befc57c328
Jason Paige <j@jasonpaige.co.uk>
2023-11-22 14:57:40 +0000

    jenkins enable migration

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/preapi/config/FlywayConfigurationTest.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.preapi.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import uk.gov.hmcts.reform.preapi.data.migration.FlywayNoOpStrategy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebMvcTest(properties = {"dbMigration.runOnStartup=false"})
+@ContextConfiguration(classes = FlywayConfiguration.class)
+class FlywayConfigurationTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    void shouldAddFlywayConfigurationBeanToApplicationContext() {
+        assertThat(applicationContext.containsBean("flywayConfiguration"));
+    }
+
+    @Test
+    void shouldReturnFlywayNoOpStrategy() {
+        final FlywayConfiguration flywayConfiguration = (FlywayConfiguration) (applicationContext.getBean(
+            "flywayConfiguration"));
+        assertThat(flywayConfiguration.flywayMigrationStrategy() instanceof FlywayNoOpStrategy);
+    }
+}
