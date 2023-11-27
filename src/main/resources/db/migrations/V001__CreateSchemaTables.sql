c5a5db2cbee432909a9f87b8737acdfc983345ae
mazzopardi2 <marianne.azzopardi@hmcts.net>
2023-11-27 14:45:37 +0000

    added enum, changed table and field names, created association table for roles and permissions

--- src/main/resources/db/migrations/V001__CreateSchemaTables.sql
+++ src/main/resources/db/migrations/V001__CreateSchemaTables.sql
@@ -1,113 +1,166 @@
 -------------------------------------- Create types
-CREATE TYPE public.COURT_TYPE AS ENUM ('crown', 'magistrate', 'family');
-CREATE TYPE public.PARTICIPANT_TYPE AS ENUM ('witness','defendant');
-CREATE TYPE public.RECORDING_STATUS AS ENUM ('standby', 'initialisation', 'recording', 'finished', 'processing', 'available', 'future');
-CREATE TYPE public.RECORDING_ORIGIN AS ENUM ('pre', 'vodafone');
-CREATE TYPE public.AUDIT_LOG_SOURCE AS ENUM ('application', 'portal', 'admin', 'auto');
-CREATE TYPE public.AUDIT_LOG_TYPE AS ENUM ('create', 'update', 'delete', 'action');
+CREATE TYPE public.COURT_TYPE AS ENUM (
+    'crown', 
+    'magistrate', 
+    'family'
+);
+
+CREATE TYPE public.PARTICIPANT_TYPE AS ENUM (
+    'witness','
+    defendant'
+);
+
+CREATE TYPE public.RECORDING_STATUS AS ENUM (
+    'standby', 
+    'initialisation', 
+    'recording', 
+    'finished', 
+    'processing', 
+    'available', 
+    'future'
+);
+
+CREATE TYPE public.RECORDING_ORIGIN AS ENUM (
+    'pre', 
+    'vodafone'
+);
+
+CREATE TYPE public.AUDIT_LOG_SOURCE AS ENUM (
+    'application', 
+    'portal', 
+    'admin', 
+    'auto'
+);
+
+CREATE TYPE public.AUDIT_LOG_TYPE AS ENUM (
+    'create', 
+    'update', 
+    'delete', 
+    'action'
+);
+
+CREATE TYPE public.ACCESS_STATUS AS ENUM (
+    'invitation_sent', 
+    'registered', 
+    'active', 
+    'inactive'
+);
+
 
 -------------------------------------- Create tables
 CREATE TABLE public.regions (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     name VARCHAR(100)
 );
 
 CREATE TABLE public.courts (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     court_type COURT_TYPE NOT NULL,
     name VARCHAR(255) NOT NULL,
     location_code VARCHAR(25)
 );
 
 CREATE TABLE public.rooms (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     room VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE public.court_regions(
-    id UUID PRIMARY KEY NOT NULL,
+CREATE TABLE public.court_region(
     court_id UUID REFERENCES courts(id) NOT NULL,
-    region_id UUID REFERENCES regions(id) NOT NULL
-
+    region_id UUID REFERENCES regions(id) NOT NULL,
+    PRIMARY KEY (court_id, region_id)
 );
 
-CREATE TABLE public.courtrooms (
-    id UUID PRIMARY KEY NOT NULL,
+CREATE TABLE public.courtrooms(
+    id UUID PRIMARY KEY,
     court_id UUID REFERENCES courts(id) NOT NULL,
     room_id UUID REFERENCES rooms(id) NOT NULL
 );
 
 CREATE TABLE public.cases (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     court_id UUID REFERENCES courts(id) NOT NULL ,
-    case_ref VARCHAR(25) NOT NULL, 
+    reference VARCHAR(25) NOT NULL, 
     test BOOLEAN DEFAULT FALSE,
     deleted BOOLEAN DEFAULT FALSE,
-    created_on TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    modified_on TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    modified_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 
 CREATE TABLE public.participants (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY ,
     case_id UUID REFERENCES cases(id) NOT NULL,
     participant_type PARTICIPANT_TYPE NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     deleted BOOLEAN DEFAULT FALSE,
-    created_on TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    modified_on TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    modified_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE public.bookings (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     case_id UUID REFERENCES cases(id) NOT NULL,
-    date DATE NOT NULL,
+    scheduled_for TIMESTAMPTZ NOT NULL,
     deleted BOOLEAN DEFAULT FALSE,
-    created_on TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    modified_on TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    modified_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 
-CREATE TABLE public.booking_participants (
-    id UUID PRIMARY KEY NOT NULL,
+CREATE TABLE public.booking_participant (
     participant_id UUID REFERENCES participants(id) NOT NULL,
-    booking_id UUID REFERENCES bookings(id) NOT NULL
+    booking_id UUID REFERENCES bookings(id) NOT NULL,
+    PRIMARY KEY (participant_id, booking_id)
 );
 
 CREATE TABLE public.users (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     organisation VARCHAR(250),
     phone VARCHAR(50),
     deleted BOOLEAN DEFAULT FALSE,
-    created_on TIMESTAMPTZ DEFAULT NOW(),
-    modified_on TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    modified_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE public.roles (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     name VARCHAR(45) NOT NULL
 );
 
+CREATE TABLE public.permissions (
+    id UUID PRIMARY KEY,
+    name VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE role_permission (
+    role_id UUID REFERENCES roles(id),
+    permission_id UUID REFERENCES permissions(id),
+    PRIMARY KEY (role_id, permission_id)
+);
+
+
 CREATE TABLE public.app_access (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     user_id UUID REFERENCES users(id) NOT NULL,
     court_id UUID REFERENCES courts(id) NOT NULL,
     role_id UUID REFERENCES roles(id) NOT NULL,
     last_access TIMESTAMPTZ,
     active BOOLEAN DEFAULT TRUE,
     deleted BOOLEAN DEFAULT FALSE,
-    created_on TIMESTAMPTZ DEFAULT NOW(),
-    modified_on TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    modified_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 
 CREATE TABLE public.capture_sessions (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     booking_id UUID REFERENCES bookings(id) NOT NULL,
+    parent_recording_id UUID REFERENCES recordings(id) NOT NULL,
     origin RECORDING_ORIGIN NOT NULL,
     ingest_address VARCHAR(255),
     live_output_url VARCHAR(100),
@@ -119,44 +172,42 @@ CREATE TABLE public.capture_sessions (
     deleted BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE public.recording_versions (
-    id UUID PRIMARY KEY NOT NULL,
+CREATE TABLE public.recordings (
+    id UUID PRIMARY KEY,
     capture_session_id UUID REFERENCES capture_sessions(id) NOT NULL,
     version INT NOT NULL,
     url VARCHAR(255) NOT NULL,
     filename VARCHAR(255) NOT NULL,
-    created_on TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
     duration TIME,
     edit_instruction JSON,
     deleted BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE public.share_recordings (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     capture_session_id UUID REFERENCES capture_sessions(id) NOT NULL,
     shared_with_user_id UUID REFERENCES users(id) NOT NULL,
     shared_by_user_id UUID REFERENCES users(id) NOT NULL,
-    created_on TIMESTAMPTZ DEFAULT NOW(),
+    created_at TIMESTAMPTZ DEFAULT NOW(),
     deleted BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE public.portal_access (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     user_id UUID REFERENCES users(id) NOT NULL,
     password VARCHAR(45) NOT NULL,
     last_access TIMESTAMPTZ,
-    invitation_sent BOOLEAN DEFAULT FALSE,
+    status ACCESS_STATUS NOT NULL DEFAULT 'invitation_sent'::ACCESS_STATUS,
     invitation_datetime TIMESTAMPTZ,
-    registered BOOLEAN DEFAULT FALSE,
     registered_datetime TIMESTAMPTZ,
-    active BOOLEAN,
     deleted BOOLEAN DEFAULT FALSE,
-    created_on TIMESTAMPTZ DEFAULT NOW(),
-    modified_on TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    modified_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE public.audits (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     table_name VARCHAR(25),
     table_record_id UUID,
     source AUDIT_LOG_SOURCE NOT NULL,
@@ -166,6 +217,22 @@ CREATE TABLE public.audits (
     functional_area VARCHAR(100),
     audit_details TEXT,
     created_by VARCHAR(50),
-    created_on TIMESTAMPTZ NOT NULL DEFAULT NOW() 
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() 
 );
 
+CREATE TABLE public.audits (
+    id UUID PRIMARY KEY,
+    auditable_type VARCHAR(25),
+    auditable_id UUID,
+    action VARCHAR(10) NOT NULL,
+    category VARCHAR(100),
+    activity VARCHAR(100),
+    functional_area VARCHAR(100),
+    audit_details TEXT,
+    created_by VARCHAR(50),
+    
+    payload JSON,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
