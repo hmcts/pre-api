6690e604440d2c1b5ee1babe29c649f68f13e3d6
Jason Paige <jason.r.paige@gmail.com>
2023-12-01 13:14:26 +0000

    Initial Booking controller

---
+++ src/main/java/uk/gov/hmcts/reform/preapi/model/Booking.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.preapi.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.Column;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.preapi.entities.Case;
+import uk.gov.hmcts.reform.preapi.entities.Participant;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Booking {
+
+    private Long id;
+    private Case forCase;
+    private Timestamp scheduledFor;
+    private Timestamp deletedAt;
+    private Timestamp createdAt;
+    private Timestamp modifiedAt;
+    private Set<Participant> participants;
+}
---
+++ src/test/java/uk/gov/hmcts/reform/preapi/controller/BookingControllerTest.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.preapi.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.reform.preapi.controllers.BookingController;
+import uk.gov.hmcts.reform.preapi.model.Booking;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookingController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class BookingControllerTest {
+
+    @Autowired
+    private transient MockMvc mockMvc;
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final String TEST_URL = "http://localhost";
+
+    @DisplayName("Should create a booking with 201 response code")
+    @Test
+    void createBookingEndpointCreated() throws Exception {
+
+        MvcResult response = mockMvc.perform(put("/cases/123/bookings/456")
+                            .with(csrf())
+                            .content(OBJECT_MAPPER.writeValueAsString(new Booking()))
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .accept(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(status().isCreated())
+            .andReturn();
+
+        assertThat(response.getResponse().getContentAsString()).isEqualTo("");
+        assertThat(response.getResponse().getHeaderValue("Location")).isEqualTo(TEST_URL + "/cases/123/bookings/456/");
+    }
+
+    @DisplayName("Should fail to create a booking with 400 response code")
+    @Test
+    void createBookingEndpointNotAcceptable() throws Exception {
+        mockMvc.perform(put("/cases/123/bookings/456")).andExpect(status().is4xxClientError());
+    }
+}
