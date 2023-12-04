ba324928bf0f286f14635e06f5e083ccd566841c
lucas-phillips28 <lucas.phillips@hmcts.net>
2023-12-04 14:49:33 +0000

    Remove unused service

--- src/main/java/uk/gov/hmcts/reform/preapi/courts/services/CourtService.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.preapi.courts.services;
-
-import uk.gov.hmcts.reform.preapi.entities.Court;
-
-import java.util.UUID;
-
-public interface CourtService {
-    Court findById(UUID id);
-}
