01ecbe019534de34bfc1ccd14b876c00eaf8a11e
lucas-phillips28 <lucas.phillips@hmcts.net>
2023-12-05 15:02:29 +0000

    Add `GET /bookings/{bookingId}/recordings`

--- src/main/java/uk/gov/hmcts/reform/preapi/repositories/RecordingRepository.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/repositories/RecordingRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.preapi.entities.Recording;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,4 +13,7 @@ import java.util.UUID;
 @SuppressWarnings("PMD.MethodNamingConventions")
 public interface RecordingRepository extends JpaRepository<Recording, UUID> {
     Optional<Recording> findByIdAndCaptureSession_Booking_Id(UUID recordingId, UUID bookingId);
+
+    List<Recording> findAllByCaptureSession_Booking_IdAndDeletedAtIsNull(UUID bookingId);
+
 }
