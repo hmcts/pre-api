95cc60ed1c4e1a335aaa466cb4cd02dccd147e79
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2023-12-07 11:31:12 +0000

    S28 2241: PUT `/bookings/{bookingId}/recordings/{recordingId}` (#105)
    * Fix recording endpoints no longer return deleted recordings
    
    * Add PUT `/bookings/{bookingId}/recordings/{recordingId}`
    
    * Tests for new endpoint
    
    * Styling
    
    * PMD suppressed
    
    * Tests for recording service exceptions
    
    * Fix merge
    
    * Fix delete tests
    
    * Styling

--- src/main/java/uk/gov/hmcts/reform/preapi/services/RecordingService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/RecordingService.java
@@ -5,8 +5,11 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.preapi.dto.RecordingDTO;
+import uk.gov.hmcts.reform.preapi.entities.Recording;
+import uk.gov.hmcts.reform.preapi.enums.UpsertResult;
 import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
 import uk.gov.hmcts.reform.preapi.repositories.BookingRepository;
+import uk.gov.hmcts.reform.preapi.repositories.CaptureSessionRepository;
 import uk.gov.hmcts.reform.preapi.repositories.RecordingRepository;
 
 import java.sql.Timestamp;
@@ -19,13 +22,15 @@ import java.util.stream.Collectors;
 public class RecordingService {
 
     private final RecordingRepository recordingRepository;
-
     private final BookingRepository bookingRepository;
+    private final CaptureSessionRepository captureSessionRepository;
 
     @Autowired
-    public RecordingService(RecordingRepository recordingRepository, BookingRepository bookingRepository) {
+    public RecordingService(RecordingRepository recordingRepository, BookingRepository bookingRepository,
+                            CaptureSessionRepository captureSessionRepository) {
         this.recordingRepository = recordingRepository;
         this.bookingRepository = bookingRepository;
+        this.captureSessionRepository = captureSessionRepository;
     }
 
     @Transactional
@@ -33,7 +38,10 @@ public class RecordingService {
         checkBookingValid(bookingId);
 
         return recordingRepository
-            .findByIdAndCaptureSession_Booking_Id(recordingId, bookingId)
+            .findByIdAndCaptureSession_Booking_IdAndDeletedAtIsNullAndCaptureSessionDeletedAtIsNull(
+                recordingId,
+                bookingId
+            )
             .map(RecordingDTO::new)
             .orElse(null);
     }
@@ -50,10 +58,52 @@ public class RecordingService {
     }
 
     @Transactional
+    @SuppressWarnings("PMD.CyclomaticComplexity")
+    public UpsertResult upsert(UUID bookingId, RecordingDTO recordingDto) {
+        checkBookingValid(bookingId);
+
+        var isUpdate = recordingRepository.existsByIdAndDeletedAtIsNull(recordingDto.getId());
+
+        var captureSession = captureSessionRepository.findByIdAndBooking_IdAndDeletedAtIsNull(
+            recordingDto.getCaptureSessionId(),
+            bookingId
+        );
+
+        if (!isUpdate && captureSession.isEmpty()) {
+            throw new NotFoundException("CaptureSession: " + recordingDto.getCaptureSessionId());
+        }
+
+        var recordingEntity = new Recording();
+        recordingEntity.setId(recordingDto.getId());
+        if (recordingDto.getParentRecordingId() != null) {
+            var parentRecording = recordingRepository.findById(recordingDto.getParentRecordingId());
+            if (parentRecording.isEmpty()) {
+                throw new NotFoundException("Recording: " + recordingDto.getParentRecordingId());
+            }
+            recordingEntity.setParentRecording(parentRecording.get());
+        } else {
+            recordingEntity.setParentRecording(null);
+        }
+        recordingEntity.setVersion(recordingDto.getVersion());
+        recordingEntity.setUrl(recordingDto.getUrl());
+        recordingEntity.setFilename(recordingDto.getFilename());
+        recordingEntity.setDuration(recordingDto.getDuration());
+        recordingEntity.setEditInstruction(recordingDto.getEditInstructions());
+
+        recordingRepository.save(recordingEntity);
+
+        return isUpdate ? UpsertResult.UPDATED : UpsertResult.CREATED;
+    }
+
+    @Transactional
     public void deleteById(UUID bookingId, UUID recordingId) {
         checkBookingValid(bookingId);
 
-        var recording = recordingRepository.findByIdAndCaptureSession_Booking_Id(recordingId, bookingId);
+        var recording = recordingRepository
+            .findByIdAndCaptureSession_Booking_IdAndDeletedAtIsNullAndCaptureSessionDeletedAtIsNull(
+                recordingId,
+                bookingId
+            );
 
         if (recording.isEmpty() || recording.get().isDeleted()) {
             throw new NotFoundException("Recording: " + recordingId);
@@ -65,7 +115,7 @@ public class RecordingService {
     }
 
     private void checkBookingValid(UUID bookingId) {
-        if (!bookingRepository.existsById(bookingId)) {
+        if (!bookingRepository.existsByIdAndDeletedAtIsNull(bookingId)) {
             throw new NotFoundException("Booking: " + bookingId);
         }
     }
