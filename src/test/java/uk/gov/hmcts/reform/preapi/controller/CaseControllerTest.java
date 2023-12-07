c36253ef8602d8fbae6f50b3e2de754fd2543a29
lucas-phillips28 <lucas.phillips@hmcts.net>
2023-12-07 11:41:30 +0000

    Fix and update tests for upsert cases endpoint

--- src/test/java/uk/gov/hmcts/reform/preapi/controller/CaseControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/controller/CaseControllerTest.java
@@ -13,13 +13,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.reform.preapi.controllers.CaseController;
 import uk.gov.hmcts.reform.preapi.dto.CaseDTO;
-import uk.gov.hmcts.reform.preapi.exception.ConflictException;
+import uk.gov.hmcts.reform.preapi.enums.UpsertResult;
 import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
 import uk.gov.hmcts.reform.preapi.repositories.CaseRepository;
 import uk.gov.hmcts.reform.preapi.services.CaseService;
 
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
@@ -74,25 +72,11 @@ class CaseControllerTest {
     @Test
     void testGetNonExistingCaseById() throws Exception {
         UUID caseId = UUID.randomUUID();
-        when(caseService.findById(caseId)).thenReturn(null);
+        doThrow(new NotFoundException("Case: " + caseId)).when(caseService).findById(any());
 
         mockMvc.perform(get(CASES_ID_PATH, caseId))
             .andExpect(status().isNotFound())
-            .andExpect(jsonPath("$.message").value("Not found: CaseDTO: " + caseId));
-    }
-
-    @DisplayName("Should return 404 when trying to get a case that doesn't exist")
-    @Test
-    void testGetDeletedCaseById() throws Exception {
-        UUID caseId = UUID.randomUUID();
-        CaseDTO mockCaseDTO = new CaseDTO();
-        mockCaseDTO.setId(caseId);
-        mockCaseDTO.setDeletedAt(Timestamp.from(Instant.now()));
-        when(caseService.findById(caseId)).thenReturn(mockCaseDTO);
-
-        mockMvc.perform(get(CASES_ID_PATH, caseId))
-            .andExpect(status().isNotFound())
-            .andExpect(jsonPath("$.message").value("Not found: CaseDTO: " + caseId));
+            .andExpect(jsonPath("$.message").value("Not found: Case: " + caseId));
     }
 
     @DisplayName("Should get list of cases with 200 response code")
@@ -119,17 +103,38 @@ class CaseControllerTest {
     @Test
     void testCreateCase() throws Exception {
         UUID caseId = UUID.randomUUID();
-        CaseDTO newCaseRequestDTO = new CaseDTO();
-        newCaseRequestDTO.setId(caseId);
+        CaseDTO caseDTO = new CaseDTO();
+        caseDTO.setId(caseId);
 
-        when(caseService.findById(caseId)).thenReturn(newCaseRequestDTO);
+        when(caseService.upsert(caseDTO)).thenReturn(UpsertResult.CREATED);
 
         MvcResult response = mockMvc.perform(put(CASES_ID_PATH, caseId)
                                                  .with(csrf())
-                                                 .content(OBJECT_MAPPER.writeValueAsString(newCaseRequestDTO))
+                                                 .content(OBJECT_MAPPER.writeValueAsString(caseDTO))
                                                  .contentType(MediaType.APPLICATION_JSON_VALUE)
                                                  .accept(MediaType.APPLICATION_JSON_VALUE))
             .andExpect(status().isCreated())
+            .andReturn();
+
+        assertThat(response.getResponse().getContentAsString()).isEqualTo("");
+        assertThat(response.getResponse().getHeaderValue("Location")).isEqualTo(TEST_URL + "/cases/" + caseId);
+    }
+
+    @DisplayName("Should update case with 204 response code")
+    @Test
+    void testUpdateCase() throws Exception {
+        UUID caseId = UUID.randomUUID();
+        CaseDTO caseDTO = new CaseDTO();
+        caseDTO.setId(caseId);
+
+        when(caseService.upsert(caseDTO)).thenReturn(UpsertResult.UPDATED);
+
+        MvcResult response = mockMvc.perform(put(CASES_ID_PATH, caseId)
+                                                 .with(csrf())
+                                                 .content(OBJECT_MAPPER.writeValueAsString(caseDTO))
+                                                 .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                                 .accept(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(status().isNoContent())
             .andReturn();
 
         assertThat(response.getResponse().getContentAsString()).isEqualTo("");
@@ -150,7 +155,7 @@ class CaseControllerTest {
                             .accept(MediaType.APPLICATION_JSON_VALUE))
             .andExpect(status().isBadRequest())
             .andExpect(jsonPath("$.message")
-                           .value("Path id does not match payload property newCaseRequestDTO.id"));
+                           .value("Path id does not match payload property caseDTO.id"));
     }
 
     @DisplayName("Should return 400 when creating case with court that does not exist")
@@ -162,7 +167,7 @@ class CaseControllerTest {
         newCaseRequestDTO.setId(caseId);
         newCaseRequestDTO.setCourtId(courtId);
 
-        doThrow(new NotFoundException("Court: " + courtId)).when(caseService).create(newCaseRequestDTO);
+        doThrow(new NotFoundException("Court: " + courtId)).when(caseService).upsert(newCaseRequestDTO);
 
 
         mockMvc.perform(put(CASES_ID_PATH, caseId)
@@ -173,25 +178,6 @@ class CaseControllerTest {
             .andExpect(status().isNotFound())
             .andExpect(jsonPath("$.message")
                            .value("Not found: Court: " + courtId));
-    }
-
-    @DisplayName("Should return 409 when creating case with an id that is already in use")
-    @Test
-    void testCreateCaseAlreadyExists() throws Exception {
-        UUID caseId = UUID.randomUUID();
-        CaseDTO newCaseRequestDTO = new CaseDTO();
-        newCaseRequestDTO.setId(caseId);
-
-        doThrow(new ConflictException(caseId.toString())).when(caseService).create(any());
-
-        mockMvc.perform(put(CASES_ID_PATH, caseId)
-                            .with(csrf())
-                            .content(OBJECT_MAPPER.writeValueAsString(newCaseRequestDTO))
-                            .contentType(MediaType.APPLICATION_JSON_VALUE)
-                            .accept(MediaType.APPLICATION_JSON_VALUE))
-            .andExpect(status().isConflict())
-            .andExpect(jsonPath("$.message")
-                           .value("Put Conflict: " + caseId + " already exists"));
     }
 
     @DisplayName("Should delete case with 200 response code")
