d5a3eb64d0eb99c4f66493f0c93bf475250b374c
Jason Paige <jason.r.paige@gmail.com>
2023-12-11 12:01:00 +0000

    Enable jpa repo annotation

--- src/main/java/uk/gov/hmcts/reform/preapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/Application.java
@@ -2,8 +2,12 @@ package uk.gov.hmcts.reform.preapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
