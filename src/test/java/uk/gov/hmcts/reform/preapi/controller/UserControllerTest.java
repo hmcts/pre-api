b2e646400dbde405e9da8dde0fe4c6f84de60ac5
lucas-phillips28 <lucas.phillips@hmcts.net>
2023-12-15 11:56:33 +0000

    Tests for new endpoint

--- src/test/java/uk/gov/hmcts/reform/preapi/controller/UserControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/controller/UserControllerTest.java
@@ -17,8 +17,11 @@ import uk.gov.hmcts.reform.preapi.services.UserService;
 
 import java.util.UUID;
 
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -65,5 +68,31 @@ public class UserControllerTest {
         mockMvc.perform(get("/users/" + userId))
             .andExpect(status().isNotFound())
             .andExpect(jsonPath("$.message").value("Not found: User: " + userId));
+    }
+
+    @DisplayName("Should delete user with 200 response code")
+    @Test
+    void deleteUserByIdSuccess() throws Exception {
+        var userId = UUID.randomUUID();
+        doNothing().when(userService).deleteById(userId);
+
+        mockMvc.perform(delete("/users/" + userId)
+                            .with(csrf()))
+            .andExpect(status().isOk());
+    }
+
+    @DisplayName("Should return 404 when user doesn't exist")
+    @Test
+    void deleteUserByIdNotFound() throws Exception {
+        var userId = UUID.randomUUID();
+        doThrow(new NotFoundException("User: " + userId))
+            .when(userService)
+            .deleteById(userId);
+
+        mockMvc.perform(delete("/users/" + userId)
+                            .with(csrf()))
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.message")
+                           .value("Not found: User: " + userId));
     }
 }
--- src/test/java/uk/gov/hmcts/reform/preapi/services/UserServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/services/UserServiceTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.preapi.services;
 
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +10,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.reform.preapi.entities.AppAccess;
 import uk.gov.hmcts.reform.preapi.entities.Court;
+import uk.gov.hmcts.reform.preapi.entities.PortalAccess;
 import uk.gov.hmcts.reform.preapi.entities.Role;
 import uk.gov.hmcts.reform.preapi.entities.User;
 import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
 import uk.gov.hmcts.reform.preapi.repositories.AppAccessRepository;
+import uk.gov.hmcts.reform.preapi.repositories.PortalAccessRepository;
+import uk.gov.hmcts.reform.preapi.repositories.UserRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -28,8 +34,16 @@ public class UserServiceTest {
     private static User userEntity;
     private static AppAccess appAccessEntity;
 
+    private static PortalAccess portalAccessEntity;
+
     @MockBean
     private AppAccessRepository appAccessRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private PortalAccessRepository portalAccessRepository;
 
     @Autowired
     private UserService userService;
@@ -54,6 +68,17 @@ public class UserServiceTest {
         appAccessEntity.setCourt(court);
         appAccessEntity.setRole(role);
         appAccessEntity.setActive(true);
+
+        portalAccessEntity = new PortalAccess();
+        portalAccessEntity.setId(UUID.randomUUID());
+        portalAccessEntity.setUser(userEntity);
+    }
+
+    @BeforeEach
+    void reset() {
+        userEntity.setDeletedAt(null);
+        appAccessEntity.setDeletedAt(null);
+        portalAccessEntity.setDeletedAt(null);
     }
 
     @DisplayName("Find a user by it's id and return a model")
@@ -83,5 +108,62 @@ public class UserServiceTest {
         );
 
         verify(appAccessRepository, times(1)).findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId());
+    }
+
+    @DisplayName("Delete a user by it's id")
+    @Test
+    void deleteUserByIdSuccess() {
+        when(userRepository.existsByIdAndDeletedAtIsNull(userEntity.getId())).thenReturn(true);
+        when(portalAccessRepository.findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId()))
+            .thenReturn(Optional.of(portalAccessEntity));
+        when(appAccessRepository.findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId()))
+            .thenReturn(Optional.of(appAccessEntity));
+
+        userService.deleteById(userEntity.getId());
+
+        verify(userRepository, times(1)).existsByIdAndDeletedAtIsNull(userEntity.getId());
+        verify(portalAccessRepository, times(1)).findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId());
+        verify(portalAccessRepository, times(1)).deleteById(portalAccessEntity.getId());
+        verify(appAccessRepository, times(1)).findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId());
+        verify(appAccessRepository, times(1)).deleteById(appAccessEntity.getId());
+        verify(userRepository, times(1)).deleteById(userEntity.getId());
+    }
+
+    @DisplayName("Delete a user by it's id when user is not attached to portal access or app access")
+    @Test
+    void deleteUserByIdNoAccessSuccess() {
+        when(userRepository.existsByIdAndDeletedAtIsNull(userEntity.getId())).thenReturn(true);
+        when(portalAccessRepository.findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId()))
+            .thenReturn(Optional.empty());
+        when(appAccessRepository.findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId()))
+            .thenReturn(Optional.empty());
+
+        userService.deleteById(userEntity.getId());
+
+        verify(userRepository, times(1)).existsByIdAndDeletedAtIsNull(userEntity.getId());
+        verify(portalAccessRepository, times(1)).findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId());
+        verify(portalAccessRepository, never()).deleteById(portalAccessEntity.getId());
+        verify(appAccessRepository, times(1)).findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId());
+        verify(appAccessRepository, never()).deleteById(appAccessEntity.getId());
+        verify(userRepository, times(1)).deleteById(userEntity.getId());
+    }
+
+    @DisplayName("Delete a user when id doesn't exist")
+    @Test
+    void deleteUserByIdNotFound() {
+        UUID userId = UUID.randomUUID();
+        when(userRepository.existsByIdAndDeletedAtIsNull(userId)).thenReturn(false);
+
+        assertThrows(
+            NotFoundException.class,
+            () -> userService.deleteById(userId)
+        );
+
+        verify(userRepository, times(1)).existsByIdAndDeletedAtIsNull(userId);
+        verify(portalAccessRepository, never()).findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId());
+        verify(portalAccessRepository, never()).deleteById(portalAccessEntity.getId());
+        verify(appAccessRepository, never()).findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userEntity.getId());
+        verify(appAccessRepository, never()).deleteById(appAccessEntity.getId());
+        verify(userRepository, never()).deleteById(userEntity.getId());
     }
 }
