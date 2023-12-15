8e92606205f03bb337465d0886fc0906ca4635bd
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2023-12-15 11:48:55 +0000

    S28 2264: GET `/users` (#136)
    * Add endpoint GET `/users`
    
    * Tests for new endpoint
    
    * Styling
    
    * Add operation annotation to new endpoint

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/UserController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/UserController.java
@@ -7,10 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.preapi.dto.UserDTO;
 import uk.gov.hmcts.reform.preapi.services.UserService;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -28,5 +30,22 @@ public class UserController {
     @Operation(operationId = "getUserById", summary = "get a User by Id")
     public ResponseEntity<UserDTO> getUserById(@PathVariable UUID userId) {
         return ResponseEntity.ok(userService.findById(userId));
+    }
+
+    @GetMapping
+    @Operation(
+        operationId = "getUsers",
+        summary = "Search for Users by first name, last name, email, organisation, court or role"
+    )
+    @SuppressWarnings("PMD.UseObejctForClearerAPI")
+    public ResponseEntity<List<UserDTO>> getUsers(
+        @RequestParam(required = false) String firstName,
+        @RequestParam(required = false) String lastName,
+        @RequestParam(required = false) String email,
+        @RequestParam(required = false) String organisation,
+        @RequestParam(required = false) UUID courtId,
+        @RequestParam(required = false) UUID roleId
+    ) {
+        return ResponseEntity.ok(userService.findAllBy(firstName, lastName, email, organisation, courtId, roleId));
     }
 }
