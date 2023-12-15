6e140e1cc8243586950b2e12c2a947c0e0cca457
lucas-phillips28 <lucas.phillips@hmcts.net>
2023-12-15 10:40:00 +0000

    Add endpoint GET `/users`

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/UserController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/UserController.java
@@ -6,10 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.preapi.dto.UserDTO;
 import uk.gov.hmcts.reform.preapi.services.UserService;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -26,5 +28,17 @@ public class UserController {
     @GetMapping("/{userId}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable UUID userId) {
         return ResponseEntity.ok(userService.findById(userId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getUsers(
+        @RequestParam(required = false) String firstName,
+        @RequestParam(required = false) String lastName,
+        @RequestParam(required = false) String email,
+        @RequestParam(required = false) String organisation,
+        @RequestParam(required = false) UUID courtId,
+        @RequestParam(required = false) UUID roleId
+    ) {
+        return ResponseEntity.ok(userService.findAllBy(firstName, lastName, email, organisation, courtId, roleId));
     }
 }
