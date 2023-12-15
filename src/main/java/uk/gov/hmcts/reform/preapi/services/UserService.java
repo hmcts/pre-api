bf2078cf2fd4b60b79f5e07ba827cb86fafa0015
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2023-12-15 11:48:55 +0000

    S28 2264: GET `/users` (#136)
    * Add endpoint GET `/users`
    
    * Tests for new endpoint
    
    * Styling
    
    * Add operation annotation to new endpoint

--- src/main/java/uk/gov/hmcts/reform/preapi/services/UserService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/UserService.java
@@ -6,16 +6,26 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.preapi.dto.UserDTO;
 import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
 import uk.gov.hmcts.reform.preapi.repositories.AppAccessRepository;
+import uk.gov.hmcts.reform.preapi.repositories.CourtRepository;
+import uk.gov.hmcts.reform.preapi.repositories.RoleRepository;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
     private final AppAccessRepository appAccessRepository;
+    private final CourtRepository courtRepository;
+    private final RoleRepository roleRepository;
 
-    public UserService(AppAccessRepository appAccessRepository) {
+    public UserService(AppAccessRepository appAccessRepository,
+                       CourtRepository courtRepository,
+                       RoleRepository roleRepository) {
         this.appAccessRepository = appAccessRepository;
+        this.courtRepository = courtRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Transactional
@@ -23,5 +33,29 @@ public class UserService {
         return appAccessRepository.findByUser_IdAndDeletedAtNullAndUser_DeletedAtNull(userId)
             .map(UserDTO::new)
             .orElseThrow(() -> new NotFoundException("User: " + userId));
+    }
+
+    @Transactional
+    @SuppressWarnings("PMD.UseObjectForClearerAPI")
+    public List<UserDTO> findAllBy(
+        String firstName,
+        String lastName,
+        String email,
+        String organisation,
+        UUID court,
+        UUID role
+    ) {
+        if (court != null && !courtRepository.existsById(court)) {
+            throw new NotFoundException("Court: " + court);
+        }
+
+        if (role != null && !roleRepository.existsById(role)) {
+            throw new NotFoundException("Role: " + role);
+        }
+
+        return appAccessRepository.searchAllBy(firstName, lastName, email, organisation, court, role)
+            .stream()
+            .map(UserDTO::new)
+            .collect(Collectors.toList());
     }
 }
