4d0914bc546430cd2160e232bf2d92bf47810a24
mazzopardi2 <marianne.azzopardi@hmcts.net>
2024-01-03 11:29:08 +0000

    using regex to match courts in grouplist to location code dict and room dicts

--- bin/migration/tables/courts.py
+++ bin/migration/tables/courts.py
@@ -1,6 +1,6 @@
 import uuid
 from .helpers import audit_entry_creation, check_existing_record, log_failed_imports
-
+import re
 
 class CourtManager:
     def __init__(self, source_cursor):
@@ -8,29 +8,40 @@ class CourtManager:
         self.failed_imports = set()
 
     def get_data(self):
-        self.source_cursor.execute("SELECT * from public.courts")
+        self.source_cursor.execute("SELECT groupid, groupname from public.grouplist where grouptype = 'Location'")
         return self.source_cursor.fetchall()
 
-    def migrate_data(self, destination_cursor, source_courts_data):
+    def migrate_data(self, destination_cursor, source_courts_data): 
         # Courts data -  https://cjscommonplatform-my.sharepoint.com/:x:/r/personal/lawrie_baber-scovell2_hmcts_net/_layouts/15/Doc.aspx?sourcedoc=%7B07C83A7F-EF01-4C78-9B02-AEDD443D15A1%7D&file=Courts%20PRE%20NRO.xlsx&wdOrigin=TEAMS-WEB.undefined_ns.rwc&action=default&mobileredirect=true
         court_types = {
             'Reading Crown Court': ('CROWN','449','UKJ-South East (England)'),
             'Nottingham Crown Court': ('CROWN','444','UKF-East Midlands (England)'),
-            'Mold Crown Court': ('CROWN','438','UKL-Wales'),
+            'Mold Crown': ('CROWN','438','UKL-Wales'),
             'Liverpool Crown Court': ('CROWN','433','UKD-North West (England)'),
             'Leeds Youth Court': ('MAGISTRATE','429','UKE-Yorkshire and The Humber'),
             'Leeds Crown Court': ('CROWN','429','UKE-Yorkshire and The Humber'),
             'Kingston-upon-Thames Crown Court': ('CROWN','427','UKI-London'),
             'Durham Crown Court': ('CROWN','422','UKC-North East (England)'),
-            'Birmingham': ('CROWN','404','UKG-West Midlands (England)')
+            'Birmingham Crown Court': ('CROWN','404','UKG-West Midlands (England)')
         }
         batch_courts_data = []
 
         for court in source_courts_data:
             id = court[0]
-            court_info = court_types.get(court[1], ('CROWN', 'Unknown', 'Unknown'))
-            court_type, location_code, _ = court_info
             name = court[1]
+            court_info = None
+
+            for court_pattern, info in court_types.items():
+                regex_pattern = re.compile(rf"{re.escape(court_pattern)}", re.IGNORECASE)
+                if re.search(regex_pattern, name):
+                    court_info = info
+                    break 
+                
+                if court_info is None:
+                    court_info = ('CROWN', 'Unknown', 'Unknown')
+
+            court_type, location_code, _ = court_info
+            
 
             if not check_existing_record(destination_cursor,'courts', 'id', id ):
                 batch_courts_data.append((id, court_type.upper(), name, location_code))
