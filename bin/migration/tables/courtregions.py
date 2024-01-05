a7283db10d5d1cf5358ab22ea248c4e5d9081e4f
mazzopardi2 <marianne.azzopardi@hmcts.net>
2024-01-05 16:50:48 +0000

    updating court regions dict

--- bin/migration/tables/courtregions.py
+++ bin/migration/tables/courtregions.py
@@ -12,13 +12,14 @@ class CourtRegionManager:
         # Court regions data -  https://cjscommonplatform-my.sharepoint.com/:x:/r/personal/lawrie_baber-scovell2_hmcts_net/_layouts/15/Doc.aspx?sourcedoc=%7B07C83A7F-EF01-4C78-9B02-AEDD443D15A1%7D&file=Courts%20PRE%20NRO.xlsx&wdOrigin=TEAMS-WEB.undefined_ns.rwc&action=default&mobileredirect=true
         court_regions = [
             {"name": "Birmingham", "region": "West Midlands (England)"},
+            {"name": "Birmingham Youth", "region": "West Midlands (England)"},
             {"name": "Mold Crown Court", "region": "Wales"},
             {"name": "Reading Crown Court", "region": "South East (England)"},
             {"name": "Leeds Crown Court", "region": "Yorkshire and The Humber"},
             {"name": "Durham Crown Court", "region": "North East (England)"},
             {"name": "Liverpool Crown Court", "region": "North West (England)"},
             {"name": "Nottingham Crown Court", "region": "East Midlands (England)"},
-            {"name": "Kingston-upon-Thames Crown Court", "region": "London"},
+            {"name": "Kingston upon Thames Crown Court", "region": "London"},
             {"name": "Leeds Youth Court", "region": "Yorkshire and The Humber"},
             {"name": "Default Court", "region": "London"}
         ]
