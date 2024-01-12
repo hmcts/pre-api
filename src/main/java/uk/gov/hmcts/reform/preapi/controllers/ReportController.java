893769e647d00e976a54cc94520144b607594000
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-01-12 10:10:36 +0000

    S28 2317: API Report concurrent capture sessions to replace `v_concurrent_recording_report` (#165)
    * Add capture session report dto
    
    * Constructor for dto
    
    * Add report endpoint for to replace old view: `v_concurrent_recording_report` -> `GET /reports/capture-session-concurrent`
    
    * Update Swagger v2 Spec
    
    * Update Swagger v2 Spec
    
    ---------
    
    Co-authored-by: Swagger V2 bot <jasonpaige@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/ReportController.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.preapi.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.preapi.dto.reports.CaptureSessionReportDTO;
+import uk.gov.hmcts.reform.preapi.services.ReportService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reports")
+public class ReportController {
+
+    private final ReportService reportService;
+
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @GetMapping("/capture-sessions-concurrent")
+    @Operation(operationId = "reportConcurrentCaptureSessions")
+    public ResponseEntity<List<CaptureSessionReportDTO>> reportConcurrentCaptureSessions() {
+        return ResponseEntity.ok(reportService.reportCaptureSessions());
+    }
+}
