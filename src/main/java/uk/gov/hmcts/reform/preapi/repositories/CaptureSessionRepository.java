f7fa130e5a5e264357b82615b9f08f58018c1139
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-01-15 09:51:07 +0000

    S28 2318: API Report Recordings Per Case (#166)
    Co-authored-by: Swagger V2 bot <jasonpaige@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/preapi/repositories/CaptureSessionRepository.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/repositories/CaptureSessionRepository.java
@@ -4,6 +4,7 @@ package uk.gov.hmcts.reform.preapi.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.preapi.entities.CaptureSession;
+import uk.gov.hmcts.reform.preapi.enums.RecordingStatus;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -12,4 +13,6 @@ import java.util.UUID;
 @SuppressWarnings("PMD.MethodNamingConventions")
 public interface CaptureSessionRepository extends JpaRepository<CaptureSession, UUID> {
     Optional<CaptureSession> findByIdAndDeletedAtIsNull(UUID captureSessionId);
+
+    int countAllByBooking_CaseId_IdAndStatus(UUID caseId, RecordingStatus status);
 }
