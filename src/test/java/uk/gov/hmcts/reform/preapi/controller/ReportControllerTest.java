b2d6c2be8dd9ea40214b9c2c3f851010e3b1ed22
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-01-15 16:49:27 +0000

    S28 2368: API Report Bookings Shared (#170)
    Co-authored-by: Swagger V2 bot <jasonpaige@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/preapi/controller/ReportControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/controller/ReportControllerTest.java
@@ -12,6 +12,7 @@ import uk.gov.hmcts.reform.preapi.controllers.ReportController;
 import uk.gov.hmcts.reform.preapi.dto.reports.CaptureSessionReportDTO;
 import uk.gov.hmcts.reform.preapi.dto.reports.EditReportDTO;
 import uk.gov.hmcts.reform.preapi.dto.reports.RecordingsPerCaseReportDTO;
+import uk.gov.hmcts.reform.preapi.dto.reports.SharedReportDTO;
 import uk.gov.hmcts.reform.preapi.services.ReportService;
 
 import java.sql.Timestamp;
@@ -90,5 +91,28 @@ public class ReportControllerTest {
             .andExpect(jsonPath("$[0].case_reference").value(reportItem.getCaseReference()))
             .andExpect(jsonPath("$[0].version").value(reportItem.getVersion()))
             .andExpect(jsonPath("$[0].recording_id").value(reportItem.getRecordingId().toString()));
+    }
+
+    @DisplayName("Should get a report containing a list of details relating to shared bookings")
+    @Test
+    void reportBookingsSharedSuccess() throws Exception {
+        var reportItem = new SharedReportDTO();
+        reportItem.setSharedAt(Timestamp.from(Instant.now()));
+        reportItem.setAllocatedTo("example1@example.com");
+        reportItem.setAllocatedBy("example2@example.com");
+        reportItem.setCaseReference("ABC123");
+        reportItem.setCourt("Example Court");
+        reportItem.setRegions(Set.of());
+        reportItem.setBookingId(UUID.randomUUID());
+
+        when(reportService.reportShared()).thenReturn(List.of(reportItem));
+        mockMvc.perform(get("/reports/shared-bookings"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$[0].allocated_to").value(reportItem.getAllocatedTo()))
+            .andExpect(jsonPath("$[0].allocated_by").value(reportItem.getAllocatedBy()))
+            .andExpect(jsonPath("$[0].case_reference").value(reportItem.getCaseReference()))
+            .andExpect(jsonPath("$[0].court").value(reportItem.getCourt()))
+            .andExpect(jsonPath("$[0].booking_id").value(reportItem.getBookingId().toString()));
     }
 }
