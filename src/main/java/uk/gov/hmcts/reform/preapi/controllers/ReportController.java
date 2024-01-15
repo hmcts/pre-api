ed2205783f5fbee5c39c7f17ac3da90c16bf7de0
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-01-15 16:49:27 +0000

    S28 2368: API Report Bookings Shared (#170)
    Co-authored-by: Swagger V2 bot <jasonpaige@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/ReportController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/ReportController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.preapi.dto.reports.CaptureSessionReportDTO;
 import uk.gov.hmcts.reform.preapi.dto.reports.EditReportDTO;
 import uk.gov.hmcts.reform.preapi.dto.reports.RecordingsPerCaseReportDTO;
+import uk.gov.hmcts.reform.preapi.dto.reports.SharedReportDTO;
 import uk.gov.hmcts.reform.preapi.services.ReportService;
 
 import java.util.List;
@@ -41,5 +42,11 @@ public class ReportController {
     @Operation(operationId = "reportEdits", summary = "Get a report on recordings edits")
     public ResponseEntity<List<EditReportDTO>> reportEdits() {
         return ResponseEntity.ok(reportService.reportEdits());
+    }
+
+    @GetMapping("/shared-bookings")
+    @Operation(operationId = "reportBookingsShared", summary = "Get a report on the bookings that have been shared")
+    public ResponseEntity<List<SharedReportDTO>> reportBookingsShared() {
+        return ResponseEntity.ok(reportService.reportShared());
     }
 }
--- src/main/java/uk/gov/hmcts/reform/preapi/services/ReportService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/ReportService.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.preapi.dto.reports.CaptureSessionReportDTO;
 import uk.gov.hmcts.reform.preapi.dto.reports.EditReportDTO;
 import uk.gov.hmcts.reform.preapi.dto.reports.RecordingsPerCaseReportDTO;
+import uk.gov.hmcts.reform.preapi.dto.reports.SharedReportDTO;
 import uk.gov.hmcts.reform.preapi.enums.RecordingStatus;
 import uk.gov.hmcts.reform.preapi.repositories.CaptureSessionRepository;
 import uk.gov.hmcts.reform.preapi.repositories.CaseRepository;
 import uk.gov.hmcts.reform.preapi.repositories.RecordingRepository;
+import uk.gov.hmcts.reform.preapi.repositories.ShareBookingRepository;
 
 import java.util.Comparator;
 import java.util.List;
@@ -21,14 +23,17 @@ public class ReportService {
     private final CaptureSessionRepository captureSessionRepository;
     private final RecordingRepository recordingRepository;
     private final CaseRepository caseRepository;
+    private final ShareBookingRepository shareBookingRepository;
 
     @Autowired
     public ReportService(CaptureSessionRepository captureSessionRepository,
                          RecordingRepository recordingRepository,
-                         CaseRepository caseRepository) {
+                         CaseRepository caseRepository,
+                         ShareBookingRepository shareBookingRepository) {
         this.captureSessionRepository = captureSessionRepository;
         this.recordingRepository = recordingRepository;
         this.caseRepository = caseRepository;
+        this.shareBookingRepository = shareBookingRepository;
     }
 
     @Transactional
@@ -64,6 +69,16 @@ public class ReportService {
             .stream()
             .map(EditReportDTO::new)
             .sorted(Comparator.comparing(EditReportDTO::getCreatedAt))
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<SharedReportDTO> reportShared() {
+        return shareBookingRepository
+            .findAll()
+            .stream()
+            .map(SharedReportDTO::new)
+            .sorted(Comparator.comparing(SharedReportDTO::getSharedAt))
             .collect(Collectors.toList());
     }
 }
