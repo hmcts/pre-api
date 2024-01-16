dc1c70507d7a76dc0dfcb0ce2197861c1047acb7
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-01-16 09:07:49 +0000

    S28 2369: API Report Playback (Portal/Application) (#173)
    Co-authored-by: Swagger V2 bot <jasonpaige@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/preapi/repositories/AuditRepository.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/repositories/AuditRepository.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.reform.preapi.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.preapi.entities.Audit;
+import uk.gov.hmcts.reform.preapi.enums.AuditLogSource;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AuditRepository extends JpaRepository<Audit, UUID> {
+    List<Audit> findBySourceAndFunctionalAreaAndActivity(AuditLogSource source, String functionalArea, String activity);
 }
--- src/main/java/uk/gov/hmcts/reform/preapi/services/ReportService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/ReportService.java
@@ -5,14 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.preapi.dto.reports.CaptureSessionReportDTO;
 import uk.gov.hmcts.reform.preapi.dto.reports.EditReportDTO;
+import uk.gov.hmcts.reform.preapi.dto.reports.PlaybackReportDTO;
 import uk.gov.hmcts.reform.preapi.dto.reports.RecordingsPerCaseReportDTO;
 import uk.gov.hmcts.reform.preapi.dto.reports.ScheduleReportDTO;
 import uk.gov.hmcts.reform.preapi.dto.reports.SharedReportDTO;
+import uk.gov.hmcts.reform.preapi.entities.User;
+import uk.gov.hmcts.reform.preapi.enums.AuditLogSource;
 import uk.gov.hmcts.reform.preapi.enums.RecordingStatus;
+import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
+import uk.gov.hmcts.reform.preapi.repositories.AuditRepository;
 import uk.gov.hmcts.reform.preapi.repositories.CaptureSessionRepository;
 import uk.gov.hmcts.reform.preapi.repositories.CaseRepository;
 import uk.gov.hmcts.reform.preapi.repositories.RecordingRepository;
 import uk.gov.hmcts.reform.preapi.repositories.ShareBookingRepository;
+import uk.gov.hmcts.reform.preapi.repositories.UserRepository;
 
 import java.util.Comparator;
 import java.util.List;
@@ -25,16 +31,22 @@ public class ReportService {
     private final RecordingRepository recordingRepository;
     private final CaseRepository caseRepository;
     private final ShareBookingRepository shareBookingRepository;
+    private final AuditRepository auditRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public ReportService(CaptureSessionRepository captureSessionRepository,
                          RecordingRepository recordingRepository,
                          CaseRepository caseRepository,
-                         ShareBookingRepository shareBookingRepository) {
+                         ShareBookingRepository shareBookingRepository,
+                         AuditRepository auditRepository,
+                         UserRepository userRepository) {
         this.captureSessionRepository = captureSessionRepository;
         this.recordingRepository = recordingRepository;
         this.caseRepository = caseRepository;
         this.shareBookingRepository = shareBookingRepository;
+        this.auditRepository = auditRepository;
+        this.userRepository = userRepository;
     }
 
     @Transactional
@@ -91,5 +103,38 @@ public class ReportService {
             .map(ScheduleReportDTO::new)
             .sorted(Comparator.comparing(ScheduleReportDTO::getScheduledFor))
             .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<PlaybackReportDTO> reportPlayback(AuditLogSource source) {
+        if (source == AuditLogSource.PORTAL || source == AuditLogSource.APPLICATION) {
+            final var activityPlay = "Play";
+            final var functionalAreaVideoPlayer = "Video Player";
+            final var functionalAreaViewRecordings = "View Recordings";
+
+            return auditRepository
+                .findBySourceAndFunctionalAreaAndActivity(
+                    source,
+                    source == AuditLogSource.PORTAL
+                        ? functionalAreaVideoPlayer
+                        : functionalAreaViewRecordings,
+                    activityPlay
+                )
+                .stream()
+                .map(a ->
+                         new PlaybackReportDTO(
+                             a,
+                             userRepository
+                                 .findById(a.getCreatedBy())
+                                 .map(User::getEmail)
+                                 .orElse(null),
+                             recordingRepository
+                                 .findById(a.getTableRecordId())
+                                 .orElse(null))
+                )
+                .collect(Collectors.toList());
+        } else {
+            throw new NotFoundException("Report for playback source: " + source);
+        }
     }
 }
