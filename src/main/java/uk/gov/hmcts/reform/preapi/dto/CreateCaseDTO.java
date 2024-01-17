c386e686c413efb9c0d7ca399ab42d6cb0dd6900
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-01-17 09:41:39 +0000

    Fix: reference length limits

--- src/main/java/uk/gov/hmcts/reform/preapi/dto/CreateCaseDTO.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/dto/CreateCaseDTO.java
@@ -3,9 +3,9 @@ package uk.gov.hmcts.reform.preapi.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 import uk.gov.hmcts.reform.preapi.entities.Case;
 
 import java.util.Set;
@@ -26,7 +26,7 @@ public class CreateCaseDTO {
     private UUID courtId;
 
     @Schema(description = "CreateCaseReference")
-    @Length(min = 9, max = 13)
+    @Size(min = 9, max = 13)
     private String reference;
 
     @Schema(description = "CaseParticipants")
