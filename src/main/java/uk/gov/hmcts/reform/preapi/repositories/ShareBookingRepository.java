a5607ed34620bbc2e3014cb8f5c4cd98f9007761
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-01-17 14:38:26 +0000

    S28 2323: API Reports Recording Access Revoked (#180)
    Co-authored-by: Swagger V2 bot <jasonpaige@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/preapi/repositories/ShareBookingRepository.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/repositories/ShareBookingRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.preapi.entities.ShareBooking;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ShareBookingRepository extends JpaRepository<ShareBooking, UUID> {
-
+    List<ShareBooking> findAllByDeletedAtIsNotNull();
 }
