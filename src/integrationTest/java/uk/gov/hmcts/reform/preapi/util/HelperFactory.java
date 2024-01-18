23e33d92bcaec012d386755962988416b4eb8740
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-01-18 10:58:50 +0000

    Add IT

--- src/integrationTest/java/uk/gov/hmcts/reform/preapi/util/HelperFactory.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/preapi/util/HelperFactory.java
@@ -6,6 +6,7 @@ import uk.gov.hmcts.reform.preapi.entities.CaptureSession;
 import uk.gov.hmcts.reform.preapi.entities.Case;
 import uk.gov.hmcts.reform.preapi.entities.Court;
 import uk.gov.hmcts.reform.preapi.entities.Participant;
+import uk.gov.hmcts.reform.preapi.entities.Recording;
 import uk.gov.hmcts.reform.preapi.entities.Region;
 import uk.gov.hmcts.reform.preapi.entities.Role;
 import uk.gov.hmcts.reform.preapi.entities.Room;
@@ -17,6 +18,7 @@ import uk.gov.hmcts.reform.preapi.enums.RecordingStatus;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.Duration;
 import java.util.Set;
 import javax.annotation.Nullable;
 
@@ -167,5 +169,23 @@ public class HelperFactory {
         room.setName(name);
         room.setCourts(courts);
         return room;
+    }
+
+    public static Recording createRecording(
+        CaptureSession captureSession,
+        @Nullable Recording parentRecording,
+        int version,
+        String url,
+        String filename,
+        @Nullable Timestamp deletedAt
+    ) {
+        var recording = new Recording();
+        recording.setCaptureSession(captureSession);
+        recording.setParentRecording(parentRecording);
+        recording.setVersion(version);
+        recording.setUrl(url);
+        recording.setFilename(filename);
+        recording.setDeletedAt(deletedAt);
+        return recording;
     }
 }
