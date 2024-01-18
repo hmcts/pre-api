85920676694c3009266203d286f98f766a79b841
Jason Paige <jasonpaige@users.noreply.github.com>
2024-01-18 09:40:04 +0000

    Search for bookings by scheduled for date (#187)
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/params/SearchBookings.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/params/SearchBookings.java
@@ -2,10 +2,12 @@ package uk.gov.hmcts.reform.preapi.controllers.params;
 
 import lombok.Data;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Data
 public class SearchBookings {
     private UUID caseId;
     private String caseReference;
+    private Date scheduledFor;
 }
