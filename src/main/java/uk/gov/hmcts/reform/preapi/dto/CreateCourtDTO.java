b81fe20d023c2783bc971fb44ef42796c4234f15
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-01-19 13:36:30 +0000

    Fix: CreateCourtDTO Ensure Valid (#196)
    Co-authored-by: Swagger V2 bot <jasonpaige@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/preapi/dto/CreateCourtDTO.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/dto/CreateCourtDTO.java
@@ -4,6 +4,8 @@ package uk.gov.hmcts.reform.preapi.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.preapi.enums.CourtType;
@@ -17,20 +19,28 @@ import java.util.UUID;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CreateCourtDTO {
     @Schema(description = "CreateCourtId")
+    @NotNull(message = "id is required")
     private UUID id;
 
     @Schema(description = "CreateCourtName")
+    @NotNull(message = "name is required")
     private String name;
 
     @Schema(description = "CreateCourtType")
+    @NotNull(message = "court_type is required")
     private CourtType courtType;
 
     @Schema(description = "CreateCourtLocationCode")
+    @NotNull(message = "location_code is required")
     private String locationCode;
 
     @Schema(description = "CreateCourtRegionIds")
+    @Size(min = 1, message = "must contain at least 1")
+    @NotNull(message = "regions is required and must contain at least 1")
     private List<UUID> regions = List.of();
 
     @Schema(description = "CreateCourtRoomIds")
+    @Size(min = 1, message = "must contain at least 1")
+    @NotNull(message = "rooms is required and must contain at least 1")
     private List<UUID> rooms = List.of();
 }
