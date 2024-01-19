33811ffba5e6bf86abee192311f7c541216ec9dd
Jason Paige <jasonpaige@users.noreply.github.com>
2024-01-19 10:44:35 +0000

    Smoke test for APIm working with authentication enabled (#195)

--- src/smokeTest/java/uk/gov/hmcts/reform/preapi/controllers/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/preapi/controllers/SmokeTest.java
@@ -3,8 +3,10 @@ package uk.gov.hmcts.reform.preapi.controllers;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.given;
@@ -13,12 +15,22 @@ import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpStatus.OK;
 
 @ExtendWith(SpringExtension.class)
+@TestPropertySource("classpath:application.properties")
 class SmokeTest {
     private static final String CONTENT_TYPE_VALUE = "application/json";
     private static final String HEALTH_ENDPOINT = "/health";
 
     @Value("${TEST_URL:http://localhost:4550}")
     private String testUrl;
+
+    @Value("${apim.uri}")
+    private String apimUri;
+
+    @Value("${apim.subscription-key.primary}")
+    private String primaryApimKey;
+
+    @Value("${apim.subscription-key.secondary}")
+    private String secondaryApimKey;
 
     @BeforeEach
     public void setUp() {
@@ -36,5 +48,30 @@ class SmokeTest {
 
         assertThat(response.statusCode()).isEqualTo(OK.value());
         assertThat(response.asString().substring(1)).startsWith("\"status\":\"UP\"");
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "APIM_ENABLED", matches = "true")
+    void apimCheck() {
+        RestAssured.baseURI = apimUri;
+
+        var primaryResponse = apimRequest(primaryApimKey);
+
+        if (primaryResponse.statusCode() != OK.value()) {
+            var secondaryResponse = apimRequest(secondaryApimKey);
+            assertThat(secondaryResponse.statusCode()).isEqualTo(OK.value());
+        } else {
+            assertThat(primaryResponse.statusCode()).isEqualTo(OK.value());
+        }
+    }
+
+    private io.restassured.response.Response apimRequest(String key) {
+        return given()
+            .relaxedHTTPSValidation()
+            .headers(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+            .headers("Ocp-Apim-Subscription-Key", key)
+            .when()
+            .get("/pre-api/courts")
+            .andReturn();
     }
 }
