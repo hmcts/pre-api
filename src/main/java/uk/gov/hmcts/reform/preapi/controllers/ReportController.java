f689aee8a23da746ed4ae5711334503640758555
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-01-19 14:33:55 +0000

    Styling

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/ReportController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/ReportController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.preapi.controllers.params.SearchSharedReport;
 import uk.gov.hmcts.reform.preapi.dto.reports.AccessRemovedReportDTO;
