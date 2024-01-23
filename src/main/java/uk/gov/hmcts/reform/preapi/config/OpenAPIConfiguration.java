9c9b8aa2da23c8e4e32d43c17a326999ccd0c2cb
Jason Paige <jasonpaige@users.noreply.github.com>
2024-01-23 13:46:41 +0000

    Add Swagger docs for X-User-Id header (#215)
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>

--- src/main/java/uk/gov/hmcts/reform/preapi/config/OpenAPIConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/config/OpenAPIConfiguration.java
@@ -1,11 +1,19 @@
 package uk.gov.hmcts.reform.preapi.config;
 
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.UUIDSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
+
 
 @Configuration
 public class OpenAPIConfiguration {
@@ -20,6 +28,30 @@ public class OpenAPIConfiguration {
             .externalDocs(new ExternalDocumentation()
                               .description("README")
                               .url("https://github.com/hmcts/pre-api"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi(OperationCustomizer customGlobalHeaders) {
+        return GroupedOpenApi.builder()
+            .group("pre-api")
+            .pathsToMatch("**/controllers/**")
+            .addOperationCustomizer(customGlobalHeaders)
+            .build();
+    }
+
+    @Bean
+    public OperationCustomizer customGlobalHeaders() {
+        return (Operation customOperation, HandlerMethod handlerMethod) -> {
+            Parameter serviceAuthorizationHeader = new Parameter()
+                .in(ParameterIn.HEADER.toString())
+                .schema(new UUIDSchema())
+                .name("X-User-Id")
+                .description("The User Id of the User making the request")
+                .example("123e4567-e89b-12d3-a456-426614174000")
+                .required(false); // set to true once Power Platform is updated.
+            customOperation.addParametersItem(serviceAuthorizationHeader);
+            return customOperation;
+        };
     }
 
 }
