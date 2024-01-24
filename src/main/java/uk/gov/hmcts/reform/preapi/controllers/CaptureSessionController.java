88db16f9769f1dbcb06f4d4948111602de05b312
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-01-24 15:33:14 +0000

    Add `PUT /capture-sessions/{id}`

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/CaptureSessionController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/CaptureSessionController.java
@@ -1,20 +1,26 @@
 package uk.gov.hmcts.reform.preapi.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.preapi.controllers.base.PreApiController;
 import uk.gov.hmcts.reform.preapi.dto.CaptureSessionDTO;
+import uk.gov.hmcts.reform.preapi.dto.CreateCaptureSessionDTO;
+import uk.gov.hmcts.reform.preapi.exception.PathPayloadMismatchException;
 import uk.gov.hmcts.reform.preapi.services.CaptureSessionService;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/capture-sessions")
-public class CaptureSessionController {
+public class CaptureSessionController extends PreApiController {
 
     private final CaptureSessionService captureSessionService;
 
@@ -27,5 +33,17 @@ public class CaptureSessionController {
     @Operation
     public ResponseEntity<CaptureSessionDTO> getCaptureSessionById(@PathVariable UUID captureSessionId) {
         return ResponseEntity.ok(captureSessionService.findById(captureSessionId));
+    }
+
+    @PutMapping("/{captureSessionId}")
+    @Operation(operationId = "upsertCaptureSession", summary = "Create or Update a Capture Session")
+    public ResponseEntity<Void> upsertCaptureSession(
+        @PathVariable UUID captureSessionId,
+        @Valid @RequestBody CreateCaptureSessionDTO createCaptureSessionDTO
+    ) {
+        if (!captureSessionId.equals(createCaptureSessionDTO.getId())) {
+            throw new PathPayloadMismatchException("id", "createCaptureSessionDTO.id");
+        }
+        return getUpsertResponse(captureSessionService.upsert(createCaptureSessionDTO), createCaptureSessionDTO.getId());
     }
 }
--- src/main/java/uk/gov/hmcts/reform/preapi/services/CaptureSessionService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/CaptureSessionService.java
@@ -4,9 +4,15 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.preapi.dto.CaptureSessionDTO;
+import uk.gov.hmcts.reform.preapi.dto.CreateCaptureSessionDTO;
 import uk.gov.hmcts.reform.preapi.entities.Booking;
+import uk.gov.hmcts.reform.preapi.entities.CaptureSession;
+import uk.gov.hmcts.reform.preapi.enums.UpsertResult;
 import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
+import uk.gov.hmcts.reform.preapi.exception.ResourceInDeletedStateException;
+import uk.gov.hmcts.reform.preapi.repositories.BookingRepository;
 import uk.gov.hmcts.reform.preapi.repositories.CaptureSessionRepository;
+import uk.gov.hmcts.reform.preapi.repositories.UserRepository;
 
 import java.util.UUID;
 
@@ -15,12 +21,18 @@ public class CaptureSessionService {
 
     private final RecordingService recordingService;
     private final CaptureSessionRepository captureSessionRepository;
+    private final BookingRepository bookingRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public CaptureSessionService(RecordingService recordingService,
-                                 CaptureSessionRepository captureSessionRepository) {
+                                 CaptureSessionRepository captureSessionRepository,
+                                 BookingRepository bookingRepository,
+                                 UserRepository userRepository) {
         this.recordingService = recordingService;
         this.captureSessionRepository = captureSessionRepository;
+        this.bookingRepository = bookingRepository;
+        this.userRepository = userRepository;
     }
 
     @Transactional
@@ -37,5 +49,50 @@ public class CaptureSessionService {
             .findAllByBookingAndDeletedAtIsNull(booking)
             .forEach(recordingService::deleteCascade);
         captureSessionRepository.deleteAllByBooking(booking);
+    }
+
+    @Transactional
+    public UpsertResult upsert(CreateCaptureSessionDTO createCaptureSessionDTO) {
+        var foundCaptureSession = captureSessionRepository.findById(createCaptureSessionDTO.getId());
+
+        if (foundCaptureSession.isPresent() && foundCaptureSession.get().isDeleted()) {
+            throw new ResourceInDeletedStateException("CaptureSessionDTO", createCaptureSessionDTO.getId().toString());
+        }
+
+        var captureSession = foundCaptureSession.orElse(new CaptureSession());
+
+        var booking = bookingRepository
+            .findByIdAndDeletedAtIsNull(createCaptureSessionDTO.getBookingId())
+            .orElseThrow(() -> new NotFoundException("Booking: " + createCaptureSessionDTO.getBookingId()));
+
+
+        var startedByUser = createCaptureSessionDTO.getStartedByUserId() != null
+            ? userRepository
+                .findByIdAndDeletedAtIsNull(createCaptureSessionDTO.getStartedByUserId())
+                .orElseThrow(() -> new NotFoundException("User: " + createCaptureSessionDTO.getStartedByUserId()))
+            : null;
+
+        var finishedByUser = createCaptureSessionDTO.getFinishedByUserId() != null
+            ? userRepository
+                .findByIdAndDeletedAtIsNull(createCaptureSessionDTO.getFinishedByUserId())
+                .orElseThrow(() -> new NotFoundException("User: " + createCaptureSessionDTO.getFinishedByUserId()))
+            : null;
+
+        captureSession.setId(createCaptureSessionDTO.getId());
+        captureSession.setBooking(booking);
+        captureSession.setOrigin(createCaptureSessionDTO.getOrigin());
+        captureSession.setIngestAddress(createCaptureSessionDTO.getIngestAddress());
+        captureSession.setLiveOutputUrl(createCaptureSessionDTO.getLiveOutputUrl());
+        captureSession.setStartedAt(createCaptureSessionDTO.getStartedAt());
+        captureSession.setStartedByUser(startedByUser);
+        captureSession.setFinishedAt(createCaptureSessionDTO.getFinishedAt());
+        captureSession.setFinishedByUser(finishedByUser);
+        captureSession.setStatus(createCaptureSessionDTO.getStatus());
+
+        captureSessionRepository.save(captureSession);
+
+        var isUpdate = foundCaptureSession.isPresent();
+
+        return isUpdate ? UpsertResult.UPDATED : UpsertResult.CREATED;
     }
 }
