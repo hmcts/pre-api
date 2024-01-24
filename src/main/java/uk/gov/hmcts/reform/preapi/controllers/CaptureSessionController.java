8d5584b536714858061b83c6850c6bdf098095b5
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-01-24 13:08:21 +0000

    Add endpoint DELETE `/capture-sessions/{id}`

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/CaptureSessionController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/CaptureSessionController.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.preapi.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +28,12 @@ public class CaptureSessionController {
     @Operation
     public ResponseEntity<CaptureSessionDTO> getCaptureSessionById(@PathVariable UUID captureSessionId) {
         return ResponseEntity.ok(captureSessionService.findById(captureSessionId));
+    }
+
+    @DeleteMapping("/{captureSessionId}")
+    @Operation(operationId = "deleteCaptureSessionById", summary = "Delete Capture Session by Id")
+    public ResponseEntity<Void> deleteCaptureSessionById(@PathVariable UUID captureSessionId) {
+        captureSessionService.deleteById(captureSessionId);
+        return ResponseEntity.noContent().build();
     }
 }
