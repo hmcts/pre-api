463ccb6f4cddd50769f50719a832dc13d7b89b3f
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-01-29 09:36:47 +0000

    Add authentication object

---
+++ src/main/java/uk/gov/hmcts/reform/preapi/security/UserDetails.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.reform.preapi.security;
+
+import lombok.Getter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import uk.gov.hmcts.reform.preapi.entities.AppAccess;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@Getter
+public class UserDetails extends AbstractAuthenticationToken {
+
+    private final UUID userId;
+    private final String email;
+    private final AppAccess appAccess;
+
+
+    public UserDetails(AppAccess access, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        userId = access.getUser().getId();
+        email = access.getUser().getEmail();
+        appAccess = access;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+}
