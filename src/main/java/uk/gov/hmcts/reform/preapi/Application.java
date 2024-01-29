193158ff5c378ebf7c790b9f85c3a421a5faf515
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-01-29 09:36:22 +0000

    Add spring security

--- src/main/java/uk/gov/hmcts/reform/preapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/Application.java
@@ -2,8 +2,10 @@ package uk.gov.hmcts.reform.preapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
