2d9477d5027cab4bced4c43e2419aee0a6d22c10
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-01-30 09:36:17 +0000

    Remove debug endpoints from auth filtering

--- src/main/java/uk/gov/hmcts/reform/preapi/config/SecurityConfig.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
         "/swagger-ui/**",
         "/v3/api-docs/**",
         "/v3/api-docs",
-        "/health",
+        "/health/**",
         "/info",
         "/prometheus"
     };
