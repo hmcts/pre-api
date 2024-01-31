34e7941c75a63cddc91404c459d8204a89a66ab1
Jason Paige <j@jasonpaige.co.uk>
2024-01-31 08:45:44 +0000

    Rewrite existing audit_details data to a json string for casting

--- src/main/resources/db/migration/V012__UpdateAuditSchema.sql
+++ src/main/resources/db/migration/V012__UpdateAuditSchema.sql
@@ -2,6 +2,8 @@ ALTER TABLE public.audits DROP COLUMN type;
 
 CREATE INDEX audit_table_record_id_table_name ON public.audits (table_record_id, table_name);
 
+UPDATE public.audits SET audit_details = CONCAT('{"description": "', audit_details, '"}') WHERE audit_details IS NOT NULL;
+
 ALTER TABLE public.audits ALTER COLUMN audit_details TYPE JSONB USING audit_details::jsonb;
 
 DROP TYPE public.AUDIT_LOG_TYPE;
