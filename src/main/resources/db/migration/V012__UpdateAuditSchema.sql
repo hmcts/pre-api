5d348beba86fecd89053593e7060a1adf4978b05
Jason Paige <jasonpaige@users.noreply.github.com>
2024-01-31 11:09:51 +0000

    Multistep column change to convert audit_details to jsonb (#238)
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/main/resources/db/migration/V012__UpdateAuditSchema.sql
+++ src/main/resources/db/migration/V012__UpdateAuditSchema.sql
@@ -1,7 +1,13 @@
 ALTER TABLE public.audits DROP COLUMN type;
 
+DROP TYPE public.AUDIT_LOG_TYPE;
+
 CREATE INDEX audit_table_record_id_table_name ON public.audits (table_record_id, table_name);
 
-ALTER TABLE public.audits ALTER COLUMN audit_details TYPE JSONB USING audit_details::jsonb;
+ALTER TABLE public.audits ADD COLUMN audit_details_jsonb JSONB;
 
-DROP TYPE public.AUDIT_LOG_TYPE;
+UPDATE public.audits set audit_details_jsonb = to_jsonb(audit_details::text);
+
+ALTER TABLE public.audits DROP COLUMN audit_details;
+
+ALTER TABLE public.audits RENAME COLUMN audit_details_jsonb TO audit_details;
