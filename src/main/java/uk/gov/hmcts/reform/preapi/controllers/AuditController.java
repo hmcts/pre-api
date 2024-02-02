199b13ec04d4df1c134a226359306c665e731ae0
Jason Paige <jasonpaige@users.noreply.github.com>
2024-02-02 09:57:48 +0000

    Remove auth from audit and make x-user-id optional (#258)
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>
    Co-authored-by: PRE DevOps <138598290+pre-devops@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/AuditController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/AuditController.java
@@ -3,9 +3,9 @@ package uk.gov.hmcts.reform.preapi.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +17,8 @@ import uk.gov.hmcts.reform.preapi.exception.PathPayloadMismatchException;
 import uk.gov.hmcts.reform.preapi.services.AuditService;
 
 import java.util.UUID;
+
+import static uk.gov.hmcts.reform.preapi.config.OpenAPIConfiguration.X_USER_ID_HEADER;
 
 @RestController
 @RequestMapping("/audit")
@@ -32,14 +34,17 @@ public class AuditController {
 
     @PutMapping("/{id}")
     @Operation(operationId = "putAudit", summary = "Create an Audit Entry")
-    @PreAuthorize("hasAnyRole('ROLE_SUPER_USER', 'ROLE_LEVEL_1', 'ROLE_LEVEL_2', 'ROLE_LEVEL_3', 'ROLE_LEVEL_4')")
-    public ResponseEntity<Void> upsertCase(@RequestHeader("X-User-Id") UUID xUserId,
-                                           @PathVariable UUID id,
-                                           @Valid @RequestBody CreateAuditDTO createAuditDTO) {
+    public ResponseEntity<Void> upsertAudit(@RequestHeader HttpHeaders headers,
+                                            @PathVariable UUID id,
+                                            @Valid @RequestBody CreateAuditDTO createAuditDTO) {
         if (!id.equals(createAuditDTO.getId())) {
             throw new PathPayloadMismatchException("id", "createAuditDTO.id");
         }
-        this.auditService.upsert(createAuditDTO, xUserId);
+
+        var userId = headers.getValuesAsList(X_USER_ID_HEADER).isEmpty()
+            ? null
+            : UUID.fromString(headers.getValuesAsList(X_USER_ID_HEADER).getFirst());
+        this.auditService.upsert(createAuditDTO, userId);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
