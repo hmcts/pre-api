ffcee735208d5998bf439c6a9fab346fa19baf4d
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-02-05 12:15:45 +0000

    Styling

--- src/test/java/uk/gov/hmcts/reform/preapi/security/AuthorisationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/security/AuthorisationServiceTest.java
@@ -31,7 +31,6 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
