040def48bf30b0885d47c983209ada1a97236937
mazzopardi2 <marianne.azzopardi@hmcts.net>
2024-02-08 16:44:34 +0000

    logic change to share bookings. With this change bookings with no recordings that are shared will be captured also

--- bin/migration/tables/sharebookings.py
+++ bin/migration/tables/sharebookings.py
@@ -10,48 +10,73 @@ class ShareBookingsManager:
         self.source_cursor.execute("SELECT * FROM public.videopermissions")
         return self.source_cursor.fetchall()
     
-    def get_booking(self, bookings_data, recording_id):
-        booking_id = next((booking[0] for booking in bookings_data if booking[1] == recording_id), None)
-        return booking_id
+    def get_case_id_and_version(self, recording_id):
+        self.source_cursor.execute("""
+                                    SELECT vp.caseuid, r.recordingversion
+                                    FROM public.videopermissions vp
+                                    LEFT JOIN public.cases c ON c.caseuid = vp.caseuid
+                                    LEFT JOIN public.recordings r ON r.recordinguid = vp.recordinguid
+                                    WHERE c.caseuid IS NOT NULL
+                                    AND r.recordinguid IS NOT NULL 
+                                    AND r.recordinguid = %s""", (recording_id,))
+        case_data = self.source_cursor.fetchone()
+        return case_data if case_data else (None, None)
+
+    """ I am getting booking ids from different tables, because if a booking has no associated recording, 
+     i won't be able to find the booking id with the recording id from the destination db """
+    def get_booking_id_v1(self, destination_cursor, recording_id):
+        destination_cursor.execute("SELECT booking_id FROM public.temp_recordings WHERE recording_id = %s",(recording_id,))
+        booking_id = destination_cursor.fetchone()
+        return booking_id[0] if booking_id else None
     
+    def get_booking_id_non_v1(self, destination_cursor, recording_id):
+        destination_cursor.execute("""  SELECT b.id AS booking_id
+                                        FROM recordings r
+                                        JOIN capture_sessions c ON c.id = r.capture_session_id
+                                        JOIN bookings b ON b.id = c.booking_id
+                                        WHERE r.id = %s
+                                        """,(recording_id,))
+        booking_id = destination_cursor.fetchone()
+        return booking_id[0] if booking_id else None
 
     def migrate_data(self, destination_cursor, source_data):
         batch_share_bookings_data = []
 
-        destination_cursor.execute("""  SELECT b.id AS booking_id, r.id AS recording_id
-                                        FROM bookings b
-                                        LEFT JOIN capture_sessions cs ON cs.booking_id = b.id
-                                        LEFT JOIN recordings r ON r.capture_session_id = cs.id
-                                        WHERE r.id is not null""")
-        bookings_data = destination_cursor.fetchall()
-
         for video_permission in source_data:
             id = video_permission[0]
             recording_id = video_permission[1]
+            case_id, version = self.get_case_id_and_version(recording_id)
 
-            booking_id = self.get_booking(bookings_data, recording_id)
+            if case_id is None:
+                self.failed_imports.add(('share_bookings',id,f"No valid case id associated with recording id: {recording_id}, not in cases table"))
+                continue
 
+            if version == '1':
+                booking_id = self.get_booking_id_v1(destination_cursor, recording_id)
+            else:
+                booking_id = self.get_booking_id_non_v1(destination_cursor, recording_id)
+            
+            if booking_id is None:
+                self.failed_imports.add(('share_bookings',id,f"No valid booking id associated with case id: {case_id} (recording id: {recording_id}) not in bookings table"))
+                continue
+            
             shared_with_user_id = video_permission[4]
+            if not check_existing_record(destination_cursor,'users','id',shared_with_user_id):
+                self.failed_imports.add(('share_bookings', id, f"Invalid shared_with_user_id value: {shared_with_user_id} (recording id: {recording_id})"))
+                continue
 
             created_by = get_user_id(destination_cursor,video_permission[18])
             shared_by_user_id = created_by
 
-            created_at = parse_to_timestamp(video_permission[19])
-            deleted_at = parse_to_timestamp(video_permission[21]) if video_permission[15] != "True" else None
-
-            if not booking_id:
-                self.failed_imports.add(('share_bookings', id, f"No booking id found for recordinguid: {recording_id}"))
-                continue
-
-            if not check_existing_record(destination_cursor,'users','id',shared_with_user_id):
-                self.failed_imports.add(('share_bookings', id, f"Invalid shared_with_user_id value: {shared_with_user_id}"))
-                continue
-
             if not shared_by_user_id:
-                self.failed_imports.add(('share_bookings', id, f"No user found for shared_with_user email : {created_by}"))
+                self.failed_imports.add(('share_bookings', id, f"No user found for shared_with_user email : {created_by} (recording id: {recording_id})"))
                 continue
 
-            batch_share_bookings_data.append((id, booking_id, shared_with_user_id, shared_by_user_id,created_at, deleted_at))
+            created_at = parse_to_timestamp(video_permission[19])
+            deleted_at = parse_to_timestamp(video_permission[21]) if video_permission[15] != "True" else None       
+
+            if not check_existing_record(destination_cursor, 'share_bookings','id',id):
+                batch_share_bookings_data.append((id, booking_id, shared_with_user_id, shared_by_user_id,created_at, deleted_at))
             
         try:
             if batch_share_bookings_data:
