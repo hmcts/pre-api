6b5a8734af10977b0a9304e95c94b5b22006cead
Laurence Rawlings <laurence.rawlings@hmcts.net>
2024-02-16 09:14:49 +0000

    S28-2322/redeem invites (#307)
    Co-authored-by: PRE DevOps <138598290+pre-devops@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/preapi/dto/InviteDTO.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/dto/InviteDTO.java
@@ -1,33 +1,46 @@
 package uk.gov.hmcts.reform.preapi.dto;
 
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.preapi.entities.Invite;
+import uk.gov.hmcts.reform.preapi.entities.PortalAccess;
 
 import java.sql.Timestamp;
+import java.util.UUID;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
-@AllArgsConstructor
 @Schema(description = "InviteDTO")
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class InviteDTO extends CreateInviteDTO {
-    @Schema(description = "InviteCreatedAt")
-    private Timestamp createdAt;
+public class InviteDTO {
+    @Schema(description = "InviteUserId")
+    protected UUID userId;
 
-    @Schema(description = "InvitedModifiedAt")
-    private Timestamp modifiedAt;
+    @Schema(description = "InviteUserFirstName")
+    protected String firstName;
 
-    public InviteDTO(Invite invite) {
-        super(invite);
-        this.createdAt = invite.getCreatedAt();
-        this.modifiedAt = invite.getModifiedAt();
+    @Schema(description = "InviteUserLastName")
+    protected String lastName;
+
+    @Schema(description = "InviteUserEmail")
+    protected String email;
+
+    @Schema(description = "InvitedAt")
+    protected Timestamp invitedAt;
+
+    @Schema(description = "InviteCode")
+    protected String code;
+
+    public InviteDTO(PortalAccess portalAccess) {
+        var user = portalAccess.getUser();
+        userId = user.getId();
+        firstName = user.getFirstName();
+        lastName = user.getLastName();
+        email = user.getEmail();
+        invitedAt = portalAccess.getInvitedAt();
+        code = portalAccess.getCode();
     }
 }
-
