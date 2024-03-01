768584277b0faa0cfae1f2bc268c7ae992a92e01
mazzopardi2 <marianne.azzopardi@hmcts.net>
2024-03-01 15:45:29 +0000

    updates to portal access, eliminate status field and adding logged_in bool

--- bin/migration/tables/portalaccess.py
+++ bin/migration/tables/portalaccess.py
@@ -49,14 +49,6 @@ class PortalAccessManager:
             if not check_existing_record(destination_cursor,'portal_access', 'user_id', user_id):
                 id = str(uuid.uuid4())
                 password = 'password' # temporary field - to be removed once B2C implemented
-
-                status_active = str(user[1]).lower() == 'active'
-                
-                if status_active:
-                    status = "ACTIVE"
-                else:
-                    status = "INACTIVE"
-
                 invited_at, registered_at = dataverse_info.get(user_id, (None, None))
                 terms_accepted_at = registered_at
                 last_access = parse_to_timestamp(self.get_last_access_date(user_email))
@@ -68,7 +60,7 @@ class PortalAccessManager:
                 
 
                 batch_portal_user_data.append((
-                    id, user_id, password,last_access, status, created_at,invited_at, registered_at,terms_accepted_at, modified_at, created_by
+                    id, user_id, password, last_access,  invited_at, registered_at, terms_accepted_at, created_at, modified_at, created_by
                 ))
 
         try: 
@@ -76,8 +68,8 @@ class PortalAccessManager:
                 destination_cursor.executemany(
                     """
                     INSERT INTO public.portal_access
-                        (id, user_id, password, last_access, status, created_at,registered_at,terms_accepted_at, invited_at, modified_at)
-                    VALUES (%s, %s, %s,%s,%s,%s,%s,%s,%s, %s)
+                        (id, user_id, password, last_access,  invited_at, registered_at, terms_accepted_at, created_at, modified_at)
+                    VALUES (%s, %s, %s,%s,%s,%s,%s,%s, %s)
                     """,
                     [entry[:-1] for entry in batch_portal_user_data],
                 )
@@ -90,8 +82,8 @@ class PortalAccessManager:
                         table_name='portal_access',
                         record_id=entry[0],
                         record=entry[1],
-                        created_at=entry[5],
-                        created_by= entry[10]
+                        created_at=entry[7],
+                        created_by= entry[9]
                     )
         except Exception as e:
             self.failed_imports.append({'table_name': 'portal_access','table_id': id,'details': str(e)})
