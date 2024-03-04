ee008fc008c193c358d458355eab7583750e24ea
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-03-04 17:17:34 +0000

    Fix issue with BST values in scheduled_for column

--- bin/migration/tables/bookings.py
+++ bin/migration/tables/bookings.py
@@ -17,7 +17,7 @@ class BookingManager:
     def migrate_data(self, destination_cursor, source_data):
         destination_cursor.execute("SELECT id FROM public.cases")
         cases_data = destination_cursor.fetchall()
-        existing_case_ids = {case[0] for case in cases_data} 
+        existing_case_ids = {case[0] for case in cases_data}
 
         destination_cursor.execute(
             """CREATE TABLE IF NOT EXISTS public.temp_recordings (
@@ -42,7 +42,7 @@ class BookingManager:
         for recording in source_data:
             case_id = recording[1]
             recording_id = recording[0]
-            scheduled_for = parse_to_timestamp(recording[10])
+            scheduled_for = parse_to_timestamp(recording[10], to_utc=True)
             recording_status = recording[11]
             created_at = parse_to_timestamp(recording[22])
             deleted_at = None
@@ -55,7 +55,7 @@ class BookingManager:
             modified_at = parse_to_timestamp(recording[24]) if recording[24] is not None else created_at
             created_by =  get_user_id(destination_cursor,recording[21])
 
-            # Check if the case has been migrated into the cases table 
+            # Check if the case has been migrated into the cases table
             if case_id not in existing_case_ids:
                 self.failed_imports.append({
                     'table_name': 'bookings',
@@ -65,18 +65,18 @@ class BookingManager:
                     'details': f'Case ID: {case_id} not found in cases data.'
                 })
                 continue
-            
-            # Insert into temp table 
+
+            # Insert into temp table
             if not check_existing_record(destination_cursor,'temp_recordings', 'recording_id', recording_id):
                 booking_id = str(uuid.uuid4())
                 try:
                     destination_cursor.execute(
                         """
-                        INSERT INTO public.temp_recordings 
+                        INSERT INTO public.temp_recordings
                             (case_id, recording_id, booking_id,scheduled_for, deleted_at, created_at, created_by,modified_at )
                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
                         """,
-                        (   case_id, recording_id, booking_id,scheduled_for,deleted_at, created_at, 
+                        (   case_id, recording_id, booking_id,scheduled_for,deleted_at, created_at,
                             created_by if created_by is not None else None,
                             modified_at if modified_at is not None else None
                         ),
@@ -100,7 +100,7 @@ class BookingManager:
             case_id = booking[4]
             scheduled_for = booking[5]
 
-            if not check_existing_record(destination_cursor,'bookings', 'id', id):   
+            if not check_existing_record(destination_cursor,'bookings', 'id', id):
                 try:
                     if scheduled_for is None:
                         self.failed_imports.append({
@@ -119,7 +119,7 @@ class BookingManager:
 
                     destination_cursor.execute(
                         """
-                        INSERT INTO public.bookings 
+                        INSERT INTO public.bookings
                             (id, case_id, scheduled_for, created_at, modified_at, deleted_at)
                         VALUES (%s, %s, %s, %s, %s, %s )
                         """,
@@ -134,9 +134,9 @@ class BookingManager:
                         created_at=created_at,
                         created_by=created_by if created_by is not None else None,
                     )
-                except Exception as e:  
+                except Exception as e:
                     self.failed_imports.append({'table_name': 'bookings','table_id': id,'details': str(e)})
-                    
+
         self.logger.log_failed_imports(self.failed_imports)
-    
-   
+
+
