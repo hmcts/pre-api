c134e200dff976089b0e82122d0e0f6be2598fe1
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-03-08 11:42:23 +0000

    S28 2511: Fix `scheduledFor` Search Param to `LocalDate` (#366)

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/params/SearchBookings.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/params/SearchBookings.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.preapi.controllers.params;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
@@ -12,7 +12,7 @@ public class SearchBookings {
     private String caseReference;
     private UUID courtId;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date scheduledFor;
+    private LocalDate scheduledFor;
     private UUID participantId;
     private Boolean hasRecordings;
 
