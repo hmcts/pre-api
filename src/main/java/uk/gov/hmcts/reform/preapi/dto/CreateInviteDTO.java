3fd9283d79faa890c62fe4c727f754e0da71271c
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-03-14 11:13:34 +0000

    Add validation constraints to putInvite endpoint + remove invite code db column

--- src/main/java/uk/gov/hmcts/reform/preapi/dto/CreateInviteDTO.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/dto/CreateInviteDTO.java
@@ -3,6 +3,10 @@ package uk.gov.hmcts.reform.preapi.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,15 +18,20 @@ import java.util.UUID;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CreateInviteDTO {
     @Schema (description = "InviteUserId")
+    @NotNull
     protected UUID userId;
 
     @Schema(description = "InviteFirstName")
+    @NotBlank
     protected String firstName;
 
     @Schema(description = "InviteLastName")
+    @NotBlank
     protected String lastName;
 
     @Schema(description = "InviteEmail")
+    @NotBlank
+    @Email
     protected String email;
 
     @Schema(description = "InviteOrganisation")
@@ -30,8 +39,5 @@ public class CreateInviteDTO {
 
     @Schema(description = "InvitePhone")
     protected String phone;
-
-    @Schema(description = "InviteCode")
-    protected String code;
 }
 
