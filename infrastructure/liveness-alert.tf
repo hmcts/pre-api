3292ef32ea995b83e153a3bfdb66eef756ce6ed2
Jason Paige <jasonpaige@users.noreply.github.com>
2024-03-15 10:20:08 +0000

    Add liveness and exception alerts (#392)
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>

---
+++ infrastructure/liveness-alert.tf
@@ -0,0 +1,25 @@
+module "pre-api-liveness-alert" {
+  count               = var.env == "prod" ? 1 : 0
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = data.azurerm_application_insights.app_insights.location
+  app_insights_name = data.azurerm_application_insights.app_insights.name
+
+  alert_name  = "PRE_API_liveness"
+  alert_desc  = "Triggers when pre api looks it's been down within a 30 minutes window timeframe."
+  common_tags = var.common_tags
+
+  app_insights_query = <<EOF
+requests
+| where name == "GET /health" and resultCode != "200"
+| where cloud_RoleName == "pre-api"
+EOF
+
+  frequency_in_minutes       = "15"
+  time_window_in_minutes     = "30"
+  severity_level             = "2"
+  action_group_name          = data.azurerm_monitor_action_group.action_group[count.index].name
+  custom_email_subject       = "PRE API liveness"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold          = "5"
+  resourcegroup_name         = "${var.product}-${var.env}"
+}
