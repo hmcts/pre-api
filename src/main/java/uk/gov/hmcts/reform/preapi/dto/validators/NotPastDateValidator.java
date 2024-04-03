793a9d2bb31ebb1a44e61bcbd4d87d7435aa891d
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-04-03 09:44:03 +0100

    S28 2788: `NotPastDateValidator` Timezone Fix (#429)

--- src/main/java/uk/gov/hmcts/reform/preapi/dto/validators/NotPastDateValidator.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/dto/validators/NotPastDateValidator.java
@@ -4,7 +4,8 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.sql.Timestamp;
-import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class NotPastDateValidator implements ConstraintValidator<NotPastDateConstraint, Timestamp> {
@@ -14,7 +15,10 @@ public class NotPastDateValidator implements ConstraintValidator<NotPastDateCons
 
     @Override
     public boolean isValid(Timestamp dateField, ConstraintValidatorContext cxt) {
-        var midnight = Instant.now().truncatedTo(ChronoUnit.DAYS).minusMillis(1);
+        var midnight = ZonedDateTime.now(ZoneId.of("Europe/London"))
+            .truncatedTo(ChronoUnit.DAYS)
+            .toInstant()
+            .minusMillis(1);
         return dateField != null && dateField.after(Timestamp.from(midnight));
     }
 }
