7d37141b2536af753f84ccdb446dc849d0aeb525
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-04-03 12:16:09 +0100

    S28 2788: Fix: `NotPastDateValidator` (#434)

--- src/main/java/uk/gov/hmcts/reform/preapi/dto/validators/NotPastDateValidator.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/dto/validators/NotPastDateValidator.java
@@ -4,9 +4,9 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 
 public class NotPastDateValidator implements ConstraintValidator<NotPastDateConstraint, Timestamp> {
     @Override
@@ -15,10 +15,13 @@ public class NotPastDateValidator implements ConstraintValidator<NotPastDateCons
 
     @Override
     public boolean isValid(Timestamp dateField, ConstraintValidatorContext cxt) {
-        var midnight = ZonedDateTime.now(ZoneId.of("Europe/London"))
-            .truncatedTo(ChronoUnit.DAYS)
-            .toInstant()
-            .minusMillis(1);
-        return dateField != null && dateField.after(Timestamp.from(midnight));
+        if (dateField == null) {
+            return false;
+        }
+
+        var localDateField = LocalDateTime.ofInstant(dateField.toInstant(), ZoneId.systemDefault()).toLocalDate();
+        var today = LocalDate.now();
+
+        return !localDateField.isBefore(today);
     }
 }
