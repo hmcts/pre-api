f6f836a48e435460271b3b5f5ce48740d02048b0
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-04-11 09:40:54 +0100

    Add test: create/update recording

--- src/functionalTest/java/uk/gov/hmcts/reform/preapi/controllers/RecordingControllerFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/preapi/controllers/RecordingControllerFT.java
@@ -1,10 +1,17 @@
 package uk.gov.hmcts.reform.preapi.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.preapi.dto.CreateCaptureSessionDTO;
+import uk.gov.hmcts.reform.preapi.dto.CreateRecordingDTO;
+import uk.gov.hmcts.reform.preapi.enums.RecordingOrigin;
+import uk.gov.hmcts.reform.preapi.enums.RecordingStatus;
 import uk.gov.hmcts.reform.preapi.util.FunctionalTestBase;
 
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RecordingControllerFT extends FunctionalTestBase {
     private record CreateRecordingResponse(UUID bookingId, UUID captureSessionId, UUID recordingId) {
@@ -28,9 +35,69 @@ public class RecordingControllerFT extends FunctionalTestBase {
         assertRecordingExists(recordingDetails.recordingId, true);
     }
 
+    @DisplayName("Scenario: Create and update a recording")
+    @Test
+    void shouldCreateAndUpdateRecording() throws JsonProcessingException {
+        var captureSession = createCaptureSession();
+        var putCaptureSession = doPutRequest(
+            CAPTURE_SESSIONS_ENDPOINT + "/" + captureSession.getId(),
+            OBJECT_MAPPER.writeValueAsString(captureSession),
+            true
+        );
+        assertResponseCode(putCaptureSession, 201);
+        assertCaptureSessionExists(captureSession.getId(), true);
+
+        // create recording
+        var recording = createRecording(captureSession.getId());
+        var putRecording1 = doPutRequest(
+            RECORDINGS_ENDPOINT + "/" + recording.getId(),
+            OBJECT_MAPPER.writeValueAsString(recording),
+            true
+        );
+        assertResponseCode(putRecording1, 201);
+        var response = assertRecordingExists(recording.getId(), true);
+        assertThat(response.body().jsonPath().getString("filename")).isEqualTo("example.file");
+
+        // update recording
+        recording.setFilename("updated.file");
+        var putRecording2 = doPutRequest(
+            RECORDINGS_ENDPOINT + "/" + recording.getId(),
+            OBJECT_MAPPER.writeValueAsString(recording),
+            true
+        );
+        assertResponseCode(putRecording2, 204);
+        var response2 = assertRecordingExists(recording.getId(), true);
+        assertThat(response2.body().jsonPath().getString("filename")).isEqualTo("updated.file");
+    }
+
+    private CreateRecordingDTO createRecording(UUID captureSessionId) {
+        var dto = new CreateRecordingDTO();
+        dto.setId(UUID.randomUUID());
+        dto.setCaptureSessionId(captureSessionId);
+        dto.setDuration(null);
+        dto.setEditInstructions("{}");
+        dto.setVersion(1);
+        dto.setUrl("example url");
+        dto.setFilename("example.file");
+        return dto;
+    }
+
     private CreateRecordingResponse createRecording() {
         var response = doPostRequest("/testing-support/should-delete-recordings-for-booking", false);
         assertResponseCode(response, 200);
         return response.body().jsonPath().getObject("", CreateRecordingResponse.class);
+    }
+
+    private CreateCaptureSessionDTO createCaptureSession() {
+        var bookingId = doPostRequest("/testing-support/create-well-formed-booking", false)
+            .body()
+            .jsonPath().getUUID("bookingId");
+
+        var dto = new CreateCaptureSessionDTO();
+        dto.setId(UUID.randomUUID());
+        dto.setBookingId(bookingId);
+        dto.setStatus(RecordingStatus.RECORDING_AVAILABLE);
+        dto.setOrigin(RecordingOrigin.PRE);
+        return dto;
     }
 }
