2c661012211a89b95417044d547ccc6f3882862a
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-04-11 13:25:47 +0100

    Styling

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/TestingSupportController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/TestingSupportController.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.preapi.entities.AppAccess;
