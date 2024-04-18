04d079c9c29ef6f298888309482e89ee5ac2d614
MarianneAzzopardi-HMCTS <147049624+mazzopardi2@users.noreply.github.com>
2024-04-18 12:44:51 +0000

    reverting python changes in PR

--- bin/migration/azure_storage_utils/main.py
+++ bin/migration/azure_storage_utils/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pandas as pd
+import argparse
 from azure_storage_utils import AzureBlobStorageManager
 
 root_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -45,7 +46,7 @@ def save_recordings_to_csv(recordings):
 
 
 def update_durations_in_dataframe():
-    df = pd.read_csv("recordings.csv", names=["Recording_ID", "Duration_seconds"], skiprows=1) 
+    df = pd.read_csv("recordings.csv", names=["Recording_ID", "Duration_seconds"])
     formatted_durations = []
     for index, row in df.iterrows():
         recording_id = row["Recording_ID"]
@@ -60,7 +61,7 @@ def update_durations_in_dataframe():
             print(f"Error for recording ID {recording_id}: {str(e)}")
 
     df["Duration_seconds"] = formatted_durations
-    df.to_csv("recordings.csv", index=False, mode="w")
+    df.to_csv("recordings.csv", index=False, mode="w", header=False)
 
 def update_recordings_table(conn):
     try:
@@ -69,37 +70,66 @@ def update_recordings_table(conn):
         for index, row in df.iterrows():
             recording_id = row["Recording_ID"]
             duration = row["Duration_seconds"]
-
-            if pd.isna(duration):
-                print(f"Skipping update for recording ID {recording_id}: Duration is NaN")
-                continue
-
             update_query = f"UPDATE public.recordings SET duration = MAKE_INTERVAL(secs => {duration}) WHERE id = '{recording_id}'"
             try:
                 cur.execute(update_query)
                 conn.connection.commit()
             except Exception as e:
                 print(f"Error updating recording ID {recording_id}: {e}")
-                conn.connection.rollback()
     except Exception as e:
         print(f"Error reading CSV file: {e}")
 
+def print_message_start(step):
+    print(f"Attempting to start step: {step}")
 
-def main():
-    print("Fetching recording ids from database and saving to csv")
-    conn = connect_to_database()
-    recordings = fetch_recordings(conn)
-    save_recordings_to_csv(recordings)
+def print_message_finish(step):
+    print(f"Finishing step: {step}")
 
-    print("Updating dataframe with recording duration values from storage")
-    update_durations_in_dataframe()
+def main(step):
+    ############################################## OPEN Connection with VPN
+    if step == "1":
+        print_message_start(step)
+        # connect to the database
+        conn = connect_to_database()
+        # Fetch recordings and save to CSV
+        recordings = fetch_recordings(conn)
+        # a csv with recording ids will be created
+        save_recordings_to_csv(recordings)
+        print_message_finish(step)
+    
+    ############################################## CLOSE Connection with VPN
+    elif step == "2":
+        print_message_start(step)
+        # Update dataframe with duration values
+        update_durations_in_dataframe()
+        print_message_finish(step)
 
-    print("Inserting values to database")
-    conn = connect_to_database()
-    update_recordings_table(conn)
+    # ############################################# OPEN Connection with VPN
+    elif step == "3":
+        print_message_start(step)
+        # re-connect to the database
+        conn = connect_to_database()
+        # update table in db
+        update_recordings_table(conn)
+        print_message_finish(step)
 
-    print("Recording durations inserted to database.")
+
+    print("Durations appended to the DataFrame.")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Script to fetch recording durations from AMS and perform database operations.")
+    parser.add_argument("--step", dest="step", choices=["1", "2", "3"],
+                        help="Specify which step to execute.")
+    args = parser.parse_args()
+
+    if args.step:
+        main(args.step)
+    else:
+        print("""Please specify a step to execute using the '--step' argument.  e.g.:  
+                    python main.py --step 1
+                    python main.py --step 2
+                    python main.py --step 3
+                    (VPN should be open for Steps 1 & 3 and closed for Step 2)
+              """)
