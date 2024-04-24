5d6f9bf9e25736b4b6f8eff8a23bc898f6f041b4
mazzopardi2 <marianne.azzopardi@hmcts.net>
2024-04-24 10:21:46 +0100

    adding backup option to pipeline, adding function to take backup

--- bin/migration/db_utils.py
+++ bin/migration/db_utils.py
@@ -1,4 +1,7 @@
 import psycopg2 
+from datetime import datetime
+import subprocess
+import os
 
 class DatabaseManager:
     def __init__(self, database, user, password, host, port):
@@ -19,6 +22,30 @@ class DatabaseManager:
         self.cursor.close()
         self.connection.close()
 
+    def take_backup(self, backup_dir):
+        dbname = self.connection.get_dsn_parameters()['dbname']
+        user = self.connection.get_dsn_parameters()['user']
+        host = self.connection.get_dsn_parameters()['host']
+        port = self.connection.get_dsn_parameters()['port']
 
+        timestamp = datetime.now().strftime("%d-%m-%Y_%H:%M")
+        backup_file = f"{backup_dir}/{timestamp}_{dbname}.sql"
+        
+        pg_dump_cmd = [
+            "pg_dump",
+            "-d", dbname,
+            "-U", user,
+            "-h", host,
+            "-p", port,
+            "-W",
+            "-f", backup_file,
+        ]
+
+        try:
+            subprocess.run(pg_dump_cmd, check=True)
+            print(f"Backup successfully created at {backup_file}")
+        except Exception as e:
+            print(f"Error: Backup failed - {e}")
+       
 
 
--- bin/migration/main.py
+++ bin/migration/main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import argparse
 
 from db_utils import DatabaseManager
 
@@ -24,7 +25,6 @@ from tables.audits import AuditLogManager
 from migration_reports.migration_tracker import MigrationTracker
 from migration_reports.failed_imports_logger import FailedImportsLogger
 
-# get passwords from env variables
 source_db_name = os.environ.get('SOURCE_DB_NAME')
 source_db_user = os.environ.get('SOURCE_DB_USER')
 source_db_password = os.environ.get('SOURCE_DB_PASSWORD')
@@ -55,15 +55,12 @@ destination_db = DatabaseManager(
 def get_start_time():
     return time.time()
 
-
 def get_end_time():
     return time.time()
-
 
 def calculate_elapsed_time(start_time, end_time):
     elapsed_time = end_time - start_time
     return round(elapsed_time, 2)
-
 
 def migrate_manager_data(manager, destination_cursor):
     start_time = get_start_time()
@@ -79,11 +76,14 @@ def migrate_manager_data(manager, destination_cursor):
     time_taken = calculate_elapsed_time(start_time, end_time)
     print(
         f"Data migration for {manager.__class__.__name__} complete in : {time_taken:.2f} seconds.\n")
+    
+def backup_source_db(backup_dir):
+    print("Taking backup of the source database...")
+    source_db.take_backup(backup_dir)
 
 
 logger = FailedImportsLogger(destination_db.connection)
 
-# managers for different tables
 room_manager = RoomManager(source_db.connection.cursor(), logger)
 user_manager = UserManager(source_db.connection.cursor(), logger)
 role_manager = RoleManager(source_db.connection.cursor(), logger)
@@ -91,56 +91,64 @@ court_manager = CourtManager(source_db.connection.cursor(), logger)
 courtroom_manager = CourtRoomManager(logger)
 region_manager = RegionManager(logger)
 court_region_manager = CourtRegionManager(logger)
-portal_access_manager = PortalAccessManager(
-    source_db.connection.cursor(), logger)
+portal_access_manager = PortalAccessManager(source_db.connection.cursor(), logger)
 app_access_manager = AppAccessManager(source_db.connection.cursor(), logger)
 case_manager = CaseManager(source_db.connection.cursor(), logger)
 booking_manager = BookingManager(source_db.connection.cursor(), logger)
 participant_manager = ParticipantManager(source_db.connection.cursor(), logger)
-booking_participant_manager = BookingParticipantManager(
-    source_db.connection.cursor(), logger)
-capture_session_manager = CaptureSessionManager(
-    source_db.connection.cursor(), logger)
+booking_participant_manager = BookingParticipantManager(source_db.connection.cursor(), logger)
+capture_session_manager = CaptureSessionManager(source_db.connection.cursor(), logger)
 recording_manager = RecordingManager(source_db.connection.cursor(), logger)
-share_bookings_manager = ShareBookingsManager(
-    source_db.connection.cursor(), logger)
+share_bookings_manager = ShareBookingsManager(source_db.connection.cursor(), logger)
 audit_log_manager = AuditLogManager(source_db.connection.cursor(), logger)
 
 
 def main():
-    destination_db_cursor = destination_db.connection.cursor()
-    print("Migrating data from source to destination database...")
+    parser = argparse.ArgumentParser(description='Database migration script')
+    parser.add_argument('--backup', action='store_true', help='Perform database backup')
+    parser.add_argument('--migration', action='store_true', help='Perform database migration')
+    args = parser.parse_args()
 
-    logger.create_table("failed_imports")
-    counter = MigrationTracker(source_db.connection, destination_db.connection)
-    start_time = get_start_time()
+    if args.backup:
+        backup_dir = "backups"
+        backup_source_db(backup_dir)
 
-    # Migrate data
-    migrate_manager_data(room_manager, destination_db_cursor)
-    migrate_manager_data(user_manager, destination_db_cursor)
-    migrate_manager_data(role_manager, destination_db_cursor)
-    migrate_manager_data(court_manager, destination_db_cursor)
-    migrate_manager_data(courtroom_manager, destination_db_cursor)
-    migrate_manager_data(region_manager, destination_db_cursor)
-    migrate_manager_data(court_region_manager, destination_db_cursor)
-    migrate_manager_data(portal_access_manager, destination_db_cursor)
-    migrate_manager_data(app_access_manager, destination_db_cursor)
-    migrate_manager_data(case_manager, destination_db_cursor)
-    migrate_manager_data(booking_manager, destination_db_cursor)
-    migrate_manager_data(participant_manager, destination_db_cursor)
-    migrate_manager_data(capture_session_manager, destination_db_cursor)
-    migrate_manager_data(recording_manager, destination_db_cursor)
-    migrate_manager_data(booking_participant_manager, destination_db_cursor)
-    migrate_manager_data(share_bookings_manager, destination_db_cursor)
-    migrate_manager_data(audit_log_manager, destination_db_cursor)
+    elif args.migration:
+        destination_db_cursor = destination_db.connection.cursor()
+        print("Migrating data from source to destination database...")
 
-    end_time = get_end_time()
-    runtime = calculate_elapsed_time(start_time, end_time)
-    counter.print_summary()
-    counter.log_records_count(runtime)
+        logger.create_table("failed_imports")
+        counter = MigrationTracker(source_db.connection, destination_db.connection)
+        start_time = get_start_time()
 
-    source_db.close_connection()
-    destination_db.close_connection()
+        migrate_manager_data(room_manager, destination_db_cursor)
+        migrate_manager_data(user_manager, destination_db_cursor)
+        migrate_manager_data(role_manager, destination_db_cursor)
+        migrate_manager_data(court_manager, destination_db_cursor)
+        migrate_manager_data(courtroom_manager, destination_db_cursor)
+        migrate_manager_data(region_manager, destination_db_cursor)
+        migrate_manager_data(court_region_manager, destination_db_cursor)
+        migrate_manager_data(portal_access_manager, destination_db_cursor)
+        migrate_manager_data(app_access_manager, destination_db_cursor)
+        migrate_manager_data(case_manager, destination_db_cursor)
+        migrate_manager_data(booking_manager, destination_db_cursor)
+        migrate_manager_data(participant_manager, destination_db_cursor)
+        migrate_manager_data(capture_session_manager, destination_db_cursor)
+        migrate_manager_data(recording_manager, destination_db_cursor)
+        migrate_manager_data(booking_participant_manager, destination_db_cursor)
+        migrate_manager_data(share_bookings_manager, destination_db_cursor)
+        migrate_manager_data(audit_log_manager, destination_db_cursor)
+
+        end_time = get_end_time()
+        runtime = calculate_elapsed_time(start_time, end_time)
+        counter.print_summary()
+        counter.log_records_count(runtime)
+
+        source_db.close_connection()
+        destination_db.close_connection()
+    
+    else:
+        print("No action specified. Use --backup or --migration.")
 
 
 if __name__ == "__main__":
