81c5fa0054d618c9b412ecad10127b65f9579ae4
Laurence Rawlings <laurence.rawlings@hmcts.net>
2024-04-25 16:05:40 +0100

    fix: add logger stream handler

--- bin/cvp-integration-poc/main.py
+++ bin/cvp-integration-poc/main.py
@@ -10,6 +10,12 @@ import requests
 load_dotenv()
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 cvp_url = os.getenv("CVP_URL")
 
 
