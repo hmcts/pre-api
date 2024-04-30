5b87441c57e9eafd29d74ddba57ac63a78d4bb63
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-04-30 11:19:01 +0100

    Add example media service controller endpoint

---
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/MediaServiceController.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.preapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.preapi.controllers.base.PreApiController;
+import uk.gov.hmcts.reform.preapi.media.AzureMediaService;
+
+@RestController
+@RequestMapping("/media-service")
+public class MediaServiceController extends PreApiController {
+
+    private final AzureMediaService mediaService;
+
+    @Autowired
+    public MediaServiceController(AzureMediaService mediaService) {
+        this.mediaService = mediaService;
+    }
+
+    @GetMapping("/example-endpoint")
+    @PreAuthorize("hasAnyRole('ROLE_SUPER_USER', 'ROLE_LEVEL_1', 'ROLE_LEVEL_2', 'ROLE_LEVEL_3', 'ROLE_LEVEL_4')")
+    public ResponseEntity<Object> example() {
+        return ResponseEntity.ok(mediaService.getAssets());
+    }
+}
