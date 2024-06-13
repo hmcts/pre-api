41e6fd7be00bf7bae246bce74495e6fb51fe6705
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-06-13 12:18:13 +0100

    S28 2884: `GET /media-service/live-events` and `GET /media-service/live-events/{name}` (#528)
    Co-authored-by: Laurence Rawlings <laurence.rawlings@hmcts.net>
    Co-authored-by: PRE DevOps <138598290+pre-devops@users.noreply.github.com>
    Co-authored-by: hmcts-jenkins-cnp <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
    Co-authored-by: Jenkins <nobody@nowhere>
    Co-authored-by: Jason Paige <jasonpaige@users.noreply.github.com>
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/MediaServiceController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/MediaServiceController.java
@@ -1,13 +1,20 @@
 package uk.gov.hmcts.reform.preapi.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.preapi.controllers.base.PreApiController;
+import uk.gov.hmcts.reform.preapi.dto.media.AssetDTO;
+import uk.gov.hmcts.reform.preapi.dto.media.LiveEventDTO;
+import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
 import uk.gov.hmcts.reform.preapi.media.MediaServiceBroker;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/media-service")
@@ -29,5 +36,45 @@ public class MediaServiceController extends PreApiController {
         return ResponseEntity.ok("successfully connected to media service ("
                                      + mediaService.getClass().getSimpleName()
                                      + ")");
+    }
+
+    @GetMapping("/assets")
+    @Operation(operationId = "getAssets", summary = "Get all media service assets")
+    @PreAuthorize("hasAnyRole('ROLE_SUPER_USER', 'ROLE_LEVEL_1', 'ROLE_LEVEL_2', 'ROLE_LEVEL_3', 'ROLE_LEVEL_4')")
+    public ResponseEntity<List<AssetDTO>> getAssets() {
+        var mediaService = mediaServiceBroker.getEnabledMediaService();
+        return ResponseEntity.ok(mediaService.getAssets());
+    }
+
+    @GetMapping("/assets/{assetName}")
+    @Operation(operationId = "getAssetsByName", summary = "Get a media service asset by name")
+    @PreAuthorize("hasAnyRole('ROLE_SUPER_USER', 'ROLE_LEVEL_1', 'ROLE_LEVEL_2', 'ROLE_LEVEL_3', 'ROLE_LEVEL_4')")
+    public ResponseEntity<AssetDTO> getAsset(@PathVariable String assetName) {
+        var mediaService = mediaServiceBroker.getEnabledMediaService();
+        var data = mediaService.getAsset(assetName);
+        if (data == null) {
+            throw new NotFoundException("Asset: " + assetName);
+        }
+        return ResponseEntity.ok(data);
+    }
+
+    @GetMapping("/live-events")
+    @Operation(operationId = "getLiveEvents", summary = "Get a list of media service live events")
+    @PreAuthorize("hasAnyRole('ROLE_SUPER_USER', 'ROLE_LEVEL_1', 'ROLE_LEVEL_2', 'ROLE_LEVEL_3', 'ROLE_LEVEL_4')")
+    public ResponseEntity<List<LiveEventDTO>> getLiveEvents() {
+        var mediaService = mediaServiceBroker.getEnabledMediaService();
+        return ResponseEntity.ok(mediaService.getLiveEvents());
+    }
+
+    @GetMapping("/live-events/{liveEventName}")
+    @Operation(operationId = "getLiveEventsByName", summary = "Get a media service live event by name")
+    @PreAuthorize("hasAnyRole('ROLE_SUPER_USER', 'ROLE_LEVEL_1', 'ROLE_LEVEL_2', 'ROLE_LEVEL_3', 'ROLE_LEVEL_4')")
+    public ResponseEntity<LiveEventDTO> getLiveEvents(@PathVariable String liveEventName) {
+        var mediaService = mediaServiceBroker.getEnabledMediaService();
+        var data = mediaService.getLiveEvent(liveEventName);
+        if (data == null) {
+            throw new NotFoundException("Live event: " + liveEventName);
+        }
+        return ResponseEntity.ok(data);
     }
 }
