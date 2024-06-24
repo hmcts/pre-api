292bacae7272df58bc6b47ff80ac027523b1c1c8
Jason Paige <j@jasonpaige.co.uk>
2024-06-24 14:10:37 +0100

    handle existing asset

--- src/main/java/uk/gov/hmcts/reform/preapi/media/AzureMediaService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/media/AzureMediaService.java
@@ -217,8 +217,15 @@ public class AzureMediaService implements IMediaService {
                         .withDescription(captureSession.getBookingId().toString())
                 );
         } catch (IllegalArgumentException e) {
+            if (e.getMessage().contains("The storage container is used by another asset already")) {
+                throw new ConflictException("Asset(IllegalArgumentException): " + assetName);
+            }
             throw new UnknownServerException("Unable to communicate with Azure. " + e.getMessage());
         } catch (ManagementException e) {
+            if (e.getResponse().getStatusCode() == 400
+                && e.getMessage().contains("The storage container is used by another asset already")) {
+                throw new ConflictException("Asset: " + assetName);
+            }
             if (e.getResponse().getStatusCode() == 409) {
                 throw new ConflictException("Asset: " + assetName);
             }
