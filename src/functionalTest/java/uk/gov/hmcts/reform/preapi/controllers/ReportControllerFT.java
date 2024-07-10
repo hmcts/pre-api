67a3334565123909aa8f4cdd177141950296614a
Jason Paige <j@jasonpaige.co.uk>
2024-07-10 13:23:07 +0100

    addressing feedback

--- src/functionalTest/java/uk/gov/hmcts/reform/preapi/controllers/ReportControllerFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/preapi/controllers/ReportControllerFT.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.preapi.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.preapi.util.FunctionalTestBase;
@@ -19,15 +18,9 @@ public class ReportControllerFT extends FunctionalTestBase {
 
         var response = doGetRequest(REPORTS_ENDPOINT + "/capture-sessions-concurrent", true);
 
-        var om = new ObjectMapper();
-
-        System.out.println(response.getBody().asString());
-
-        var json = om.readTree(response.getBody().asString());
+        var json = OBJECT_MAPPER.readTree(response.getBody().asString());
 
         assertThat(json.get(0).get("duration").asText()).isEqualTo("PT30M");
         assertThat(json.get(0).get("start_time").asText()).matches("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z");
-
-
     }
 }
