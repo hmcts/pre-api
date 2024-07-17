a159f49ddf91d379aa4429075eaca5324fa7a866
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-07-17 11:13:51 +0100

    S28 2972: Check Stream Endpoint (#596)
    Co-authored-by: hmcts-jenkins-cnp <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
    Co-authored-by: PRE DevOps <138598290+pre-devops@users.noreply.github.com>
    Co-authored-by: Jason Paige <jasonpaige@users.noreply.github.com>
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>

--- src/test/java/uk/gov/hmcts/reform/preapi/media/AzureMediaServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/media/AzureMediaServiceTest.java
@@ -37,6 +37,7 @@ import uk.gov.hmcts.reform.preapi.exception.ConflictException;
 import uk.gov.hmcts.reform.preapi.exception.LiveEventNotRunningException;
 import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
 import uk.gov.hmcts.reform.preapi.exception.UnknownServerException;
+import uk.gov.hmcts.reform.preapi.media.storage.AzureFinalStorageService;
 
 import java.util.List;
 import java.util.UUID;
