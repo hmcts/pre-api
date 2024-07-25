778788fd830ad3e0a602912db0fe3e2ece1995c0
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-07-25 17:18:11 +0100

    Fix: JWT Key Incorrectly Set (AMS) (#637)

--- src/main/java/uk/gov/hmcts/reform/preapi/media/AzureMediaService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/media/AzureMediaService.java
@@ -63,7 +63,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
-import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -114,7 +113,7 @@ public class AzureMediaService implements IMediaService {
     public PlaybackDTO playAsset(String assetName, String userId) {
         getAsset(assetName);
         // todo check asset has files
-        createContentKeyPolicy(userId, Base64.getEncoder().encodeToString(symmetricKey.getBytes()));
+        createContentKeyPolicy(userId, symmetricKey);
         assertStreamingPolicyExists(userId);
         refreshStreamingLocatorForUser(userId, assetName);
 
