103b5e6cbd5cc55744f5e5a362da4f1e51c98597
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-08-12 10:53:27 +0100

    Fix merge

--- src/test/java/uk/gov/hmcts/reform/preapi/services/CaseServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/services/CaseServiceTest.java
@@ -37,7 +37,6 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
