fbc7e9bee5db080dee40babd72353eaf77d8c769
Jason Paige <j@jasonpaige.co.uk>
2024-08-13 10:42:09 +0100

    fix test

--- src/functionalTest/java/uk/gov/hmcts/reform/preapi/controllers/ReportControllerFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/preapi/controllers/ReportControllerFT.java
@@ -13,14 +13,19 @@ public class ReportControllerFT extends FunctionalTestBase {
     @DisplayName("Scenario: Should format Duration and date correctly")
     @Test
     void shouldFormatDurationAndDateCorrectly() throws JsonProcessingException {
-        var postResponseData = doPostRequest("/testing-support/should-delete-recordings-for-booking", false)
-            .body().jsonPath();
+        doPostRequest("/testing-support/should-delete-recordings-for-booking", false);
 
         var response = doGetRequest(REPORTS_ENDPOINT + "/capture-sessions-concurrent", true);
 
         var json = OBJECT_MAPPER.readTree(response.getBody().asString());
 
-        assertThat(json.get(0).get("duration").asText()).isEqualTo("PT30M");
         assertThat(json.get(0).get("start_time").asText()).matches("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z");
+        // find a record which has a duration set (which is optional) and validate the format
+        for (int i = 0; i < json.size(); i++) {
+            if (json.get(i).has("duration")) {
+                assertThat(json.get(i).get("duration").asText()).matches("PT(\\d{1,2}H)?\\d{1,2}M");
+                break;
+            }
+        }
     }
 }
