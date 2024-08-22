269559372219b45b7fce82873be7473b45a2e34b
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-08-22 15:56:16 +0100

    S28 3192: Default Endpoint for Live Playback (#676)
    Co-authored-by: Jason Paige <jasonpaige@users.noreply.github.com>

--- src/functionalTest/java/uk/gov/hmcts/reform/preapi/controllers/ReportControllerFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/preapi/controllers/ReportControllerFT.java
@@ -3,24 +3,33 @@ package uk.gov.hmcts.reform.preapi.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.preapi.dto.reports.ConcurrentCaptureSessionReportDTO;
 import uk.gov.hmcts.reform.preapi.util.FunctionalTestBase;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReportControllerFT extends FunctionalTestBase {
-
-
     @DisplayName("Scenario: Should format Duration and date correctly")
     @Test
     void shouldFormatDurationAndDateCorrectly() throws JsonProcessingException {
-        var postResponseData = doPostRequest("/testing-support/should-delete-recordings-for-booking", false)
-            .body().jsonPath();
+        var captureSessionId = doPostRequest("/testing-support/should-delete-recordings-for-booking", false)
+            .body().jsonPath().getUUID("captureSessionId");
 
         var response = doGetRequest(REPORTS_ENDPOINT + "/capture-sessions-concurrent", true);
+        var list = response.getBody().jsonPath().getList("", ConcurrentCaptureSessionReportDTO.class);
+
+        int index = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId().equals(captureSessionId)) {
+                index = i;
+                break;
+            }
+        }
 
         var json = OBJECT_MAPPER.readTree(response.getBody().asString());
 
-        assertThat(json.get(0).get("duration").asText()).isEqualTo("PT30M");
-        assertThat(json.get(0).get("start_time").asText()).matches("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z");
+        assertThat(json.get(index).get("duration").asText()).isEqualTo("PT30M");
+        assertThat(json.get(index).get("start_time")
+                       .asText()).matches("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z");
     }
 }
