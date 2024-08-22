93e6513872826d72747689aad85f8a2c16ec50ea
Jason Paige <jasonpaige@users.noreply.github.com>
2024-08-22 16:26:29 +0100

    S28 3072/bugs (#652)
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>
    Co-authored-by: PRE DevOps <138598290+pre-devops@users.noreply.github.com>
    Co-authored-by: lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
    Co-authored-by: lucas-phillips28 <lucas.phillips@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/preapi/dto/media/GenerateAssetDTO.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/dto/media/GenerateAssetDTO.java
@@ -1,25 +1,31 @@
 package uk.gov.hmcts.reform.preapi.dto.media;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Schema(description = "GenerateAssetDTO")
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(value = { "new_recording_id" })
 public class GenerateAssetDTO {
 
     @Schema(description = "GenerateAssetSourceContainer")
+    @Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}-input$")
     private String sourceContainer;
 
     @Schema(description = "GenerateAssetDestinationContainer")
-    private String destinationContainer;
+    private UUID destinationContainer;
 
     @Schema(description = "GenerateAssetTempAsset")
     private String tempAsset;
@@ -29,4 +35,12 @@ public class GenerateAssetDTO {
 
     @Schema(description = "GenerateAssetDescription")
     private String description;
+
+    @Schema(description = "ParentRecordingId")
+    private UUID parentRecordingId;
+
+
+    public UUID getNewRecordingId() {
+        return destinationContainer;
+    }
 }
