06dc6b2bf11cca788cc91f25846b68e71d567268
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-08-29 13:09:07 +0100

    styling

--- src/main/java/uk/gov/hmcts/reform/preapi/services/CaseService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/CaseService.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.preapi.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
