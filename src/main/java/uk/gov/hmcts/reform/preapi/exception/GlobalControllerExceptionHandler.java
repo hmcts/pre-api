1eb168dcc098ea8201a5b87c2eab4403919afc56
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-09-06 09:28:04 +0100

    S28 3248: Handle Streaming Locator 409 Error Response (#686)

--- src/main/java/uk/gov/hmcts/reform/preapi/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/exception/GlobalControllerExceptionHandler.java
@@ -212,6 +212,11 @@ public class GlobalControllerExceptionHandler {
                                  HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<String> exceptionHandler(final Exception e) throws JsonProcessingException {
+        return getResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     private static ResponseEntity<String> getResponseEntity(String message, HttpStatus status)
         throws JsonProcessingException {
 
