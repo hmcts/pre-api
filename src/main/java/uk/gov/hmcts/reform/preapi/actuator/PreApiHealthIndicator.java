c67ba0f13fc50344f1652c6708fd1761f9d2e23b
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2024-10-08 17:18:45 +0100

    S28 3223: Custom Health Indicator (Add checks for MK storage account connections) (#717)
    Co-authored-by: Jenkins <nobody@nowhere>
    Co-authored-by: hmcts-jenkins-cnp <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/preapi/actuator/PreApiHealthIndicator.java
@@ -0,0 +1,92 @@
+package uk.gov.hmcts.reform.preapi.actuator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.preapi.media.MediaKindAccountsClient;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class PreApiHealthIndicator implements HealthIndicator {
+    @Value("${media-service}")
+    protected String mediaService;
+
+    @Value("${azure.ingestStorage.accountName}")
+    protected String ingestStorageAccountName;
+
+    @Value("${azure.finalStorage.accountName}")
+    protected String finalStorageAccountName;
+
+    public final MediaKindAccountsClient mediaKindAccountsClient;
+
+    @Autowired
+    public PreApiHealthIndicator(@Lazy MediaKindAccountsClient mediaKindAccountsClient) {
+        this.mediaKindAccountsClient = mediaKindAccountsClient;
+    }
+
+    @Override
+    public Health health() {
+        if (!mediaService.equals("MediaKind")) {
+            return Health.up().build();
+        }
+        try {
+            var details = checkMediaKindConnections();
+            return Health.up().withDetail("mediakindConnections", details).build();
+        } catch (Exception e) {
+            log.error("Encountered an error when attempting to check MK storage account connections: {}",
+                      e.getMessage());
+        }
+
+        return Health.up().withDetail("mediakindConnections", "unknown error").build();
+    }
+
+    private Map<String, Boolean> checkMediaKindConnections() {
+        var storageAccounts = mediaKindAccountsClient.getStorageAccounts();
+        if (storageAccounts.getItems().isEmpty()) {
+            log.error("MediaKind does not have any storage account connections");
+            return Map.of(
+                ingestStorageAccountName, false,
+                finalStorageAccountName, false
+            );
+        }
+
+        var expectedStorageAccounts = List.of(ingestStorageAccountName, finalStorageAccountName);
+        return expectedStorageAccounts
+            .stream()
+            .map(storageAccount ->
+                storageAccounts
+                    .getItems()
+                    .stream()
+                    .filter(connection -> connection.getSpec().getName().equals(storageAccount))
+                    .findFirst()
+                    .map(connection -> {
+                        if (connection.getStatus().getActiveCredentialId() == null
+                            || !Objects.equals(connection.getStatus().getPrivateLinkServiceConnectionStatus(),
+                                               "Running")) {
+                            log.error("MediaKind connection {} is not active", storageAccount);
+                            return Map.entry(storageAccount, false);
+                        } else {
+                            log.info("MediaKind connection {} is active", storageAccount);
+                            return Map.entry(storageAccount, true);
+                        }
+                    })
+                    .or(
+                        () -> {
+                            log.error("MediaKind connection {} not found", storageAccount);
+                            return Optional.of(Map.entry(storageAccount, false));
+                        }))
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
