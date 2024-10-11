f36292f5ec51ebcece8f842808a4e563eab0763d
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-10-11 15:55:00 +0100

    styling

--- src/main/java/uk/gov/hmcts/reform/preapi/repositories/AuditRepository.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/repositories/AuditRepository.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.preapi.repositories;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
