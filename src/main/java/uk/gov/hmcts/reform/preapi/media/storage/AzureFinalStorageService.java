96ecb67811ae4a48743f3de0f837b8384636f99e
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-10-17 11:09:41 +0100

    Add ability to download from blob storage

--- src/main/java/uk/gov/hmcts/reform/preapi/media/storage/AzureFinalStorageService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/media/storage/AzureFinalStorageService.java
@@ -1,11 +1,14 @@
 package uk.gov.hmcts.reform.preapi.media.storage;
 
 import com.azure.storage.blob.BlobServiceClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class AzureFinalStorageService extends AzureStorageService {
+
     @Autowired
     public AzureFinalStorageService(BlobServiceClient finalStorageClient) {
         super(finalStorageClient);
