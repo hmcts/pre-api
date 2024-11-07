13a55f7b097b6805588df4d027090d45a6775aec
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-11-07 10:35:21 +0000

    styling

--- src/test/java/uk/gov/hmcts/reform/preapi/media/storage/AzureFinalStorageServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/media/storage/AzureFinalStorageServiceTest.java
@@ -35,8 +35,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
