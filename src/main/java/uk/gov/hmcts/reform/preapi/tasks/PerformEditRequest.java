fd1389968f56fcb01e252df05badd48674f901ab
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-11-08 11:54:48 +0000

    styling

--- src/main/java/uk/gov/hmcts/reform/preapi/tasks/PerformEditRequest.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/tasks/PerformEditRequest.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.PessimisticLockingFailureException;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.preapi.entities.EditRequest;
 import uk.gov.hmcts.reform.preapi.enums.EditRequestStatus;
