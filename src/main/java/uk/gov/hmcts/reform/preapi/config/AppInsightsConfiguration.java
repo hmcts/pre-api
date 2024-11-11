6ab8241dc1e1af9a7bcd3a8814e36d1439248a0d
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-11-11 12:24:43 +0000

    test app insight custom event

---
+++ src/main/java/uk/gov/hmcts/reform/preapi/config/AppInsightsConfiguration.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.reform.preapi.config;
+
+import com.microsoft.applicationinsights.TelemetryClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppInsightsConfiguration {
+    @Bean
+    public TelemetryClient telemetryClient(@Value("${azure.application-insights.connection-string:}") String connectionString) {
+        if (connectionString == null) {
+            return null;
+        }
+        var client = new TelemetryClient();
+        client.getContext().setConnectionString(connectionString);
+        return client;
+    }
+}
