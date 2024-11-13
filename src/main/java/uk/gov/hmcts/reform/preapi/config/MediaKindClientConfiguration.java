67b3cb605d8bde496d1768f5824e3b9346dfd262
Jason Paige <j@jasonpaige.co.uk>
2024-11-13 15:52:45 +0000

    remove log

--- src/main/java/uk/gov/hmcts/reform/preapi/config/MediaKindClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/config/MediaKindClientConfiguration.java
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Logger;
 import feign.codec.Decoder;
 import feign.jackson.JacksonDecoder;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import uk.gov.hmcts.reform.preapi.media.FeignErrorDecoder;
 
-@Slf4j
 public class MediaKindClientConfiguration {
 
     @Value("${mediakind.token}")
@@ -18,7 +16,6 @@ public class MediaKindClientConfiguration {
 
     @Bean
     public MKRequestInterceptor requestInterceptor() {
-        log.info("Creating MKRequestInterceptor with token: {}", mkToken);
         return new MKRequestInterceptor(mkToken);
     }
 
