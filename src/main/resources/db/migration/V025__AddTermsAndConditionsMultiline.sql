a97064ae3d2786bffdcc6b5135935c4d06ac6695
Jason Paige <j@jasonpaige.co.uk>
2024-11-14 16:00:25 +0000

    test

---
+++ src/main/resources/db/migration/V025__AddTermsAndConditionsMultiline.sql
@@ -0,0 +1,23 @@
+INSERT into public.terms_and_conditions VALUES
+(
+ 'a017f73f-0ca7-4297-a249-d8e023f61809',
+ '# Terms & Conditions (including Acceptable Use) for the Pre-Recorded Evidence Service'
+
+'(All Users)'
+
+'**July 2022**'
+
+'## Introduction'
+
+'This PRE Service is provided by HM Courts and Tribunals Service (HMCTS) Unauthorised use is a criminal offence under the Computer Misuse Act 1990, and you should disconnect immediately if you have not been authorised to use this system. Unauthorised access is prevented by two factor authentication.'
+
+'The PRE Service provides access to pre-recorded cross examination recordings. It is supplied to individual users (The User) in accordance with the following Terms & Conditions.'
+
+'**The User** understands that use of the Service will be taken as their acceptance of these Terms & Conditions and that they are fully aware of their responsibilities in relation to the use of the service as set out in the Terms & Conditions on the page below.'
+
+'## Terms and Conditions'
+
+'1. I have a legitimate need to use the PRE Service, and will only access recordings associated with media of cases where I have a business need to do so.'
+'2. I will comply with UK Data Protection Act 2018, relevant privacy regulations and all professional codes of conduct by which I am bound and will ensure all information accessed through the PRE Service is treated accordingly. I acknowledge that any breach of these provisions may result in my access to the PRE Service being suspended or terminated. Any breach may also result in disciplinary action.',
+ 'PORTAL'
+);
