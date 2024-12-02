a6d26276b0a7bed94632af78e1b210f1759874ad
Jason Paige <j@jasonpaige.co.uk>
2024-12-02 14:36:48 +0000

    coverage

---
+++ src/test/java/uk/gov/hmcts/reform/preapi/config/AzureConfigurationTest.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.preapi.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AzureConfigurationTest {
+
+    @DisplayName("Test Jackson config for timestamp mapping")
+    @Test
+    void testGetBlobServiceClientUsingManagedIdentity() {
+        var  azureConfiguration = new AzureConfiguration();
+
+        var client = azureConfiguration.ingestStorageClient("connectionString",
+                                                            "ingestStorageAccountName",
+                                                            "managedIdentityClientId");
+        assertThat(client).isNotNull();
+    }
+}
