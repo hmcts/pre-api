e0ef802c6f731c75b6cc671c4eeab5a3e9d9a6ee
Jason Paige <j@jasonpaige.co.uk>
2024-12-03 10:59:17 +0000

    fml

--- src/main/java/uk/gov/hmcts/reform/preapi/config/AzureConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/config/AzureConfiguration.java
@@ -9,8 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.Nullable;
 import java.util.Arrays;
+import javax.annotation.Nullable;
 
 @Configuration
 @Slf4j
