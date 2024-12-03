447dda11955758706bda6a98f11675c889629853
lucas-phillips28 <lucas.phillips@hmcts.net>
2024-12-03 13:14:50 +0000

    ensure credentials used are for Super User role (if test account has secondary courts)

--- src/main/java/uk/gov/hmcts/reform/preapi/tasks/RobotUserTask.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/tasks/RobotUserTask.java
@@ -24,7 +24,10 @@ public abstract class RobotUserTask implements Runnable {
         log.info("Sign in as robot user");
         var user = userService.findByEmail(cronUserEmail);
 
-        var appAccess = user.getAppAccess().stream().findFirst()
+        var appAccess = user.getAppAccess()
+            .stream()
+            .filter(access -> access.getRole().getName().equals("Super User"))
+            .findFirst()
             .orElseThrow(() -> new RuntimeException(
                 "Failed to authenticate as cron user with email " + cronUserEmail)
             );
