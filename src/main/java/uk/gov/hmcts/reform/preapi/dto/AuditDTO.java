57266b98826a191f9029bda7e6d5e45c93b17183
Phoebe Revolta <phoebe.revolta@solirius.com>
2024-12-18 11:52:15 +0000

    S28-3556: reduce code repetition with AuditDTO inheritance from CreateAuditDTO

--- src/main/java/uk/gov/hmcts/reform/preapi/dto/AuditDTO.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/dto/AuditDTO.java
@@ -1,54 +1,23 @@
 package uk.gov.hmcts.reform.preapi.dto;
 
-import com.fasterxml.jackson.annotation.JsonRawValue;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.preapi.entities.Audit;
-import uk.gov.hmcts.reform.preapi.enums.AuditLogSource;
 
 import java.sql.Timestamp;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Schema(description = "AuditDTO")
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class AuditDTO {
-
-    @Schema(description = "AuditId")
-    @NotNull
-    private UUID id;
-
-    @Schema(description = "AuditTableName")
-    private String tableName;
-
-    @Schema(description = "AuditTableNameRecordId")
-    private UUID tableRecordId;
-
-    @Schema(description = "AuditLogSource")
-    @NotNull
-    private AuditLogSource source;
-
-    @Schema(
-        description = "AuditCategory",
-        examples = {"User", "Password", "Login", "2FA Code", "Recording", "Livestream"}
-    )
-    private String category;
-
-    @Schema(description = "AuditActivity", examples = {"Create", "Update", "Delete", "Check", "Play", "Locked"})
-    private String activity;
-
-    @Schema(description = "AuditFunctionalArea", examples = {"Registration", "Login", "Video Player", "API", "Admin"})
-    private String functionalArea;
-
-    @Schema(description = "AuditDetailsJSONString")
-    @JsonRawValue
-    private JsonNode auditDetails;
+public class AuditDTO extends CreateAuditDTO {
 
     @Schema(description = "AuditCreatedAt")
     @NotNull
@@ -59,6 +28,7 @@ public class AuditDTO {
     private UUID createdBy;
 
     public AuditDTO(Audit auditEntity) {
+        super();
         this.id = auditEntity.getId();
         this.tableName = auditEntity.getTableName();
         this.tableRecordId = auditEntity.getTableRecordId();
