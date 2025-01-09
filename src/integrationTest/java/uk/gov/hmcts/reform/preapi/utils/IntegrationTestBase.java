b29faf770dc30f0da00f76dee7b957322354e678
Jason Paige <jasonpaige@users.noreply.github.com>
2025-01-09 13:04:27 +0000

    Update src/integrationTest/java/uk/gov/hmcts/reform/preapi/utils/IntegrationTestBase.java
    Co-authored-by: lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/preapi/utils/IntegrationTestBase.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/preapi/utils/IntegrationTestBase.java
@@ -43,7 +43,6 @@ public abstract class IntegrationTestBase {
         return mockAuth;
     }
 
-    @SuppressWarnings("checkstyle:EmptyCatchBlock")
     @AfterEach
     void tearDown() {
         try {
