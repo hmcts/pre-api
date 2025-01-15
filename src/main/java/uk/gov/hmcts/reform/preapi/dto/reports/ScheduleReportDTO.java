2261ef8f18ba8a5bc324d4a195a71d359bff2b7e
lucas-phillips28 <lucas.phillips@hmcts.net>
2025-01-15 10:30:25 +0000

    inherit base report dto

--- src/main/java/uk/gov/hmcts/reform/preapi/dto/reports/ScheduleReportDTO.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/dto/reports/ScheduleReportDTO.java
@@ -4,34 +4,20 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.preapi.entities.CaptureSession;
-import uk.gov.hmcts.reform.preapi.entities.Region;
 import uk.gov.hmcts.reform.preapi.utils.DateTimeUtils;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Schema(description = "ScheduleReportDTO")
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class ScheduleReportDTO {
+public class ScheduleReportDTO extends BaseReportDTO {
 
     @Schema(description = "ScheduleReportStartedDate")
     private String scheduledDate;
-
-    @Schema(description = "ScheduleReportCaseReference")
-    private String caseReference;
-
-    @Schema(description = "ScheduleReportCourtName")
-    private String court;
-
-    @Schema(description = "ScheduleReportCourtCounty")
-    private String county;
-
-    @Schema(description = "ScheduleReportCourtPostcode")
-    private String postcode;
-
-    @Schema(description = "ScheduleReportCourtRegion")
-    private String region;
 
     @Schema(description = "ScheduleReportBookingCreatedAt")
     private String dateOfBooking;
@@ -40,18 +26,11 @@ public class ScheduleReportDTO {
     private String user;
 
     public ScheduleReportDTO(CaptureSession captureSession) {
+        super(captureSession.getBooking().getCaseId());
         var booking = captureSession.getBooking();
-        var caseEntity = booking.getCaseId();
         scheduledDate = DateTimeUtils.formatDate(booking.getScheduledFor());
-        caseReference = caseEntity.getReference();
-
-        var courtEntity = caseEntity.getCourt();
-        court = courtEntity.getName();
-        county = courtEntity.getCounty();
-        postcode = courtEntity.getPostcode();
-        region = courtEntity.getRegions().stream().findFirst().map(Region::getName).orElse(null);
-
         dateOfBooking = DateTimeUtils.formatDate(booking.getCreatedAt());
+
         if (captureSession.getStartedByUser() != null) {
             user = captureSession.getStartedByUser().getEmail();
         }
