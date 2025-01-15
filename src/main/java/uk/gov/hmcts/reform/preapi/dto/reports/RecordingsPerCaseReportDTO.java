7014be7b60bacfd8054216e57692f060a4524621
lucas-phillips28 <lucas.phillips@hmcts.net>
2025-01-15 10:13:25 +0000

    inherit base report dto

--- src/main/java/uk/gov/hmcts/reform/preapi/dto/reports/RecordingsPerCaseReportDTO.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/dto/reports/RecordingsPerCaseReportDTO.java
@@ -4,45 +4,22 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.preapi.entities.Case;
-import uk.gov.hmcts.reform.preapi.entities.Region;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Schema(description = "RecordingsPerCaseReportDTO")
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class RecordingsPerCaseReportDTO {
-
-    @Schema(description = "RecordingsPerCaseCaseReference")
-    private String caseReference;
-
-    @Schema(description = "RecordingsPerCaseCourt")
-    private String court;
-
-    @Schema(description = "RecordingsPerCaseCourtCounty")
-    private String county;
-
-    @Schema(description = "RecordingsPerCaseCourtPostcode")
-    private String postcode;
-
-    @Schema(description = "RecordingsPerCaseRegion")
-    private String region;
+public class RecordingsPerCaseReportDTO extends BaseReportDTO {
 
     @Schema(description = "RecordingsPerCaseCount")
     private int count;
 
     public RecordingsPerCaseReportDTO(Case caseEntity, int count) {
-        caseReference = caseEntity.getReference();
-        var courtEntity = caseEntity.getCourt();
-        court = courtEntity.getName();
-        county = courtEntity.getCounty();
-        postcode = courtEntity.getPostcode();
-        region = courtEntity.getRegions()
-            .stream()
-            .findFirst()
-            .map(Region::getName)
-            .orElse(null);
+        super(caseEntity);
         this.count = count;
     }
 }
