d223940a31b656c22d096df05432c28d6852a00e
Marianne Azzopardi <76755256+marazzo@users.noreply.github.com>
2025-01-17 11:38:35 +0000

    updating courts with new info

--- src/main/resources/db/migration/V026__AddNewCourtsNRO.sql
+++ src/main/resources/db/migration/V026__AddNewCourtsNRO.sql
@@ -29,7 +29,7 @@ VALUES
 (gen_random_uuid(), 'CROWN', 'Warwickshire (South) Justice Centre', '463'),
 (gen_random_uuid(), 'CROWN', 'Worcester Combined Court', '466'),
 (gen_random_uuid(), 'CROWN', 'Derby Combined Centre', '419'),
-(gen_random_uuid(), 'CROWN', 'Wolverhampton Crown Court', '421'),
+(gen_random_uuid(), 'CROWN', 'Wolverhampton Combined Court Centre', '421'),
 (gen_random_uuid(), 'CROWN', 'Leicester Crown Court', '430'),
 (gen_random_uuid(), 'CROWN', 'Lincoln Crown Court', '432'),
 (gen_random_uuid(), 'CROWN', 'Bradford Crown Court', '402'),
@@ -47,7 +47,6 @@ VALUES
 (gen_random_uuid(), 'CROWN', 'Lancaster Crown Court', '409'),
 (gen_random_uuid(), 'CROWN', 'Manchester Crown Court (Crown Square)', '435'),
 (gen_random_uuid(), 'CROWN', 'Manchester Crown Court (Minshull Street)', '436'),
-(gen_random_uuid(), 'CROWN', 'Preston Combined Court Centre (Sessions House Annex)', '448'),
 (gen_random_uuid(), 'CROWN', 'Warrington Crown Court', '462'),
 (gen_random_uuid(), 'CROWN', 'Bolton Combined Court', '470'),
 (gen_random_uuid(), 'CROWN', 'Aylesbury Crown Court', '401'),
@@ -55,9 +54,6 @@ VALUES
 (gen_random_uuid(), 'CROWN', 'Chelmsford Crown Court', '414'),
 (gen_random_uuid(), 'CROWN', 'Ipswich Crown Court', '426'),
 (gen_random_uuid(), 'CROWN', 'Lewes Combined Court', '431'),
-(gen_random_uuid(), 'CROWN', 'Chichester Nightingale Crown Court', NULL),
-(gen_random_uuid(), 'CROWN', 'Hove Trial Centre', '1602'),
-(gen_random_uuid(), 'MAGISTRATE', 'Brighton Magistrates Court', '431'),
 (gen_random_uuid(), 'CROWN', 'Maidstone Combined Court Centre', '434'),
 (gen_random_uuid(), 'CROWN', 'Norwich Combined Court Centre', '443'),
 (gen_random_uuid(), 'CROWN', 'Oxford Combined Court Centre', '445'),
@@ -68,7 +64,6 @@ VALUES
 (gen_random_uuid(), 'CROWN', 'Luton Crown Court', '476'),
 (gen_random_uuid(), 'CROWN', 'Canterbury Combined Court', '479'),
 (gen_random_uuid(), 'CROWN', 'King''s Lynn Crown Court', '765'),
-(gen_random_uuid(), 'CROWN', 'Southend Crown Court', '772'),
 (gen_random_uuid(), 'CROWN', 'Bournemouth Combined Court', '406'),
 (gen_random_uuid(), 'CROWN', 'Bristol Crown Court', '408'),
 (gen_random_uuid(), 'CROWN', 'Gloucester Crown Court', '424'),
@@ -79,13 +74,15 @@ VALUES
 (gen_random_uuid(), 'CROWN', 'Taunton Combined Court', '459'),
 (gen_random_uuid(), 'CROWN', 'Winchester Combined Court Centre', '465'),
 (gen_random_uuid(), 'CROWN', 'Truro Combined Court', '477'),
-(gen_random_uuid(), 'CROWN', 'Newport (I.O.W.) Combined Court', '478'),
 (gen_random_uuid(), 'CROWN', 'Salisbury Law Courts', '480'),
 (gen_random_uuid(), 'CROWN', 'Cardiff Crown Court', '411'),
 (gen_random_uuid(), 'CROWN', 'Merthyr Tydfil Crown Court', '437'),
 (gen_random_uuid(), 'CROWN', 'Newport (South Wales) Crown Court', '441'),
 (gen_random_uuid(), 'CROWN', 'Swansea Crown Court', '457'),
-(gen_random_uuid(), 'CROWN', 'Caernarfon Justice Centre', '755');
+(gen_random_uuid(), 'CROWN', 'Caernarfon Justice Centre', '755'),
+(gen_random_uuid(), 'CROWN','Hereford Crown Court','762'),
+(gen_random_uuid(), 'CROWN','Isle of Wight Combined (and Magistrates) Court','478'),
+(gen_random_uuid(), 'CROWN','Preston Combined Court Centre','448');
 
 -- Insert court-region associations into 'court_region'
 -- Fetch court_id and region_id based on matching location codes and region names
@@ -95,14 +92,14 @@ FROM PUBLIC.courts c
 JOIN PUBLIC.regions r
 ON CASE
 	WHEN c.location_code IN ( '413', '418', '440', '453', '468', '469', '471', '472', '475' ) THEN r.NAME = 'London'
-    WHEN c.location_code IN ( '417', '452', '455', '456', '463', '466', '421' ) THEN r.NAME = 'West Midlands (England)'
+    WHEN c.location_code IN ( '417', '452', '455', '456', '463', '466', '421','762' ) THEN r.NAME = 'West Midlands (England)'
     WHEN c.location_code IN ( '442', '419', '430', '432' ) THEN r.NAME = 'East Midlands (England)'
 	WHEN c.location_code IN ( '409', '412', '415', '435', '436', '448', '462', '470' ) THEN r.NAME = 'North West (England)'
-    WHEN c.location_code IN ( '401', '431', '1602', '434', '445', '474', '479', '447', '454', '465', '478' ) THEN r.NAME = 'South East (England)'
-    WHEN c.location_code IN ( '406', '408', '424', '446', '458', '459', '477', '480' ) THEN r.NAME = 'South West (England)'
+    WHEN c.location_code IN ( '401', '431', '434', '445', '474', '479', '447', '454', '465' ) THEN r.NAME = 'South East (England)'
+    WHEN c.location_code IN ( '406', '408', '424', '446', '458', '459', '477', '480','478' ) THEN r.NAME = 'South West (England)'
     WHEN c.location_code IN ( '439', '460', '467' ) THEN r.NAME = 'North East (England)'
     WHEN c.location_code IN ( '411', '437', '441', '457', '755' )  THEN r.NAME = 'Wales'
-    WHEN c.location_code IN ( '410', '414', '426', '443', '450', '461', '473', '476', '765', '772' ) THEN r.NAME = 'East of England'
+    WHEN c.location_code IN ( '410', '414', '426', '443', '450', '461', '473', '476', '765' ) THEN r.NAME = 'East of England'
     WHEN c.location_code IN ( '402', '420', '425', '451', '766' ) THEN r.NAME = 'Yorkshire and The Humber'
 END; 
 
@@ -137,18 +134,15 @@ SELECT
 FROM public.courts c
 WHERE (c.location_code = '409' AND c.name = 'Burnley Combined Court Centre')
    OR (c.location_code = '409' AND c.name = 'Lancaster Crown Court')
-   OR (c.location_code = '431' AND c.name = 'Lewes Combined Court')
-   OR (c.location_code = '431' AND c.name = 'Brighton Magistrates Court')
    OR (c.name = 'Newcastle upon Tyne Combined Court Centre')
-   OR (c.name = 'Chichester Nightingale Crown Court')
    OR c.location_code IN (
     '401', '402', '406', '408', '410', '411', '412', '413',
 	'414', '415', '417', '418', '419', '420', '421', '424', '425', '426',
-	'430', '432', '434', '435', '436', '437', '439', '440',
+	'430', '432', '434', '435', '436', '437', '439', '440', '431',
 	'441', '442', '443', '445', '446', '447', '448', '450', '451', '452',
 	'453', '454', '455', '456', '457', '458', '459', '460', '461', '462',
 	'463', '465', '466', '467', '468', '469', '470', '471', '472', '473',
 	'474', '475', '476', '477', '478', '479', '480', '755', '765', '766',
-	'772', '1602'
+    '762'
 );
 
