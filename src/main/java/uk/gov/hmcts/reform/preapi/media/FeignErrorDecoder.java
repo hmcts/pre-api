a41dc3a25eec93cf737e04b27def46ddd67a143b
Jason Paige <jasonpaige@users.noreply.github.com>
2025-01-30 16:34:48 +0000

    Check for response body before processing errors (#911)

--- src/main/java/uk/gov/hmcts/reform/preapi/media/FeignErrorDecoder.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/media/FeignErrorDecoder.java
@@ -18,22 +18,24 @@ public class FeignErrorDecoder implements ErrorDecoder {
 
     @Override
     public Exception decode(String methodKey, Response response) {
-        MkErrorResponse errorResponse = null;
-        try (InputStream bodyIs = response.body().asInputStream()) {
-            ObjectMapper mapper = new ObjectMapper();
-            errorResponse = mapper.readValue(bodyIs, MkErrorResponse.class);
-        } catch (IOException e) {
-            return new Exception(e.getMessage());
-        }
-        if (response.status() == 404) {
-            return new NotFoundException(errorResponse.getError().getDetail() != null
-                                             ? errorResponse.getError().getDetail()
-                                             : "Not found");
-        }
-        // MK API sometimes returns a 400 instead of a 409 when the live event already exists
-        if ((response.status() == 400 && errorResponse.getError().getDetail().contains(" already exists."))
-            || response.status() == 409) {
-            return new ConflictException(errorResponse.getError().getDetail());
+        if (response.body() != null) {
+            MkErrorResponse errorResponse = null;
+            try (InputStream bodyIs = response.body().asInputStream()) {
+                ObjectMapper mapper = new ObjectMapper();
+                errorResponse = mapper.readValue(bodyIs, MkErrorResponse.class);
+            } catch (IOException e) {
+                return new Exception(e.getMessage());
+            }
+            if (response.status() == 404) {
+                return new NotFoundException(errorResponse.getError().getDetail() != null
+                                                 ? errorResponse.getError().getDetail()
+                                                 : "Not found");
+            }
+            // MK API sometimes returns a 400 instead of a 409 when the live event already exists
+            if ((response.status() == 400 && errorResponse.getError().getDetail().contains(" already exists."))
+                || response.status() == 409) {
+                return new ConflictException(errorResponse.getError().getDetail());
+            }
         }
 
         return errorDecoder.decode(methodKey, response);
