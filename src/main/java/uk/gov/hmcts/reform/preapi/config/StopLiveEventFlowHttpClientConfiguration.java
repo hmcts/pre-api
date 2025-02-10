549f9c8fc2eb1376c4bc0bf51b9bf53d0985ec09
lucas-phillips28 <lucas.phillips@hmcts.net>
2025-02-10 11:52:37 +0000

    duplicate lines

--- src/main/java/uk/gov/hmcts/reform/preapi/config/StopLiveEventFlowHttpClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/config/StopLiveEventFlowHttpClientConfiguration.java
@@ -1,13 +1,7 @@
 package uk.gov.hmcts.reform.preapi.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import feign.Logger;
-import feign.codec.Decoder;
-import feign.jackson.JacksonDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import uk.gov.hmcts.reform.preapi.media.FeignErrorDecoder;
 
 @Configuration
 public class StopLiveEventFlowHttpClientConfiguration {
@@ -15,21 +9,5 @@ public class StopLiveEventFlowHttpClientConfiguration {
     @Bean
     public StopLiveEventFlowRequestInterceptor requestInterceptor() {
         return new StopLiveEventFlowRequestInterceptor();
-    }
-
-    @Bean
-    @Primary
-    Decoder feignDecoder(ObjectMapper objectMapper) {
-        return new JacksonDecoder(objectMapper);
-    }
-
-    @Bean
-    public FeignErrorDecoder errorDecoder() {
-        return new FeignErrorDecoder();
-    }
-
-    @Bean
-    Logger.Level feignLoggerLevel() {
-        return Logger.Level.FULL;
     }
 }
