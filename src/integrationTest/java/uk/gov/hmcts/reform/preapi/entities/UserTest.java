30803f26dc185a8e9049ee390be0743a5efced13
Jason Paige <j@jasonpaige.co.uk>
2025-02-11 12:08:48 +0000

    checkstyle

--- src/integrationTest/java/uk/gov/hmcts/reform/preapi/entities/UserTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/preapi/entities/UserTest.java
@@ -1,9 +1,7 @@
 package uk.gov.hmcts.reform.preapi.entities;
 
-import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.reform.preapi.Application;
 import uk.gov.hmcts.reform.preapi.util.HelperFactory;
