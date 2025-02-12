f1d4bc0e7770c278025d2e0b82accf2a2ed9808e
Marianne Azzopardi <76755256+marazzo@users.noreply.github.com>
2025-02-12 18:19:29 +0000

    update Leeds court name, update 2 location codes, insert into court regions, update audit table

---
+++ src/main/resources/db/migration/V028__UpdateNewCourtsNRO.sql
@@ -0,0 +1,93 @@
+-- Leeds Crown Court - Updated name to "Leeds Combined Court Centre"
+UPDATE courts
+SET name = 'Leeds Combined Court Centre'
+WHERE name = 'Leeds Crown Court';
+
+-- Newcastle upon Tyne Combined Court Centre - Updated location code to 439
+UPDATE public.courts
+SET location_code = 439
+WHERE name = 'Newcastle upon Tyne Combined Court Centre';
+
+-- Kingston-upon-Thames Crown Court - Updated location code to 427
+UPDATE courts
+SET location_code = 427
+WHERE name = 'Kingston-upon-Thames Crown Court';
+
+--Kingston-upon-Thames Crown Court - Updated court region to "London"
+--Newcastle upon Tyne Combined Court Centre - Updated court region to "North East (England)"
+INSERT INTO PUBLIC.court_region (court_id, region_id)
+SELECT c.id AS court_id, r.id AS region_id
+FROM PUBLIC.courts c
+JOIN PUBLIC.regions r
+ON CASE
+    WHEN c.name = 'Newcastle upon Tyne Combined Court Centre' THEN r.NAME = 'North East (England)'
+    WHEN c.name = 'Kingston-upon-Thames Crown Court' THEN r.NAME = 'London'
+END; 
+
+-- Leeds Crown Court - Audit entry regarding update
+INSERT INTO public.audits (
+    id, 
+    table_name, 
+    table_record_id, 
+    source, 
+    category, 
+    activity, 
+    functional_area, 
+    created_by, 
+    created_at, 
+    audit_details
+)
+SELECT
+    gen_random_uuid() AS id,
+    'courts' AS table_name,
+    c.id AS table_record_id,
+    'AUTO' AS source,
+    'Court' AS category,
+    'Update' AS activity,
+    'Update a court' AS functional_area,
+    (SELECT id FROM public.users WHERE last_name = 'Admin' LIMIT 1) AS created_by,
+    NOW() AS created_at,
+    jsonb_build_object(
+        'description', 'Court name has been updated.',
+        'courtName', c.name,
+        'locationCode', c.location_code
+    ) AS audit_details
+FROM public.courts c
+WHERE c.name IN (
+    'Leeds Combined Court Centre'
+);
+
+--Kingston-upon-Thames Crown Court - Audit entry regarding update
+--Newcastle upon Tyne Combined Court Centre - Audit entry regarding update
+INSERT INTO public.audits (
+    id, 
+    table_name, 
+    table_record_id, 
+    source, 
+    category, 
+    activity, 
+    functional_area, 
+    created_by, 
+    created_at, 
+    audit_details
+)
+SELECT
+    gen_random_uuid() AS id,
+    'courts' AS table_name,
+    c.id AS table_record_id,
+    'AUTO' AS source,
+    'Court' AS category,
+    'Update' AS activity,
+    'Update a court' AS functional_area,
+    (SELECT id FROM public.users WHERE last_name = 'Admin' LIMIT 1) AS created_by,
+    NOW() AS created_at,
+    jsonb_build_object(
+        'description', 'Court location code updated',
+        'courtName', c.name,
+        'locationCode', c.location_code
+    ) AS audit_details
+FROM public.courts c
+WHERE c.name IN (
+    'Newcastle upon Tyne Combined Court Centre',
+    'Kingston-upon-Thames Crown Court'
+);
