90a2d9f1d6c5cf672dd11e0ebd96833fa666d3ff
lucas-phillips28 <lucas.phillips@hmcts.net>
2025-03-25 10:26:32 +0000

    change `@MockBean` to `@MockitoBean`

--- src/test/java/uk/gov/hmcts/reform/preapi/services/EditRequestServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/services/EditRequestServiceTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.PessimisticLockingFailureException;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import uk.gov.hmcts.reform.preapi.entities.EditRequest;
 import uk.gov.hmcts.reform.preapi.enums.EditRequestStatus;
 import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
@@ -28,7 +28,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest(classes = EditRequestService.class)
 public class EditRequestServiceTest {
 
-    @MockBean
+    @MockitoBean
     private EditRequestRepository editRequestRepository;
 
     @Autowired
