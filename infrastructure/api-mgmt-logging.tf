1b1a0c383095e3de4fe04acfd2e6ccf77d34ff8c
Josh Blackmoor <joshua.blackmoor@justice.gov.uk>
2025-03-26 11:26:09 +0000

    Case correction

--- infrastructure/api-mgmt-logging.tf
+++ infrastructure/api-mgmt-logging.tf
@@ -8,7 +8,7 @@ resource "azurerm_api_management_api_diagnostic" "api_mgmt_logs" {
   sampling_percentage = 100.0
   always_log_errors = true
   log_client_ip = true
-  verbosity = "Verbose"
+  verbosity = "verbose"
   http_correlation_protocol = "W3C"
 
   frontend_request {
