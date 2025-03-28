397f65b07ed8b55f06341b0435e966abc80b040b
lucas-phillips28 <lucas.phillips@hmcts.net>
2025-03-28 14:56:54 +0000

    fix test

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/AuditController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/AuditController.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +16,6 @@ import uk.gov.hmcts.reform.preapi.dto.CreateAuditDTO;
 import uk.gov.hmcts.reform.preapi.exception.PathPayloadMismatchException;
 import uk.gov.hmcts.reform.preapi.services.AuditService;
 
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import static uk.gov.hmcts.reform.preapi.config.OpenAPIConfiguration.X_USER_ID_HEADER;
@@ -35,16 +34,16 @@ public class AuditController {
 
     @PutMapping("/{id}")
     @Operation(operationId = "putAudit", summary = "Create an Audit Entry")
-    public ResponseEntity<Void> upsertAudit(@RequestHeader Map<String, List<String>> headers,
+    public ResponseEntity<Void> upsertAudit(@RequestHeader MultiValueMap<String, String> headers,
                                             @PathVariable UUID id,
                                             @Valid @RequestBody CreateAuditDTO createAuditDTO) {
         if (!id.equals(createAuditDTO.getId())) {
             throw new PathPayloadMismatchException("id", "createAuditDTO.id");
         }
 
-        UUID userId = headers.containsKey(X_USER_ID_HEADER)
-            ? UUID.fromString(headers.get(X_USER_ID_HEADER).getFirst())
-            : null;
+        UUID userId = !headers.containsKey(X_USER_ID_HEADER)
+            ? null
+            : UUID.fromString(headers.get(X_USER_ID_HEADER).getFirst());
         this.auditService.upsert(createAuditDTO, userId);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
