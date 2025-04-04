9caf00e793c33baed6811845da62fbfa228be3a2
Marianne Azzopardi <76755256+marazzo@users.noreply.github.com>
2025-04-04 10:53:30 +0100

    adding noArgConstructor annotation to CSVArchiveListData and CSVChannelData

--- src/main/java/uk/gov/hmcts/reform/preapi/batch/entities/CSVArchiveListData.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/batch/entities/CSVArchiveListData.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.preapi.batch.entities;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.util.Objects;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class CSVArchiveListData implements IArchiveData  {
     private static final List<String> DATE_PATTERNS = List.of(
         "dd/MM/yyyy HH:mm", "dd/MM/yyyy H:mm",
--- src/main/java/uk/gov/hmcts/reform/preapi/batch/entities/CSVChannelData.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/batch/entities/CSVChannelData.java
@@ -2,18 +2,20 @@ package uk.gov.hmcts.reform.preapi.batch.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CSVChannelData {
 
     private String channelName;
     private String channelUser;
     private String channelUserEmail;
     private String caseReference;
-
+    
     @Override
     public String toString() {
         return "CSVChannelData{"
