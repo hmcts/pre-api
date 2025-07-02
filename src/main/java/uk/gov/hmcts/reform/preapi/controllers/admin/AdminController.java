4febcea8bf224a1587f7b23c7600a547f30ea8fb
ruth.bovell <ruth.bovell@justice.gov.uk>
2025-07-02 12:56:32 +0100

    creates admin endpoint

---
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/admin/AdminController.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.reform.preapi.controllers.admin;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.preapi.services.admin.AdminService;
+
+import java.util.UUID;
+
+/**
+ * Helper endpoints intended for developers to use in troubleshooting/investigating
+ * incidents.
+ * Your user will need to have the role ROLE_SUPER_USER to access these endpoints.
+ */
+@RestController
+@RequestMapping("/admin")
+@PreAuthorize("hasRole('ROLE_SUPER_USER')")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+
+    @GetMapping("/{id}")
+    @Operation(operationId = "checkUuid", summary = "Check if a UUID exists in the system",
+        description = "Checks if a UUID exists in any of the tables: User, Recording, CaptureSession, Booking, Case, Court.")
+    public ResponseEntity<String> checkUuidExists(@PathVariable UUID id) {
+        return ResponseEntity.ok("Uuid relates to a " + adminService.findUuidType(id));
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/admin/AdminService.java
@@ -0,0 +1,50 @@
+package uk.gov.hmcts.reform.preapi.services.admin;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
+import uk.gov.hmcts.reform.preapi.repositories.admin.AdminRepository;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class AdminService {
+
+    private final AdminRepository adminRepository;
+
+    @Autowired
+    public AdminService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    /**
+     * Checks if a UUID exists in relevant tables.
+     * Uses the AdminRepository to search the database for the UUID. The String returned from the repository
+     * is then converted to an enum value.
+     * @param id the UUID to check the tables for
+     * @return a string indicating the type of table related to the UUID
+     * @throws NotFoundException if the UUID does not exist in the tables being checked
+     */
+    public UuidTableType findUuidType(UUID id) {
+
+        String tableName = adminRepository.findUuidType(Objects.requireNonNull(id, "UUID should not be null"))
+            .orElseThrow(() -> new NotFoundException(id + " does not exist in any relevant table"));
+
+       return UuidTableType.valueOf(tableName.toUpperCase());
+    }
+
+    /**
+     * Enum representing the tables the UUID could belong to.
+     */
+    @Getter
+    public enum UuidTableType {
+        USER,
+        RECORDING,
+        CAPTURE_SESSION,
+        BOOKING,
+        CASE,
+        COURT;
+    }
+}
