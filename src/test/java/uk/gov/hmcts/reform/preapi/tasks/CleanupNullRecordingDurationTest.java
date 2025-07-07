72dcd363c87020bcfadbd64063df0c86d4726193
lucas-phillips28 <lucas.phillips@hmcts.net>
2025-07-07 16:28:55 +0100

    feat: add a task to clean up recordings with null duration

---
+++ src/test/java/uk/gov/hmcts/reform/preapi/tasks/CleanupNullRecordingDurationTest.java
@@ -0,0 +1,253 @@
+package uk.gov.hmcts.reform.preapi.tasks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import uk.gov.hmcts.reform.preapi.dto.AccessDTO;
+import uk.gov.hmcts.reform.preapi.dto.CaptureSessionDTO;
+import uk.gov.hmcts.reform.preapi.dto.CreateRecordingDTO;
+import uk.gov.hmcts.reform.preapi.dto.RecordingDTO;
+import uk.gov.hmcts.reform.preapi.dto.base.BaseAppAccessDTO;
+import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
+import uk.gov.hmcts.reform.preapi.media.edit.FfmpegService;
+import uk.gov.hmcts.reform.preapi.media.storage.AzureFinalStorageService;
+import uk.gov.hmcts.reform.preapi.security.authentication.UserAuthentication;
+import uk.gov.hmcts.reform.preapi.security.service.UserAuthenticationService;
+import uk.gov.hmcts.reform.preapi.services.RecordingService;
+import uk.gov.hmcts.reform.preapi.services.UserService;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = CleanupNullRecordingDuration.class)
+class CleanupNullRecordingDurationTest {
+    @MockitoBean
+    private RecordingService recordingService;
+
+    @MockitoBean
+    private AzureFinalStorageService azureFinalStorageService;
+
+    @MockitoBean
+    private FfmpegService ffmpegService;
+
+    @MockitoBean
+    private UserService userService;
+
+    @MockitoBean
+    private UserAuthenticationService userAuthenticationService;
+
+    @Autowired
+    private CleanupNullRecordingDuration cleanupNullRecordingDuration;
+
+    private static final String CRON_USER_EMAIL = "test@test.com";
+
+    @BeforeEach
+    void beforeEach() {
+        AccessDTO accessDto = mock(AccessDTO.class);
+        BaseAppAccessDTO baseAppAccessDTO = mock(BaseAppAccessDTO.class);
+        when(baseAppAccessDTO.getId()).thenReturn(UUID.randomUUID());
+
+        when(userService.findByEmail(CRON_USER_EMAIL)).thenReturn(accessDto);
+        when(accessDto.getAppAccess()).thenReturn(Set.of(baseAppAccessDTO));
+
+        UserAuthentication userAuth = mock(UserAuthentication.class);
+        when(userAuth.getUserId()).thenReturn(UUID.randomUUID());
+        when(userAuthenticationService.validateUser(any())).thenReturn(Optional.of(userAuth));
+    }
+
+    @Test
+    @DisplayName("Should process recording with null duration successfully")
+    void runSuccess() {
+        UUID recordingId = UUID.randomUUID();
+        RecordingDTO recordingDto = new RecordingDTO();
+        recordingDto.setId(recordingId);
+        CaptureSessionDTO captureSessionDto = new CaptureSessionDTO();
+        captureSessionDto.setId(UUID.randomUUID());
+        recordingDto.setCaptureSession(captureSessionDto);
+        recordingDto.setFilename("test.mp4");
+        when(recordingService.findAllDurationNull()).thenReturn(List.of(recordingDto));
+        when(azureFinalStorageService.doesContainerExist(recordingId.toString())).thenReturn(true);
+        when(azureFinalStorageService.getRecordingDuration(recordingId)).thenReturn(Duration.ofMinutes(10));
+        when(azureFinalStorageService.doesBlobExist(recordingId.toString(), "test.mp4")).thenReturn(true);
+
+        cleanupNullRecordingDuration.run();
+
+        ArgumentCaptor<CreateRecordingDTO> captor = ArgumentCaptor.forClass(CreateRecordingDTO.class);
+        verify(recordingService).upsert(captor.capture());
+        CreateRecordingDTO capturedRecording = captor.getValue();
+        assertThat(capturedRecording.getDuration()).isEqualTo(Duration.ofMinutes(10));
+    }
+
+    @Test
+    @DisplayName("Should skip recording when storage container does not exist")
+    void runSkipRecordingWithNoStorageContainer() {
+        UUID recordingId = UUID.randomUUID();
+        RecordingDTO recordingDto = new RecordingDTO();
+        recordingDto.setId(recordingId);
+        when(recordingService.findAllDurationNull()).thenReturn(List.of(recordingDto));
+        when(azureFinalStorageService.doesContainerExist(recordingId.toString())).thenReturn(false);
+
+        cleanupNullRecordingDuration.run();
+
+        verifyNoInteractions(ffmpegService);
+        verify(recordingService, never()).upsert(any());
+    }
+
+    @Test
+    @DisplayName("Should update filename when current filename is invalid")
+    void runUpdateFilenameWhenFilenameIsInvalid() {
+        UUID recordingId = UUID.randomUUID();
+        CaptureSessionDTO captureSessionDto = new CaptureSessionDTO();
+        captureSessionDto.setId(UUID.randomUUID());
+        RecordingDTO recordingDto = new RecordingDTO();
+        recordingDto.setId(recordingId);
+        recordingDto.setCaptureSession(captureSessionDto);
+        recordingDto.setFilename(null);
+        when(recordingService.findAllDurationNull()).thenReturn(List.of(recordingDto));
+        String newFilename = "new-filename.mp4";
+        when(azureFinalStorageService.getMp4FileName(recordingId.toString())).thenReturn(newFilename);
+        when(azureFinalStorageService.doesContainerExist(recordingId.toString())).thenReturn(true);
+        when(azureFinalStorageService.getRecordingDuration(recordingId)).thenReturn(null);
+        when(azureFinalStorageService.generateReadSasUrl(recordingId.toString(), newFilename))
+            .thenReturn("sas-url");
+        when(ffmpegService.getDurationFromMp4ViaSasToken("sas-url")).thenReturn(Duration.ofMinutes(10));
+
+        cleanupNullRecordingDuration.run();
+
+        verify(azureFinalStorageService, times(1)).doesContainerExist(recordingId.toString());
+        verify(azureFinalStorageService, never()).doesBlobExist(any(), any());
+        verify(azureFinalStorageService, times(1)).getMp4FileName(recordingId.toString());
+        verify(azureFinalStorageService, times(1)).generateReadSasUrl(recordingId.toString(), newFilename);
+        verify(ffmpegService, times(1)).getDurationFromMp4ViaSasToken("sas-url");
+        ArgumentCaptor<CreateRecordingDTO> captor = ArgumentCaptor.forClass(CreateRecordingDTO.class);
+        verify(recordingService, times(1)).upsert(captor.capture());
+
+        CreateRecordingDTO capturedRecording = captor.getValue();
+        assertThat(capturedRecording.getDuration()).isEqualTo(Duration.ofMinutes(10));
+        assertThat(capturedRecording.getFilename()).isEqualTo(newFilename);
+    }
+
+    @Test
+    @DisplayName("Should skip recording when mp4 file is not found")
+    void runSkipRecordingWithoutMp4File() {
+        UUID recordingId = UUID.randomUUID();
+        CaptureSessionDTO captureSessionDto = new CaptureSessionDTO();
+        captureSessionDto.setId(UUID.randomUUID());
+        RecordingDTO recordingDto = new RecordingDTO();
+        recordingDto.setId(recordingId);
+        recordingDto.setCaptureSession(captureSessionDto);
+        recordingDto.setFilename(null);
+        when(recordingService.findAllDurationNull()).thenReturn(List.of(recordingDto));
+        when(azureFinalStorageService.doesContainerExist(recordingId.toString())).thenReturn(true);
+        when(azureFinalStorageService.getMp4FileName(recordingId.toString())).thenThrow(
+            new NotFoundException("File not found"));
+        when(azureFinalStorageService.getRecordingDuration(recordingId)).thenReturn(null);
+
+        cleanupNullRecordingDuration.run();
+
+        verify(azureFinalStorageService, times(1)).doesContainerExist(recordingId.toString());
+        verify(azureFinalStorageService).getMp4FileName(recordingId.toString());
+        verify(recordingService, never()).upsert(any());
+    }
+
+    @Test
+    @DisplayName("Should try to get duration from ffmpeg when metadata duration is null")
+    void runGetDurationFromFfmpegWhenMetadataDurationIsNull() {
+        UUID recordingId = UUID.randomUUID();
+        CaptureSessionDTO captureSessionDto = new CaptureSessionDTO();
+        captureSessionDto.setId(UUID.randomUUID());
+        RecordingDTO recordingDto = new RecordingDTO();
+        recordingDto.setId(recordingId);
+        recordingDto.setCaptureSession(captureSessionDto);
+        recordingDto.setFilename("test.mp4");
+        when(recordingService.findAllDurationNull()).thenReturn(List.of(recordingDto));
+        when(azureFinalStorageService.doesContainerExist(recordingId.toString())).thenReturn(true);
+        when(azureFinalStorageService.getRecordingDuration(recordingId)).thenReturn(null);
+        when(azureFinalStorageService.doesBlobExist(recordingId.toString(), "test.mp4")).thenReturn(true);
+        when(azureFinalStorageService.generateReadSasUrl(recordingId.toString(), "test.mp4")).thenReturn("sas-url");
+        when(ffmpegService.getDurationFromMp4ViaSasToken("sas-url")).thenReturn(Duration.ofMinutes(5));
+
+        cleanupNullRecordingDuration.run();
+
+        verify(ffmpegService).getDurationFromMp4ViaSasToken("sas-url");
+        verify(recordingService).upsert(any(CreateRecordingDTO.class));
+    }
+
+    @Test
+    @DisplayName("Should log error when ffmpeg fails to get duration")
+    void runDoNotUpdateWhenFfmpegFailsToGetDuration() {
+        UUID recordingId = UUID.randomUUID();
+        CaptureSessionDTO captureSessionDto = new CaptureSessionDTO();
+        captureSessionDto.setId(UUID.randomUUID());
+        RecordingDTO recordingDto = new RecordingDTO();
+        recordingDto.setId(recordingId);
+        recordingDto.setCaptureSession(captureSessionDto);
+        recordingDto.setFilename("test.mp4");
+        when(recordingService.findAllDurationNull()).thenReturn(Collections.singletonList(recordingDto));
+        when(azureFinalStorageService.doesContainerExist(recordingId.toString())).thenReturn(true);
+        when(azureFinalStorageService.getRecordingDuration(recordingId)).thenReturn(null);
+        when(azureFinalStorageService.doesBlobExist(recordingId.toString(), "test.mp4")).thenReturn(true);
+        when(azureFinalStorageService.generateReadSasUrl(recordingId.toString(), "test.mp4")).thenReturn("sas-url");
+        when(ffmpegService.getDurationFromMp4ViaSasToken("sas-url")).thenThrow(new RuntimeException("FFmpeg error"));
+
+        cleanupNullRecordingDuration.run();
+
+        verify(ffmpegService).getDurationFromMp4ViaSasToken("sas-url");
+        verify(recordingService, never()).upsert(any());
+    }
+
+    @Test
+    @DisplayName("Should use fallback when filename blob does not exist")
+    void runGetFilenameWhenBlobDoesntExist() {
+        UUID recordingId = UUID.randomUUID();
+        CaptureSessionDTO captureSessionDto = new CaptureSessionDTO();
+        captureSessionDto.setId(UUID.randomUUID());
+
+        RecordingDTO recordingDto = new RecordingDTO();
+        recordingDto.setId(recordingId);
+        recordingDto.setCaptureSession(captureSessionDto);
+        recordingDto.setFilename("missing.mp4");
+
+        when(recordingService.findAllDurationNull()).thenReturn(List.of(recordingDto));
+        when(azureFinalStorageService.doesContainerExist(recordingId.toString())).thenReturn(true);
+        when(azureFinalStorageService.getRecordingDuration(recordingId)).thenReturn(null);
+        when(azureFinalStorageService.doesBlobExist(recordingId.toString(), "missing.mp4")).thenReturn(false);
+
+        String fallbackFilename = "fallback.mp4";
+        when(azureFinalStorageService.getMp4FileName(recordingId.toString())).thenReturn(fallbackFilename);
+        when(azureFinalStorageService.doesBlobExist(recordingId.toString(), fallbackFilename)).thenReturn(true);
+        when(azureFinalStorageService.generateReadSasUrl(recordingId.toString(), fallbackFilename))
+            .thenReturn("sas-url");
+        when(ffmpegService.getDurationFromMp4ViaSasToken("sas-url")).thenReturn(Duration.ofMinutes(3));
+
+        cleanupNullRecordingDuration.run();
+
+        verify(azureFinalStorageService).getMp4FileName(recordingId.toString());
+        verify(azureFinalStorageService).generateReadSasUrl(recordingId.toString(), fallbackFilename);
+        verify(ffmpegService).getDurationFromMp4ViaSasToken("sas-url");
+
+        ArgumentCaptor<CreateRecordingDTO> captor = ArgumentCaptor.forClass(CreateRecordingDTO.class);
+        verify(recordingService).upsert(captor.capture());
+
+        CreateRecordingDTO captured = captor.getValue();
+        assertThat(captured.getFilename()).isEqualTo(fallbackFilename);
+        assertThat(captured.getDuration()).isEqualTo(Duration.ofMinutes(3));
+    }
+}
