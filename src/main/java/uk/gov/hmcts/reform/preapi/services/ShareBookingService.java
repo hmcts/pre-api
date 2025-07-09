c2bf7a751004d304cbc2d8f3b3742a7baf06a6d8
lucas-phillips28 <140058662+lucas-phillips28@users.noreply.github.com>
2025-07-09 17:33:14 +0100

    S28 2346: Re-enable Functional Test (#987)

--- src/main/java/uk/gov/hmcts/reform/preapi/services/ShareBookingService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/ShareBookingService.java
@@ -37,9 +37,7 @@ import java.util.stream.Stream;
 public class ShareBookingService {
 
     private final ShareBookingRepository shareBookingRepository;
-
     private final BookingRepository bookingRepository;
-
     private final UserRepository userRepository;
 
     private final EmailServiceFactory emailServiceFactory;
