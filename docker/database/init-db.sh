8f019953716e7836db6f85fb465d566233249fbb
Jason Paige <j@jasonpaige.co.uk>
2025-07-09 19:11:38 +0100

    dockerfile update

--- docker/database/init-db.sh
+++ docker/database/init-db.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username postgres --dbname api <<-EOSQL
   CREATE EXTENSION IF NOT EXISTS pgcrypto;
   CREATE EXTENSION IF NOT EXISTS pg_trgm;
   SET TIME ZONE 'UTC';
