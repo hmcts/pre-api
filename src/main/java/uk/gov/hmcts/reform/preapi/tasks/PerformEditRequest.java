96647cca414e7e7055261c51191b99e0c3668ed8
lucas-phillips28 <lucas.phillips@hmcts.net>
2025-07-24 13:21:19 +0100

    feat(ffmpeg): implement multi-command execution and segment concatenation support

--- src/main/java/uk/gov/hmcts/reform/preapi/tasks/PerformEditRequest.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/tasks/PerformEditRequest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.PessimisticLockingFailureException;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.preapi.entities.EditRequest;
 import uk.gov.hmcts.reform.preapi.exception.ResourceInWrongStateException;
@@ -27,6 +28,7 @@ public class PerformEditRequest extends RobotUserTask {
     }
 
     @Override
+    @Scheduled(cron = "0 * * * * *")
     public void run() {
         signInRobotUser();
         log.info("Running PerformEditRequest task");
