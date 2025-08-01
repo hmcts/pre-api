369e46754ded2300aa83b80960a1a2ef0b79997b
Jason Paige <j@jasonpaige.co.uk>
2025-08-01 15:13:56 +0100

    unused imports

--- src/test/java/uk/gov/hmcts/reform/preapi/services/AuditServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/services/AuditServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
