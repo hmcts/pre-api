dd06f87aa0fc5873544b6da54d70df39ee7cd755
George Barnes <george.barnes@justice.gov.uk>
2025-08-11 13:42:30 +0100

    Fix typo

--- src/test/java/uk/gov/hmcts/reform/preapi/services/CaseServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/services/CaseServiceTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.preapi.services;
 
 import feign.FeignException;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
