ef008435abe4962a1eb4aafeef8253bee087d11a
ruth.bovell <ruth.bovell@justice.gov.uk>
2025-08-15 10:48:44 +0100

    adds evicting users cache when user accepts terms and conditions

--- src/main/java/uk/gov/hmcts/reform/preapi/services/UserTermsAcceptedService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/UserTermsAcceptedService.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.preapi.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,22 +21,38 @@ public class UserTermsAcceptedService {
     private final UserRepository userRepository;
     private final TermsAndConditionsRepository termsAndConditionsRepository;
     private final UserTermsAcceptedRepository userTermsAcceptedRepository;
+    private final CacheManager cacheManager;
 
     @Autowired
     public UserTermsAcceptedService(UserRepository userRepository,
                                     TermsAndConditionsRepository termsAndConditionsRepository,
-                                    UserTermsAcceptedRepository userTermsAcceptedRepository) {
+                                    UserTermsAcceptedRepository userTermsAcceptedRepository,
+                                    CacheManager cacheManager) {
         this.userRepository = userRepository;
         this.termsAndConditionsRepository = termsAndConditionsRepository;
         this.userTermsAcceptedRepository = userTermsAcceptedRepository;
+        this.cacheManager = cacheManager;
     }
 
+    /**
+     * Accepts the specified terms and conditions for a user.
+     * <p>Retrieves the user from the database via security context. Gets the specified terms
+     * and conditions then creates and persists a record of the user's acceptance of those terms.</p>
+     * <p>
+     * After persisting the acceptance, this method manually evicts the the cache populated by {@link uk.gov.hmcts.reform.preapi.services.UserService#findByEmail(String)}.
+     * This ensures that the user's updated terms acceptance is reflected in the cache on subsequent calls to
+     * {@link uk.gov.hmcts.reform.preapi.services.UserService#findByEmail(String)}.
+     * <p>
+     * @param termsId the UUID of the terms and conditions to accept
+     * @throws uk.gov.hmcts.reform.preapi.exception.NotFoundException if the user or terms and conditions are not found
+     */
     @Transactional
     public void acceptTermsAndConditions(UUID termsId) {
         var userId = ((UserAuthentication) SecurityContextHolder.getContext().getAuthentication()).getUserId();
         var user = userRepository.findById(userId)
             // this will not happen
             .orElseThrow(() -> new NotFoundException("User: " + userId));
+        var userEmail = user.getEmail(); // for cache eviction
         var termsAndConditions = termsAndConditionsRepository.findById(termsId)
             .orElseThrow(() -> new NotFoundException("TermsAndConditions: " + termsId));
 
@@ -45,5 +62,10 @@ public class UserTermsAcceptedService {
         accepted.setTermsAndConditions(termsAndConditions);
         accepted.setAcceptedAt(Timestamp.from(Instant.now()));
         userTermsAcceptedRepository.save(accepted);
+
+        var cache = cacheManager.getCache("users");
+        if (cache != null) {
+            cache.evict(userEmail.toLowerCase());
+        }
     }
 }
