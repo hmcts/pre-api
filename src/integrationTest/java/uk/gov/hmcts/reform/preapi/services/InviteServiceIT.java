ca3b72ef2d89b6ce966a46a1561e0636ba6bb5db
George Barnes <george.barnes@justice.gov.uk>
2025-08-18 11:50:30 +0100

    Integration Tests for Invite service

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/preapi/services/InviteServiceIT.java
@@ -0,0 +1,59 @@
+package uk.gov.hmcts.reform.preapi.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.reform.preapi.entities.TermsAndConditions;
+import uk.gov.hmcts.reform.preapi.entities.User;
+import uk.gov.hmcts.reform.preapi.enums.TermsAndConditionsType;
+import uk.gov.hmcts.reform.preapi.util.HelperFactory;
+import uk.gov.hmcts.reform.preapi.utils.IntegrationTestBase;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+public class UserTermsAcceptedServiceIT extends IntegrationTestBase {
+
+    @Autowired
+    private UserTermsAcceptedService userTermsAcceptedService;
+
+    private User user;
+    private TermsAndConditions termsAndConditions;
+
+    @BeforeEach
+    void setUp() {
+        user = HelperFactory.createDefaultTestUser();
+        user.setId(UUID.randomUUID());
+        entityManager.persist(user);
+
+        termsAndConditions = HelperFactory.createTermsAndConditions(TermsAndConditionsType.APP, "Content");
+        entityManager.persist(termsAndConditions);
+
+        entityManager.flush();
+    }
+
+    @Test
+    @Transactional
+    public void acceptTermsAndConditions() {
+        var mockedUser = mockAdminUser();
+        when(mockedUser.getUserId()).thenReturn(user.getId());
+        userTermsAcceptedService.acceptTermsAndConditions(termsAndConditions.getId());
+
+        entityManager.flush();
+        entityManager.refresh(user);
+
+        assertThat(user.getUserTermsAccepted()).isNotNull();
+        assertThat(user.getUserTermsAccepted()).hasSize(1);
+
+        var termsAccepted = user.getUserTermsAccepted().stream().findFirst().get();
+        assertThat(termsAccepted).isNotNull();
+        assertThat(termsAccepted.getUser().getId()).isEqualTo(user.getId());
+        assertThat(termsAccepted.getTermsAndConditions().getId()).isEqualTo(termsAndConditions.getId());
+        // very recently created
+        assertThat(termsAccepted.getAcceptedAt()).isAfter(Instant.now().minusSeconds(5));
+    }
+}
