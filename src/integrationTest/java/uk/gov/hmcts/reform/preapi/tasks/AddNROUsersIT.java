57e35290ac9c4968b77e0cb4cde14d64c1a4b4a1
ruth.bovell <ruth.bovell@justice.gov.uk>
2025-09-08 14:10:02 +0100

    Removes unused import

--- src/integrationTest/java/uk/gov/hmcts/reform/preapi/tasks/AddNROUsersIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/preapi/tasks/AddNROUsersIT.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.TestPropertySource;
 import uk.gov.hmcts.reform.preapi.entities.AppAccess;
 import uk.gov.hmcts.reform.preapi.entities.base.BaseEntity;
 import uk.gov.hmcts.reform.preapi.enums.CourtType;
