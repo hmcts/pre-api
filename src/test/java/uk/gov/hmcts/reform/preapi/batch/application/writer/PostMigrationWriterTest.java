d01044be4f92216e18ff22e1b4923414e1e18356
Jason Paige <j@jasonpaige.co.uk>
2025-09-17 16:40:07 +0100

    style

--- src/test/java/uk/gov/hmcts/reform/preapi/batch/application/writer/PostMigrationWriterTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/batch/application/writer/PostMigrationWriterTest.java
@@ -17,8 +17,12 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class PostMigrationWriterTest {
