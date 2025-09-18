4c60ff21a105165f6d08c38cd1581f2805d517e5
bytesize-hq <bytesize-hq@gmail.com>
2025-09-18 14:10:01 +0100

    remove comment

--- src/main/java/uk/gov/hmcts/reform/preapi/batch/application/services/migration/MigrationGroupBuilderService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/batch/application/services/migration/MigrationGroupBuilderService.java
@@ -86,7 +86,6 @@ public class MigrationGroupBuilderService {
         );
         loggingService.logDebug("Migrating group: %s", migrationGroup);
         migrationTrackerService.addMigratedItem(passItem);
-        // migrationRecordService.updateToSuccess(item.getArchiveId());
         return migrationGroup;
     }
 
