4c800fa9352ec70870af4a96d7de7627474a34f1
Lydia Ralph <lydia.raph@justice.gov.uk>
2025-11-14 12:00:58 +0000

    Checkstyle

--- src/main/java/uk/gov/hmcts/reform/preapi/services/CaptureSessionService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/CaptureSessionService.java
@@ -38,8 +38,8 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
