502215e38956761042fd0a4912db2c88ab210750
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2026-01-23 15:14:50 +0000

    Updating Terraform Formatting

--- infrastructure/api-mgmt-logging.tf
+++ infrastructure/api-mgmt-logging.tf
@@ -1,14 +1,14 @@
 resource "azurerm_api_management_api_diagnostic" "api_mgmt_logs" {
-  identifier = "applicationinsights"
-  resource_group_name = "ss-${var.env}-network-rg"
-  api_management_name = "sds-api-mgmt-${var.env}"
-  api_name = module.pre_api[0].name
+  identifier               = "applicationinsights"
+  resource_group_name      = "ss-${var.env}-network-rg"
+  api_management_name      = "sds-api-mgmt-${var.env}"
+  api_name                 = module.pre_api[0].name
   api_management_logger_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/ss-${var.env}-network-rg/providers/Microsoft.ApiManagement/service/sds-api-mgmt-${var.env}/loggers/sds-api-mgmt-${var.env}-logger"
 
-  sampling_percentage = 100.0
-  always_log_errors = true
-  log_client_ip = true
-  verbosity = "verbose"
+  sampling_percentage       = 100.0
+  always_log_errors         = true
+  log_client_ip             = true
+  verbosity                 = "verbose"
   http_correlation_protocol = "W3C"
 
   frontend_request {
