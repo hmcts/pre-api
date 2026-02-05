6bae28ab52bb6e9aa451a75969b09ec65dbe8b3d
bytesize-hq <bytesize-hq@gmail.com>
2026-02-05 11:54:07 +0000

    coveragE

--- src/test/java/uk/gov/hmcts/reform/preapi/batch/config/jobs/FetchDataJobConfigTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/batch/config/jobs/FetchDataJobConfigTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.scope.context.StepContext;
@@ -14,15 +13,12 @@ import org.springframework.transaction.PlatformTransactionManager;
 import uk.gov.hmcts.reform.preapi.batch.application.processor.ArchiveMetadataXmlExtractor;
 import uk.gov.hmcts.reform.preapi.batch.application.services.MigrationRecordService;
 import uk.gov.hmcts.reform.preapi.batch.application.services.reporting.LoggingService;
-import uk.gov.hmcts.reform.preapi.batch.config.jobs.FetchDataJobConfig;
 import uk.gov.hmcts.reform.preapi.batch.config.steps.CoreStepsConfig;
 
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -74,31 +70,7 @@ class FetchDataJobConfigTest {
         assertEquals("fetchAndConvertXmlFileStep", step.getName());
     }
 
-    @Test
-    void createXmlFetchStep_shouldHandleCsvSourceType() throws Exception {
-
-        var jobParams = new HashMap<String, Object>();
-        jobParams.put("sourceType", "csv");
-
-        ChunkContext chunkContext = mock(ChunkContext.class);
-        StepContext stepContext = mock(StepContext.class);
-        StepExecution stepExecution = mock(StepExecution.class);
-
-        when(chunkContext.getStepContext()).thenReturn(stepContext);
-        when(stepContext.getJobParameters()).thenReturn(jobParams);
-        when(stepContext.getStepExecution()).thenReturn(stepExecution);
-
-        when(stepExecution.getExecutionContext()).thenReturn(new org.springframework.batch.item.ExecutionContext());
-
-        Step step = config.createXmlFetchStep();
-        var taskletStep = (TaskletStep) step;
-        var tasklet = taskletStep.getTasklet();
-
-        assertThrows(Exception.class, () -> tasklet.execute(null, chunkContext));
-
-        verify(loggingService).logInfo(contains("Skipping XML fetch"), eq("Archive_List_initial"));
-    }
-
+    
     @Test
     void createXmlFetchStep_shouldHandleXmlSourceType() throws Exception {
 
