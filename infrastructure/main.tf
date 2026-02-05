637e26ef79ce0ba17fa9e196657cef08ff1fa0bd
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2026-02-05 10:34:33 +0000

    Updating Terraform Formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,7 +10,7 @@ locals {
   env_to_deploy    = 1
   env_long_name    = var.env == "sbox" ? "sandbox" : var.env == "stg" ? "staging" : var.env
   apim_service_url = var.env == "prod" ? "https://pre-api.platform.hmcts.net" : "https://pre-api.${local.env_long_name}.platform.hmcts.net"
-  api_revision = "127"
+  api_revision     = "127"
 }
 
 data "azurerm_client_config" "current" {}
@@ -206,11 +206,11 @@ module "pre_api_b2c" {
 }
 
 module "pre-api-b2c-mgmt-api-policy" {
-  count         = var.env == "prod" ? 0 : 1
-  source        = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
-  api_name      = module.pre_api_b2c[0].name
-  api_mgmt_name = "sds-api-mgmt-${var.env}"
-  api_mgmt_rg   = "ss-${var.env}-network-rg"
+  count                  = var.env == "prod" ? 0 : 1
+  source                 = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
+  api_name               = module.pre_api_b2c[0].name
+  api_mgmt_name          = "sds-api-mgmt-${var.env}"
+  api_mgmt_rg            = "ss-${var.env}-network-rg"
   api_policy_xml_content = <<XML
 <policies>
   <inbound>
