104ff70cfa529e051c3c29ae38200b2c001c93d5
ruth.bovell <ruth.bovell@justice.gov.uk>
2026-02-24 16:45:31 +0000

    more tests

--- src/test/java/uk/gov/hmcts/reform/preapi/services/RegistrationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/preapi/services/RegistrationServiceTest.java
@@ -12,6 +12,7 @@ import uk.gov.hmcts.reform.preapi.dto.media.AssetDTO;
 import uk.gov.hmcts.reform.preapi.dto.media.LiveEventDTO;
 import uk.gov.hmcts.reform.preapi.enums.RecordingStatus;
 import uk.gov.hmcts.reform.preapi.enums.UpsertResult;
+import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
 import uk.gov.hmcts.reform.preapi.exception.ResourceInWrongStateException;
 import uk.gov.hmcts.reform.preapi.media.IMediaService;
 import uk.gov.hmcts.reform.preapi.media.MediaKind;
@@ -19,12 +20,10 @@ import uk.gov.hmcts.reform.preapi.media.MediaServiceBroker;
 import uk.gov.hmcts.reform.preapi.media.dto.MkJob;
 import uk.gov.hmcts.reform.preapi.media.storage.AzureIngestStorageService;
 
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -52,8 +51,6 @@ public class RegistrationServiceTest {
     private static final UUID CAPTURE_SESSION_ID = UUID.fromString("11111111-1111-1111-1111-111111111111");
     private static final UUID RECORDING_ID = UUID.fromString("22222222-2222-2222-2222-222222222222");
     private final String liveEventId = getSanitisedLiveEventId(CAPTURE_SESSION_ID);
-    private final String ingestJobName = format("%s-%d", getSanitisedLiveEventId(CAPTURE_SESSION_ID),
-                                                Instant.now().getEpochSecond());
     private final MkJob ingestJob = mock(MkJob.class);
 
     private RegistrationService underTest;
@@ -63,6 +60,45 @@ public class RegistrationServiceTest {
         mediaService = mock(IMediaService.class);
         when(mediaServiceBroker.getEnabledMediaService()).thenReturn(mediaService);
         underTest = new RegistrationService(captureSessionService, mediaServiceBroker, azureIngestStorageService);
+    }
+
+    @Test
+    @DisplayName("Registering: should successfully register capture session if jobs completed and recording found")
+    public void testRegistrationWhenEncodingIsCompleted() {
+        when(mediaService.getLiveEvent(liveEventId)).thenThrow(NotFoundException.class);
+        when(mediaService.getJobFromPartialName(MediaKind.ENCODE_FROM_INGEST_TRANSFORM, liveEventId))
+                .thenReturn(ingestJob);
+        MkJob.MkJobProperties properties = mock(MkJob.MkJobProperties.class);
+        when(ingestJob.getProperties()).thenReturn(properties);
+        when(properties.getState()).thenReturn(JobState.FINISHED);
+        List<JobOutputAsset> jobOutputAssets = new ArrayList<>();
+        JobOutputAsset jobOutputAsset = mock(JobOutputAsset.class);
+        jobOutputAssets.add(jobOutputAsset);
+        when(properties.getOutputs()).thenReturn(jobOutputAssets);
+        AssetDTO assetDTO = mock(AssetDTO.class);
+        when(jobOutputAsset.assetName()).thenReturn("2222222222222222222222222222222_temp");
+        when(mediaService.getAsset("2222222222222222222222222222222_temp")).thenReturn(assetDTO);
+        when(assetDTO.getContainer()).thenReturn(RECORDING_ID.toString());
+        when(mediaService.verifyFinalAssetExists(any())).thenReturn(RecordingStatus.RECORDING_AVAILABLE);
+        CaptureSessionDTO captureSessionDTO = new CaptureSessionDTO();
+        captureSessionDTO.setId(CAPTURE_SESSION_ID);
+        captureSessionDTO.setBookingId(UUID.fromString("33333333-3333-3333-3333-333333333333"));
+        when(captureSessionService
+                .stopCaptureSession(CAPTURE_SESSION_ID, RecordingStatus.RECORDING_AVAILABLE, RECORDING_ID))
+                .thenReturn(captureSessionDTO);
+
+        UpsertResult register = underTest.register(CAPTURE_SESSION_ID);
+
+        assertThat(register).isEqualTo(UpsertResult.UPDATED);
+
+        verify(mediaService, times(1)).verifyFinalAssetExists(RECORDING_ID);
+        verify(captureSessionService, times(1))
+                .stopCaptureSession(CAPTURE_SESSION_ID, RecordingStatus.RECORDING_AVAILABLE, RECORDING_ID);
+
+        verify(azureIngestStorageService, times(1))
+                .markContainerAsSafeToDelete("33333333-3333-3333-3333-333333333333");
+        verify(azureIngestStorageService, times(1))
+                .markContainerAsSafeToDelete(RECORDING_ID.toString());
     }
 
     @Test
@@ -130,43 +166,5 @@ public class RegistrationServiceTest {
         );
 
         verify(mediaService, times(1)).verifyFinalAssetExists(any());
-    }
-
-    @Test
-    @DisplayName("Registering: should successfully register capture session if jobs completed and recording found")
-    public void testRegistrationWhenEncodingIsCompleted() {
-        when(mediaService.getJobFromPartialName(MediaKind.ENCODE_FROM_INGEST_TRANSFORM, liveEventId))
-            .thenReturn(ingestJob);
-        MkJob.MkJobProperties properties = mock(MkJob.MkJobProperties.class);
-        when(ingestJob.getProperties()).thenReturn(properties);
-        when(properties.getState()).thenReturn(JobState.FINISHED);
-        List<JobOutputAsset> jobOutputAssets = new ArrayList<>();
-        JobOutputAsset jobOutputAsset = mock(JobOutputAsset.class);
-        jobOutputAssets.add(jobOutputAsset);
-        when(properties.getOutputs()).thenReturn(jobOutputAssets);
-        AssetDTO assetDTO = mock(AssetDTO.class);
-        when(jobOutputAsset.assetName()).thenReturn("2222222222222222222222222222222_temp");
-        when(mediaService.getAsset("2222222222222222222222222222222_temp")).thenReturn(assetDTO);
-        when(assetDTO.getContainer()).thenReturn(RECORDING_ID.toString());
-        when(mediaService.verifyFinalAssetExists(any())).thenReturn(RecordingStatus.RECORDING_AVAILABLE);
-        CaptureSessionDTO captureSessionDTO = new CaptureSessionDTO();
-        captureSessionDTO.setId(CAPTURE_SESSION_ID);
-        captureSessionDTO.setBookingId(UUID.fromString("33333333-3333-3333-3333-333333333333"));
-        when(captureSessionService
-                 .stopCaptureSession(CAPTURE_SESSION_ID, RecordingStatus.RECORDING_AVAILABLE, RECORDING_ID))
-                 .thenReturn(captureSessionDTO);
-
-        UpsertResult register = underTest.register(CAPTURE_SESSION_ID);
-
-        assertThat(register).isEqualTo(UpsertResult.UPDATED);
-
-        verify(mediaService, times(1)).verifyFinalAssetExists(RECORDING_ID);
-        verify(captureSessionService, times(1))
-            .stopCaptureSession(CAPTURE_SESSION_ID, RecordingStatus.RECORDING_AVAILABLE, RECORDING_ID);
-
-        verify(azureIngestStorageService, times(1))
-            .markContainerAsSafeToDelete("33333333-3333-3333-3333-333333333333");
-        verify(azureIngestStorageService, times(1))
-            .markContainerAsSafeToDelete(RECORDING_ID.toString());
     }
 }
