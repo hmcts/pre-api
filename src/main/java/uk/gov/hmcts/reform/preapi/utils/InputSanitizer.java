9e1fef9e06f495bf863afc7b63a42bebb0cc8087
ruth.bovell <ruth.bovell@justice.gov.uk>
2026-03-18 17:22:41 +0000

    wip

--- src/main/java/uk/gov/hmcts/reform/preapi/utils/InputSanitizer.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/utils/InputSanitizer.java
@@ -5,6 +5,11 @@ import org.jsoup.Jsoup;
 import org.jsoup.safety.Cleaner;
 import org.jsoup.safety.Safelist;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @UtilityClass
 public class InputSanitizer {
 
@@ -15,4 +20,31 @@ public class InputSanitizer {
 
         return text;
     }
+
+    public static Object sanitizeObject(Object dto) throws IllegalAccessException {
+        if (dto == null) {
+            return null;
+        }
+        Field[] fields = getAllFields(dto.getClass());
+        for (Field field : fields) {
+            if (field.getType() == String.class && !(field.get(dto) == null)) {
+                field.setAccessible(true);
+                field.set(dto, sanitize((String) field.get(dto)));
+            }
+        }
+
+        return dto;
+    }
+
+
+    public static Field[] getAllFields(Class<?> type) {
+        List<Field> fields = new ArrayList<>();
+
+        for (Class<?> c = type; c != null; c = c.getSuperclass()) {
+            fields.addAll(Arrays.asList(c.getDeclaredFields()));
+        }
+
+        return fields.toArray(new Field[0]);
+    }
+
 }
