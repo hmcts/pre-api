de2eeb9501454122345f8e65895cb6a0631ae785
Lydia Ralph <lydia.ralph@justice.gov.uk>
2026-04-21 19:24:19 +0100

    Original commit for S28 4898/store api spec locally (#1393)
    Co-authored-by: PRE DevOps <138598290+pre-devops@users.noreply.github.com>
    Co-authored-by: 52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>

---
+++ scripts/increment-apim-revision-and-commit.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Written for Github Actions (see .github/workflows)
+#
+# Run from project root folder.
+#
+# Install api-spec-converter before running:
+# npm install -g api-spec-converter --ignore-scripts
+
+main() {
+  set_up
+  convert_open_api_to_swagger
+  bump_revision_number_and_commit_changes_to_github
+  echo "done"
+}
+
+set_up() {
+  if ! test -f "specs/pre-api.json"; then
+    echo "Could not find API spec at path specs/pre-api.json"
+    exit 1
+  fi
+}
+
+convert_open_api_to_swagger() {
+  mkdir -p swagger_docs
+
+  echo "Converting OpenAPI to Swagger for pre-api"
+
+  api-spec-converter --to=swagger_2 --from=openapi_3  --syntax=yaml \
+    --order=alpha "specs/pre-api.json" > "swagger_docs/1.yaml"
+
+  sed -E "s/basePath\: \//basePath\: \/pre-api/g" "swagger_docs/1.yaml" > "swagger_docs/2.yaml"
+  sed -E "s/\- http/\- https/g" "swagger_docs/2.yaml" > "swagger_docs/3.yaml"
+  sed -E "s/(host\: .+)/host\: 'sds-api-mgmt.staging.platform.hmcts.net'/g" "swagger_docs/3.yaml" > "swagger_docs/4.yaml"
+
+  cp swagger_docs/4.yaml "pre-api-stg.yaml"
+
+  rm -rf swagger_docs
+};
+
+bump_revision_number_and_commit_changes_to_github(){
+  mkdir -p "temp_master"
+  git show master:infrastructure/main.tf > temp_master/main.tf
+  git show master:pre-api-stg.yaml > temp_master/pre-api-stg.yml
+
+  master_revision=$(grep -i 'api_revision' temp_master/main.tf | grep -oE '[0-9]+')
+  branch_revision=$(grep -i 'api_revision' infrastructure/main.tf | grep -oE '[0-9]+')
+  new_revision=master_revision
+
+  echo "Comparing master to branch"
+  if cmp temp_master/pre-api-stg.yml pre-api-stg.yaml; then
+    echo "No changes to pre-api-stg.yaml"
+    if [[ $master_revision -gt $branch_revision ]]; then
+      echo "Bump branch revision number to match master"
+      awk 'BEGIN{FS=OFS="\""} /api_revision=/{$2=new_revision}1' infrastructure/main.tf > temp && mv temp infrastructure/main.tf
+      git add infrastructure/main.tf
+    fi
+  else
+    echo "Main API Spec has changed compared to master, ensure revision is incremented"
+    echo "Master revision $master_revision; branch revision $branch_revision"
+    if [[ $master_revision -ge $branch_revision ]]; then
+      echo "Master revision $master_revision is less than/equal to branch revision $branch_revision"
+      new_revision=$((master_revision + 1))
+      echo "New revision number: $new_revision"
+      sed -e "s/^  api_revision.*\"$branch_revision\"/  api_revision = \"$new_revision\"/"  infrastructure/main.tf > temp && mv temp infrastructure/main.tf
+      git add infrastructure/main.tf
+    fi
+  fi
+
+  rm -rf temp_master
+
+  commit_message=""
+
+  CHANGED=$(git diff --name-only "specs/pre-api.json" pre-api-stg.yaml infrastructure/main.tf)
+
+  if [ "$CHANGED" != "" ]; then
+    echo "OpenAPI spec has been updated this commit"
+    git add -u "specs/pre-api.json" pre-api-stg.yaml infrastructure/main.tf
+    commit_message="Update OpenAPI Spec for pre-api with revision number $new_revision"
+    echo "About to commit: ${commit_message}"
+    git -c user.name='PRE DevOps' -c user.email='138598290+pre-devops@users.noreply.github.com' commit -m "$commit_message"
+    git push
+  fi
+}
+
+main
+
