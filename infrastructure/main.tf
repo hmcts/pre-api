317e2a27c5078c6c285539042330a177caaf1fa3
Lydia Ralph <lydia.ralph@justice.gov.uk>
2026-04-22 14:03:29 +0100

    Original commit for S28 4898/store api spec locally (#1393) (#1400)
    Co-authored-by: PRE DevOps <138598290+pre-devops@users.noreply.github.com>
    Co-authored-by: 52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -48,8 +48,8 @@ module "pre_api" {
   product_id            = module.pre_product[0].product_id
   path                  = "pre-api"
   service_url           = local.apim_service_url
-  swagger_url           = "https://raw.githubusercontent.com/hmcts/cnp-api-docs/master/docs/specs/pre-api.json"
-  content_format        = "openapi+json-link"
+  swagger_url           = file("../specs/pre-api.json")
+  content_format        = "openapi+json"
   protocols             = ["http", "https"]
   subscription_required = true
 }
@@ -177,8 +177,8 @@ module "pre_api_b2c" {
   product_id            = module.pre_b2c_product.product_id
   path                  = "pre-api-b2c"
   service_url           = local.apim_service_url
-  swagger_url           = "https://raw.githubusercontent.com/hmcts/cnp-api-docs/master/docs/specs/pre-api-b2c.json"
-  content_format        = "openapi+json-link"
+  swagger_url           = file("../specs/b2c-api.json")
+  content_format        = "openapi+json"
   protocols             = ["http", "https"]
   subscription_required = false
 }
