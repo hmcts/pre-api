f10f4ca7a6ff3343c963b9c47b0db6020f293966
Lydia Ralph <lydia.ralph@justice.gov.uk>
2026-05-26 15:52:02 +0100

    Try to trigger comment generation

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/CourtController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/CourtController.java
@@ -50,6 +50,11 @@ public class CourtController extends PreApiController {
         this.courtService = courtService;
     }
 
+    @GetMapping("/test")
+    public ResponseEntity<String> randomNewEndpointForTestingGithubActions() {
+        return ResponseEntity.ok("TODO: Delete this endpoint when testing is complete");
+    }
+
     @GetMapping("/{courtId}")
     @Operation(operationId = "getCourtById", summary = "Get a Court by Id")
     @PreAuthorize("hasAnyRole('ROLE_SUPER_USER', 'ROLE_LEVEL_1', 'ROLE_LEVEL_2', 'ROLE_LEVEL_3', 'ROLE_LEVEL_4')")
