ee2df66641f532f4c82fd2b2ae8ccb8f98fa247c
Jason Paige <j@jasonpaige.co.uk>
2026-06-18 10:49:33 +0100

    S28-4813 return edit request directly

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/TestingSupportController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/TestingSupportController.java
@@ -432,7 +432,7 @@ class TestingSupportController {
 
     @SneakyThrows
     @PostMapping(value = "/trigger-edit-request-processing/{editId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Object>> triggerEditRequestProcessing(@PathVariable UUID editId) {
+    public ResponseEntity<EditRequestDTO> triggerEditRequestProcessing(@PathVariable UUID editId) {
         Role role = roleRepository.findFirstByName("Super User")
             .orElse(createRole("Super User"));
         AppAccess appAccess = createAppAccess(role);
@@ -445,9 +445,7 @@ class TestingSupportController {
         editRequestPerformService.performEdit(editRequest);
         EditRequestDTO request = editRequestService.findById(editId);
 
-        return ResponseEntity.ok(Map.of(
-            "request", request
-        ));
+        return ResponseEntity.ok(request);
     }
 
     @PostMapping(value = "/trigger-task/{taskName}")
