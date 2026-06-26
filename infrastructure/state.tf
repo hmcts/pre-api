1129b44329c57b161f5da0a5e34a7c3188723e5c
ruth.bovell <ruth.bovell@justice.gov.uk>
2026-06-26 11:33:53 +0100

    add explanation for azurerm staying version 4.21

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -3,6 +3,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      # Temporarily pinned: do not upgrade azurerm until issue #29502 is resolved.
+      # https://github.com/hashicorp/terraform-provider-azurerm/issues/29502
       version = "4.21.0"
     }
   }
