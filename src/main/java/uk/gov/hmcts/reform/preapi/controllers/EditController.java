ebe3131aaaca14b64115709d643780e6096f6832
Lydia Ralph <lydia.ralph@justice.gov.uk>
2026-07-21 18:26:37 +0100

    Checkstyle

--- src/main/java/uk/gov/hmcts/reform/preapi/controllers/EditController.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/controllers/EditController.java
@@ -13,7 +13,6 @@ import org.springframework.data.web.SortDefault;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
