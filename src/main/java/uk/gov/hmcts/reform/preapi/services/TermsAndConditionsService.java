389cd73b83c7c1fd965a13e01fac09de06f18b10
Ruth Bovell <ruth.bovell@justice.gov.uk>
2026-07-22 12:00:12 +0100

    S28-5047 - Update App terms and conditions to include Dynatrace (#1517)

--- src/main/java/uk/gov/hmcts/reform/preapi/services/TermsAndConditionsService.java
+++ src/main/java/uk/gov/hmcts/reform/preapi/services/TermsAndConditionsService.java
@@ -2,24 +2,50 @@ package uk.gov.hmcts.reform.preapi.services;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.preapi.dto.TermsAndConditionsDTO;
+import uk.gov.hmcts.reform.preapi.entities.TermsAndConditions;
 import uk.gov.hmcts.reform.preapi.enums.TermsAndConditionsType;
 import uk.gov.hmcts.reform.preapi.exception.NotFoundException;
 import uk.gov.hmcts.reform.preapi.repositories.TermsAndConditionsRepository;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Service
 public class TermsAndConditionsService {
     private final TermsAndConditionsRepository termsAndConditionsRepository;
+
+    @Value("${feature-flags.dynatrace-terms-and-conditions.cut-off-date}")
+    private LocalDate cutOffDate;
+
+    @Value("${feature-flags.dynatrace-terms-and-conditions.enabled}")
+    private boolean isDynatraceAppTermsEnabled;
+
 
     @Autowired
     public TermsAndConditionsService(TermsAndConditionsRepository termsAndConditionsRepository) {
         this.termsAndConditionsRepository = termsAndConditionsRepository;
     }
 
+    /**
+     * Returns the latest terms for the given type.
+     * For `PORTAL`, always returns the newest terms.
+     * Temporarily - For `APP`, returns either the newest terms or the newest terms before the configured cutoff date,
+     * depending on `feature-flags.dynatrace-terms-and-conditions.enabled` property.
+     * @param type terms and conditions type (`APP` or `PORTAL`)
+     * @return latest applicable terms and conditions
+     */
     public TermsAndConditionsDTO getLatestTermsAndConditions(@NotNull TermsAndConditionsType type) {
-        return termsAndConditionsRepository.findFirstByTypeOrderByCreatedAtDesc(type)
-            .map(TermsAndConditionsDTO::new)
+        boolean useLatestTerms = type == TermsAndConditionsType.PORTAL || isDynatraceAppTermsEnabled;
+        Optional<TermsAndConditions> terms = useLatestTerms
+            ? termsAndConditionsRepository.findFirstByTypeOrderByCreatedAtDesc(type)
+            : termsAndConditionsRepository.findFirstByTypeAndCreatedAtBeforeOrderByCreatedAtDesc(
+            type, Timestamp.valueOf(cutOffDate.atStartOfDay()));
+
+        return terms.map(TermsAndConditionsDTO::new)
             .orElseThrow(() -> new NotFoundException("Terms and conditions of type: " + type));
     }
 }
